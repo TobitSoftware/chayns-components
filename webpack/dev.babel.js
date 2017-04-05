511d7ee88422da75dd3886e354d67d2db3e1185c
mbraun <michael.braun@tobit.software>
2017-04-05 18:53:33 +0200

    Added webpack-dev-server for all components

---
+++ webpack/dev.babel.js
@@ -0,0 +1,82 @@
+import webpack from 'webpack';
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const ROOT_PATH = path.resolve('.');
+
+export default {
+    entry: {
+        template: [
+            path.resolve(ROOT_PATH, 'examples', 'index')
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    output: {
+        path: path.resolve(ROOT_PATH, 'build'),
+        filename: `[name].bundle.js`
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 9000,
+        historyApiFallback: true,
+        compress: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader'],
+                include: path.resolve(ROOT_PATH, 'examples')
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true,
+                            minimize: false
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(ROOT_PATH, 'examples', 'index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development'),
+            __DEV__: true,
+            __QA__: false,
+            __LIVE__: false,
+        })
+    ]
+};
