ed563ebaa497fcc7f97f92ba7e559527720ab91a
Michael Braun <michael.braun@tobit.software>
2017-04-25 09:21:25 +0200

    Added Swiper (modified to work with react + chayns)

---
+++ src/react-chayns-swiper/index.jsx
@@ -0,0 +1,11 @@
+export {
+    default as Swiper,
+} from "./component/Slider";
+
+export {
+    default as Swipe,
+} from "./component/Slide";
+
+export {
+    default as SwipeOverlay,
+} from "./component/SlideOverlay";
