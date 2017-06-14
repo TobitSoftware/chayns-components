ecf518c0f7b12460f080941e1511276c7cfe823f
Kevin Mathmann <kevin.mathmann@gmail.com>
2017-06-14 14:32:45 +0200

    Improve Input example
    Fix react-transition-group was installed but react-addons-css-transition-group was used
    
    #34

--- examples/react-chayns-input/Example.jsx
+++ examples/react-chayns-input/Example.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 
-import {Input} from '../../src/react-chayns-input/index';
+import { Input } from '../../src/react-chayns-input/index';
 
 export default class Example extends React.Component {
     constructor() {
         super();
         this.state = {
-            userName: null
+            userNameOnBlur: null,
+            userNameOnChange: 'heeello',
         }
     }
 
     render() {
-        return(
-            <div>
+        const { userNameOnBlur, userNameOnChange } = this.state;
+        return (
+            <div style={{ border: 'solid 1px grey', padding: '5px' }}>
+                <h1>Input Example</h1>
+
+                <h3>userNameOnBlur</h3>
+                <p>{userNameOnBlur || '-'}</p>
+
+                <h3>userNameOnChange</h3>
+                <p>{userNameOnChange || '-'}</p>
+
                 <Input
-                    placeholder="Looking for 3 lowercase a's"
-                    regExp="^a{3}$"
-                    onBlur={ (text) => { this.setState({userName: text}, () => { console.log(this.state)}) }}
+                    defaultValue="heello i bims 1 invalid default Value ;)"
+                    placeholder="Looking for 3 lowercase e's in a row"
+                    //regExp=".*e{3}.*"
+                    onBlur={(text) => {
+                        this.setState({ userNameOnBlur: text })
+                    }}
+                    onChange={(text) => {
+                        this.setState({ userNameOnChange: text })
+                    }}
                     responsive={false}
-                    ref={(ref) => {this._node = ref}}
                 />
             </div>
         );
