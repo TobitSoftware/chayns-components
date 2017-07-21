406f3203eb50b7f63edea22d6d88c98ba199ea6c
Michael Braun <michael.braun@tobit.software>
2017-07-21 14:23:34 +0200

    Fixed Input-example

--- examples/react-chayns-input/Example.jsx
+++ examples/react-chayns-input/Example.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Input } from '../../src/react-chayns-input/index';
+import Input from '../../src/react-chayns-input/index';
 
 export default class Example extends React.Component {
     constructor() {
