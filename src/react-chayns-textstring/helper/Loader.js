0191b9118b48ce914e70ff05194b081095000ead
Michael Braun <michael.braun@tobit.software>
2017-07-25 10:45:39 +0200

    Fixed eslint-errors in TextString-Component

--- src/react-chayns-textstring/helper/Loader.js
+++ src/react-chayns-textstring/helper/Loader.js
@@ -1,15 +1,13 @@
-var loaded = [];
-
+const loaded = [];
 
 export default class Loader {
     static loadScript(url) {
-
         if(loaded.indexOf(url) !== -1) return Promise.resolve();
 
-        let head = Loader.getHead();
-        let tag = Loader.getScriptTag(url);
+        const head = Loader.getHead();
+        const tag = Loader.getScriptTag(url);
 
-        let retval = Loader.registerLoaders(tag).then(() => {
+        const retval = Loader.registerLoaders(tag).then(() => {
             loaded.push(url);
         });
 
@@ -19,35 +17,35 @@ export default class Loader {
     }
 
     static getHead() {
-        return document.getElementsByTagName("head")[0];
+        return document.getElementsByTagName('head')[0];
     }
 
     static getScriptTag(url) {
-        let script = document.createElement('script');
-        script.type='text/javascript';
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
         script.src = url;
 
         return script;
     }
 
     static registerLoaders(tag) {
-        return new Promise((resolve, reject) => {
-            tag.onload = resolve;
+        const scriptTag = tag;
 
+        return new Promise((resolve) => {
+            scriptTag.onload = resolve;
 
-            //for IE Browsers
-            ieLoadBugFix(tag);
-
-            function ieLoadBugFix(scriptElement){
-                if (scriptElement.readyState=='loaded' || scriptElement.readyState=='completed') {
+            function ieLoadBugFix(scriptElement) {
+                if (scriptElement.readyState === 'loaded' || scriptElement.readyState === 'completed') {
                     resolve();
                 } else {
-                    setTimeout(function() {
+                    setTimeout(() => {
                         ieLoadBugFix(scriptElement);
                     }, 100);
                 }
             }
 
+            // for IE Browsers
+            ieLoadBugFix(scriptTag);
         });
     }
 }
