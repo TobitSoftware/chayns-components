22bab5d11cdc846b1ee330c2bee9e17fbb9432c1
Michael Braun <michael.braun@tobit.software>
2017-11-21 12:10:06 +0100

    Update example

--- examples/react-chayns-scrollview/index.jsx
+++ examples/react-chayns-scrollview/index.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 import { ScrollView } from '../../src/index';
+import '../../src/react-chayns-scrollview/index.scss';
+
 import ExampleContainer from '../ExampleContainer';
 
 window.chayns.ready.then(() => {
     ReactDom.render(
         <ExampleContainer headline="ScrollView">
-            <ScrollView>
+            <ScrollView style={{ maxHeight: '100px' }}>
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
+                Test<br />
                 Test
             </ScrollView>
         </ExampleContainer>,
