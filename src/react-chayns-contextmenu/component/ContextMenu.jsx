9b6bcd5747403ba7a1338ce3ab6fe816ae330c2e
Michael Braun <michael.braun@tobit.software>
2017-11-21 13:31:22 +0100

    Fix eslint-errors in react-chayns-contextmenu

--- src/react-chayns-contextmenu/component/ContextMenu.jsx
+++ src/react-chayns-contextmenu/component/ContextMenu.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import ContextMenuItem from './ContextMenuItem';
 
+const ContextMenu = ({ hide, onLayerClick, x, y, items }) => {
+    const contextMenuClass = hide ? 'context-menu' : 'context-menu context-menu--active';
 
-const ContextMenu = ({hide, onLayerClick, x, y, items}) => {
-    const contextMenuClass = hide ? "context-menu" : "context-menu context-menu--active";
     return (
         <div className={contextMenuClass}>
-            <div className="context-menu__page-block" onClick={onLayerClick}></div>
-            <div className="context-menu__body" style={{
-                position: 'absolute',
-                left: x,
-                top: y
-            }}>
+            <div
+                className="context-menu__page-block"
+                onClick={onLayerClick}
+            />
+            <div
+                className="context-menu__body"
+                style={{
+                    position: 'absolute',
+                    left: x,
+                    top: y
+                }}
+            >
                 <ul className="context-menu__item-list">
                     {items.map((item, index) => {
                         return (
                             <ContextMenuItem
-                                key={index}
+                                key={`${item.className}-${index.text}`}
                                 iconClassName={item.className}
                                 text={item.text}
                                 onClick={item.onClick}
                             />
-                        )
+                        );
                     })}
                 </ul>
             </div>
         </div>
-    )
+    );
 };
 
 ContextMenu.defaultProps = {
     hide: true,
     onLayerClick: () => {},
     x: undefined,
-    y: undefined
+    y: undefined,
+    items: [],
 };
 
 ContextMenu.propTypes = {
--- src/react-chayns-contextmenu/index.jsx
+++ src/react-chayns-contextmenu/index.jsx
@@ -1,1 +1,1 @@
-export default from "./component/ContextMenu";
+export default from './component/ContextMenu';
