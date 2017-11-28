06520c16bd186f338685fcaeec0fec772a426f23
Michael Braun <michael.braun@tobit.software>
2017-11-28 13:30:44 +0100

    Add ScrollView-component

---
+++ src/react-chayns-scrollview/component/ScrollView.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import ScrollViewHelper from './ScrollViewHelper';
+
+export default class ScrollView extends React.Component {
+    static propTypes = {
+        children: PropTypes.oneOfType([
+            PropTypes.node,
+            PropTypes.arrayOf(PropTypes.node),
+        ]),
+        style: PropTypes.object,
+        className: PropTypes.string,
+        showScrollbar: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        children: null,
+        style: undefined,
+        className: undefined,
+        showScrollbar: false,
+    };
+
+    componentDidMount() {
+        this.scrollView = new ScrollViewHelper(this.node, {
+            wrapper: this.wrapper,
+            content: this.content,
+            bar: this.bar,
+        });
+    }
+
+    componentDidUpdate() {
+        this.scrollView.refresh();
+    }
+
+    render() {
+        const { style, showScrollbar, className } = this.props;
+
+        const classNames = classnames('cc__scroll-view', {
+            'cc__scroll-view--hide': !showScrollbar,
+            [className]: className,
+        });
+
+        return(
+            <div
+                ref={(ref) => { this.node = ref; }}
+                style={style}
+                className={classNames}
+            >
+                <div
+                    className="cc__scroll-view__wrapper"
+                    ref={(ref) => { this.wrapper = ref; }}
+                >
+                    <div
+                        style={{
+                            maxHeight: (style && style.maxHeight) ? style.maxHeight : undefined,
+                            height: (style && style.height) ? style.height : undefined,
+                            overflowY: 'scroll',
+                        }}
+                        className="cc__scroll-view__content"
+                        ref={(ref) => { this.content = ref; }}
+                    >
+                        {this.props.children}
+                    </div>
+                </div>
+                <div
+                    className="cc__scroll-view__scrollbar"
+                    ref={(ref) => { this.bar = ref; }}
+                />
+            </div>
+        );
+    }
+}
