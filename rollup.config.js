2876437f2cb3f4eb03b2314d24da2c5d23c04285
Michael Braun <michael.braun@tobit.software>
2018-02-22 10:00:04 +0100

    Add rollup-config for building library

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import filesize from "rollup-plugin-filesize";
+import external from 'rollup-plugin-peer-deps-external';
+
+const env = process.env.NODE_ENV;
+const pkg = require("./package.json");
+
+export default {
+    input: "src/index.js",
+    output: {
+        file: {
+            es: pkg.module,
+            cjs: pkg.main
+        }[env],
+        format: env
+    },
+    external: ["react", "classnames", "prop-types", "ract-dom", "react-transition-group", "emojione"],
+    plugins: [
+        external(),
+        resolve({
+            extensions: [ '.js', '.jsx' ],
+        }),
+        babel({
+            exclude: "node_modules/**",
+            presets: ['react'],
+            plugins: ["external-helpers"],
+            // if external helpers true then use global babel object
+            externalHelpers: true
+        }),
+        commonjs(),
+        filesize()
+    ]
+};
