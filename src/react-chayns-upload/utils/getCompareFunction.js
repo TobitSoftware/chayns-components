dc8647ed9f0ec59344889da5168e2fce1eba925c
Michael Braun <michael.braun@tobit.software>
2018-03-16 13:18:07 +0100

    Add mime-type checker for audio and video

--- src/react-chayns-upload/utils/getCompareFunction.js
+++ src/react-chayns-upload/utils/getCompareFunction.js
@@ -1,9 +1,13 @@
 export const IMAGE_MIME_TYPES = ['image/png', 'image/jpeg', 'image/gif', 'image/bmp', 'image/webp', 'image/x-icon', 'image/*'];
+export const AUDIO_MIME_TYPES = ['audio/mp3', 'audio/mpeg3', 'audio/x-mpeg-3', 'audio/midi', 'audio/mpeg', 'audio/webm', 'audio/ogg', 'audio/wav', 'audio/x-mpeg', 'audio/x-wav', 'audio/*'];
+export const VIDEO_MIME_TYPES = ['video/mp4', 'video/webm', 'video/ogg', 'video/x-flv', 'application/x-mpegURL', 'video/MP2T', 'video/3gpp', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv', 'video/*'];
 
-function isImage(file) {
-    const type = (file.type || '').toLowerCase();
+function isOfType(types) {
+    return (file) => {
+        const type = (file.type || '').toLowerCase();
 
-    return (IMAGE_MIME_TYPES.indexOf(type) !== -1);
+        return (types.indexOf(type) !== -1);
+    };
 }
 
 function isDoc() {
@@ -12,7 +16,15 @@ function isDoc() {
 
 export default function getCompareFunction(type) {
     if (type === 'images') {
-        return isImage;
+        return isOfType(IMAGE_MIME_TYPES);
+    }
+
+    if (type === 'audio') {
+        return isOfType(AUDIO_MIME_TYPES);
+    }
+
+    if (type === 'video') {
+        return isOfType(VIDEO_MIME_TYPES);
     }
 
     return isDoc;
