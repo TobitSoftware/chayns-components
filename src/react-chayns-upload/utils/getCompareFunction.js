6b2448a9d5a9f47ced8efe7afa1aec274d4222d1
Michael Braun <michael.braun@tobit.software>
2018-03-16 13:28:24 +0100

    Add resolving of mime-types from mime-type list for audio and video

--- src/react-chayns-upload/utils/getCompareFunction.js
+++ src/react-chayns-upload/utils/getCompareFunction.js
@@ -1,6 +1,4 @@
-export const IMAGE_MIME_TYPES = ['image/png', 'image/jpeg', 'image/gif', 'image/bmp', 'image/webp', 'image/x-icon', 'image/*'];
-export const AUDIO_MIME_TYPES = ['audio/mp3', 'audio/mpeg3', 'audio/x-mpeg-3', 'audio/midi', 'audio/mpeg', 'audio/webm', 'audio/ogg', 'audio/wav', 'audio/x-mpeg', 'audio/x-wav', 'audio/*'];
-export const VIDEO_MIME_TYPES = ['video/mp4', 'video/webm', 'video/ogg', 'video/x-flv', 'application/x-mpegURL', 'video/MP2T', 'video/3gpp', 'video/quicktime', 'video/x-msvideo', 'video/x-ms-wmv', 'video/*'];
+import getMimeTypes from './getMimeTypes';
 
 function isOfType(types) {
     return (file) => {
@@ -15,16 +13,10 @@ function isDoc() {
 }
 
 export default function getCompareFunction(type) {
-    if (type === 'images') {
-        return isOfType(IMAGE_MIME_TYPES);
-    }
-
-    if (type === 'audio') {
-        return isOfType(AUDIO_MIME_TYPES);
-    }
-
-    if (type === 'video') {
-        return isOfType(VIDEO_MIME_TYPES);
+    if (type === 'image' ||
+        type === 'audio' ||
+        type === 'video') {
+        return isOfType(getMimeTypes(type));
     }
 
     return isDoc;
