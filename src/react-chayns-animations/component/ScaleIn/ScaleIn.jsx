2f5068544c0958957d65297bc3dd3777799d1e56
Michael Braun <michael.braun@tobit.software>
2018-04-10 18:20:56 +0200

    Add animate-decorator Add ScaleIn-animation

---
+++ src/react-chayns-animations/component/ScaleIn/ScaleIn.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import connectToOverlay from '../../decorator/connectToOverlay';
+
+class ScaleIn extends Component {
+    static propTypes = {
+        props: PropTypes.object.isRequired,
+        component: PropTypes.oneOfType([
+            React.PropTypes.string,
+            React.PropTypes.func
+        ]).isRequired,
+        // eslint-disable-next-line react/forbid-prop-types
+        animate: PropTypes.any.isRequired,
+        in: PropTypes.bool.isRequired,
+    };
+
+    componentDidMount() {
+        this.props.animate.setAnimationListener(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.in !== this.props.in) {
+            if (nextProps.in) {
+                this.container.classList.add('cc__animation__scale-in--show');
+            } else {
+                this.container.classList.remove('cc__animation__scale-in--show');
+            }
+        }
+    }
+
+    setWrapperReference(ref) {
+        this.updateClasses(ref);
+    }
+
+    updateClasses(wrapper) {
+        const { left, right } = wrapper.getBoundingClientRect();
+
+        if (left < right) {
+            this.container.classList.add('cc__animation__scale-in--left');
+            this.container.classList.remove('cc__animation__scale-in--right');
+        } else {
+            this.container.classList.remove('cc__animation__scale-in--left');
+            this.container.classList.add('cc__animation__scale-in--right');
+        }
+    }
+
+    render() {
+        const { props, component: BaseComponent } = this.props;
+
+        return (
+            <div
+                ref={(ref) => { this.container = ref; }}
+                className="cc__animation__scale-in"
+            >
+            {/*{props.in && (*/}
+                <BaseComponent
+                    {...props}
+                />
+            {/*)}*/}
+            </div>
+        );
+    }
+}
+
+ScaleIn.duplicate = true;
+ScaleIn.wrapperClassName = 'cc__animate--scale-in';
+
+export default ScaleIn;
---
+++ src/react-chayns-animations/component/animate.jsx
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+import ScaleIn from './ScaleIn/ScaleIn';
+
+const animate = (AnimationComponent, config) => BaseComponent => class AnimateComponent extends Component {
+    static propTypes = {
+        in: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        in: true,
+    };
+
+    componentDidMount() {
+        this.updateRefs();
+    }
+
+    componentDidUpdate() {
+        this.updateRefs();
+    }
+
+    setAnimationListener(animation) {
+        this._animation = animation;
+
+        this.updateRefs();
+    }
+
+    updateRefs() {
+        if (!this._wrapper || !this._animation) {
+            return;
+        }
+
+        if (this._animation.setWrapperReference) {
+            this._animation.setWrapperReference(this._wrapper);
+        }
+    }
+
+    render() {
+        if (AnimationComponent.duplicate) {
+            const classNames = classnames('cc__animate', {
+                [AnimationComponent.wrapperClassName]: AnimationComponent.wrapperClassName
+            });
+
+            return (
+                <div
+                    className={classNames}
+                    ref={(ref) => { this._wrapper = ref; }}
+                >
+                    <AnimationComponent
+                        key="shadow"
+                        component={BaseComponent}
+                        config={config}
+                        in={this.props.in}
+                        props={Object.assign({
+                            animationClone: true,
+                        }, this.props)}
+                        animate={this}
+                    />
+                    <BaseComponent
+                        key="base"
+                        {...this.props}
+                    />
+                </div>
+            );
+        }
+
+        return (
+            <AnimationComponent
+                key="shadow"
+                component={BaseComponent}
+                config={config}
+                in={this.props.in}
+                props={this.props}
+                animate={this}
+            />
+        );
+    }
+};
+
+
+export {
+    ScaleIn as SCALE_IN,
+};
+
+export default animate;
