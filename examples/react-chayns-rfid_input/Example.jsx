8a4b0af5c46bdc2875076287078dabda1946c949
Patrick Löcker <Patrick.Loecker@Tobit.software>
2018-05-07 15:34:54 +0200

    add rfid-input example

---
+++ examples/react-chayns-rfid_input/Example.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { RfidInput } from '../../src/index';
+import '../../src/react-chayns-rfid_input/index.scss';
+import ExampleContainer from '../ExampleContainer';
+
+export default class Example extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rfidInput: '',
+            rfid: '',
+        };
+    }
+
+    onInput = (rfidInput) => {
+        this.setState({ rfidInput });
+    };
+
+    onConfirm = (rfid) => {
+        this.setState({ rfid });
+    };
+
+    render() {
+        return(
+            <ExampleContainer headline="RFID Input">
+                <h3>RFID-Live</h3>
+                <p>{this.state.rfidInput || '-'}</p>
+                <h3>RFID</h3>
+                <p>{this.state.rfid || '-'}</p>
+
+                <RfidInput
+                    onConfirm={this.onConfirm}
+                    onInput={this.onInput}
+                    value={this.state.rfidInput}
+                />
+            </ExampleContainer>
+        );
+    }
+}
