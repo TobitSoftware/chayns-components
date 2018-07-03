184d9d6b4f8faa4446921da74dd1988adf62b432
SHoltkamp <steffen.holtkamp@tobit.software>
2018-07-03 11:12:34 +0200

    VCahnge documentation and examples

--- examples/react-chayns-gallery/Example.jsx
+++ examples/react-chayns-gallery/Example.jsx
@@ -12,31 +12,38 @@ export default class Example extends React.Component {
 
 
     render() {
-      const urls = ['https://tsimg.space/v1/images/f37122dd-2f1d-e811-8381-00155d099e09_mshortedgescale-s500.jpg', 'https://tsimg.space/v1/images/971ec821-6076-e811-80d6-0025905a8161_mshortedgescale-s500.jpg', 'https://tsimg.space/v1/images/e5327caf-3579-e811-80d6-0025905a8161_mshortedgescale-s500.jpg', 'https://tsimg.space/v1/images/3c85833e-2d2c-e811-8381-00155d099e09_mshortedgescale-s500.jpg', 'https://tsimg.space/v1/images/f55c0529-be72-e811-80d6-0025905a8161_mshortedgescale-s500.jpg', 'https://tsimg.space/v1/images/0b7ae926-5e6e-e811-80d6-0025905a8161_mshortedgescale-s500.jpg'];
+      const urls = ['https://tsimg.space/v1/images/6ffbd340-a77b-e811-80d6-0025905a8161.jpg',
+          'https://tsimg.space/v1/images/c9a8d7ad-ee72-e811-80d6-0025905a8161.jpg',
+          'https://tsimg.space/v1/images/416b53f0-ee72-e811-80d6-0025905a8161.jpg',
+          'https://tsimg.space/v1/images/416b53f0-ee72-e811-80d6-0025905a8161.jpg'];
       return(
             <ExampleContainer headline="Gallery">
-                <div style={{height:'500px', width: '100%'}}>
+                <div style={{ marginTop: '30px' }}>
+                    <Gallery urls={[urls[0]]} />
+                </div>
+
+                <div style={{ marginTop: '30px' }}>
+                    <Gallery urls={[urls[0], urls[1]]} />
+                </div>
+
+                <div style={{ marginTop: '30px' }}>
+                    <Gallery urls={[urls[0], urls[1], urls[3]]} />
+                </div>
+
+                <div style={{ marginTop: '30px' }}>
                     <Gallery urls={urls} />
                 </div>
-                <Gallery urls={urls} />
 
-                <div style={{marginTop: '30px', height:'100%', width: '50%'}}>
-                    <Gallery urls={urls} onClick={console.log} />
-                </div>
-
-                <div style={{marginTop: '30px'}}>
-                    <Gallery urls={urls} onlyIcon/>
-                </div>
-
-                <div style={{marginTop: '30px', width: '20%'}}>
+                <div style={{ marginTop: '30px' }}>
                     <Gallery urls={urls} onlyIcon />
                 </div>
 
-                <div style={{marginTop: '30px', height:'500px', width: '100%'}}>
-                    <Gallery urls={[urls[0], urls[1]]} />
-                </div>
-                <div style={{height:'500px', width: '100%'}}>
-                    <Gallery urls={[urls[0]]} />
+                <div style={{ marginTop: '30px' }}>
+                    <Gallery
+                        urls={urls}
+                        onlyIcon
+                        onClick={console.log}
+                    />
                 </div>
             </ExampleContainer>
         );
--- src/react-chayns-gallery/README.md
+++ src/react-chayns-gallery/README.md
@@ -17,16 +17,21 @@ import 'chayns-components/lib/react-chayns-gallery/index.css';
 You can use the input like this:
 ```jsx
 <Gallery
-    urls={['https://tsimg.space/v1/images/6ffbd340-a77b-e811-80d6-0025905a8161.jpg', 'https://tsimg.space/v1/images/c9a8d7ad-ee72-e811-80d6-0025905a8161.jpg','https://tsimg.space/v1/images/416b53f0-ee72-e811-80d6-0025905a8161.jpg'https://tsimg.space/v1/images/416b53f0-ee72-e811-80d6-0025905a8161.jpg]} 
+    urls={['https://tsimg.space/v1/images/6ffbd340-a77b-e811-80d6-0025905a8161.jpg',
+        'https://tsimg.space/v1/images/c9a8d7ad-ee72-e811-80d6-0025905a8161.jpg',
+        'https://tsimg.space/v1/images/416b53f0-ee72-e811-80d6-0025905a8161.jpg',
+        'https://tsimg.space/v1/images/416b53f0-ee72-e811-80d6-0025905a8161.jpg']} 
     onlyIcon
 />
 ```
 
 
 ## Props
-The following properties can be set on the EmojiInput-Component
+The following properties can be set on the Gallery-Component
 
 | **Property** | **Description**                                      | **Type** | **Default Value** | **Required** |
 | ------------ | ---------------------------------------------------- | -------- | ----------------- | ------------ |
 | urls         | The Images which should be displayed                 | array    |                   | true         |
-| onlyIcon     | Show only an icon, if there is more then three images| bool     | false           | true         |
+| onlyIcon     | Show only an icon, if there is more then three images| bool     | false             |              |
+| onClick      | Called when clicked on a image                       | func     | chayns.openImage  |              |
+
