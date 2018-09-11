68e454f032d60b9d6be4b91b08b69c6301fb577e
Jonas Gossens <jg@tobit.software>
2018-09-11 17:39:02 +0200

    Add imageService for scaling images

--- src/react-chayns-gallery/component/Gallery.jsx
+++ src/react-chayns-gallery/component/Gallery.jsx
@@ -24,12 +24,33 @@ export default class Gallery extends Component {
     constructor() {
         super();
         this.openImage = this.openGallery.bind(this);
+        this.scaledImage = this.getScaledImageUrl.bind(this);
+        this.bigImages = this.getBigImages.bind(this);
+    }
+
+    getScaledImageUrl(url, shortEdgeSize) {
+        if (url.indexOf('tsimg.space') >= 0 && (url.indexOf('jpg') >= 0 || url.indexOf('jpeg') >= 0 || url.indexOf('png') >= 0)) {
+            return url
+                .replace('.jpg', `_s${shortEdgeSize}-mshortedgescale.jpg`)
+                .replace('.jpeg', `_s${shortEdgeSize}-mshortedgescale.jpeg`)
+                .replace('.png', `_s${shortEdgeSize}-mshortedgescale.png`);
+        }
+        return url;
+    }
+
+    getBigImages(urls) {
+        const bigUrls = [];
+        urls.forEach((url) => {
+            // eslint-disable-next-line no-restricted-globals
+            bigUrls.push(this.scaledImage(url, screen.availHeight * 0.9));
+        });
+        return bigUrls;
     }
 
     openGallery(start) {
         const { onClick, urls, deleteMode } = this.props;
-        if(!deleteMode) {
-            onClick(urls, start);
+        if (!deleteMode) {
+            onClick(this.bigImages(urls), start);
         }
     }
 
@@ -41,34 +62,43 @@ export default class Gallery extends Component {
             onDelete,
             deleteMode,
         } = this.props;
-        const style = {};
-        if (width) {
-            style.width = `${width}px`;
-        }
+        let styleHeight;
         if (!deleteMode) {
-            if(height) {
-                style.height = `${height}px`;
-            } else if(chayns.env.mobile) {
-                style.height = '256px';
+            if (height) {
+                styleHeight = height;
+            } else if (chayns.env.mobile) {
+                styleHeight = 256;
             } else {
-                style.height = '428px';
+                styleHeight = 428;
             }
         }
+        const numberOfImages = urls.length;
         return (
             <div className="chayns-gallery" style={{ height: `${styleHeight}px`, width: width ? `${width}px` : undefined }}>
                 <div className={classNames('gallery-grid', { 'delete-mode': deleteMode })}>
                     {
                         urls.map((url, index) => {
                             if (index <= 3 || deleteMode) {
+                                let scaleHeight = 150;
+                                if(!deleteMode) {
+                                    if (index <= 2 && numberOfImages <= 2) {
+                                        scaleHeight = styleHeight;
+                                    } else if (index === 0) {
+                                        scaleHeight = parseInt((styleHeight * 2) / 3, 10);
+                                    } else if (index < 4) {
+                                        scaleHeight = parseInt(styleHeight / 3, 10);
+                                    }
+                                }
+
                                 return (
                                     <ImageContainer
                                         key={url}
-                                        url={url}
+                                        url={this.scaledImage(url, scaleHeight)}
                                         index={index}
                                         openImage={this.openImage}
                                         onDelete={onDelete}
                                         deleteMode={deleteMode}
-                                        moreImages={(index === 3 && !deleteMode) ? urls.length - 1 - index : 0}
+                                        moreImages={(index === 3 && !deleteMode) ? numberOfImages - 1 - index : 0}
                                     />
                                 );
                             }
