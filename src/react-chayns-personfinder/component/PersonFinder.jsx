58d8de89e9cabcc3af7b860ce212e57a4495f581
Lucas Engelke <L-Engelke@web.de>
2018-09-17 10:47:18 +0200

    Implement find site

--- src/react-chayns-personfinder/component/PersonFinder.jsx
+++ src/react-chayns-personfinder/component/PersonFinder.jsx
@@ -1,44 +1,97 @@
+/* eslint-disable no-return-assign */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import isDescendant from '../../utils/isDescendant';
 
 export default class PersonFinder extends Component {
     static propTypes = {
         className: PropTypes.string,
         placeholder: PropTypes.string,
         defaultValue: PropTypes.string,
-        onChange: PropTypes.func
+        onChange: PropTypes.func,
+        showPersons: PropTypes.bool,
+        showSites: PropTypes.bool,
     };
 
     static defaultProps = {
         className: null,
         placeholder: '',
         defaultValue: null,
-        onChange: null
+        onChange: null,
+        showPersons: true,
+        showSites: false
     };
 
     state = {
-        results: []
+        persons: [],
+        sites: [],
+        showPopup: false,
+        value: ''
     };
+
+    componentDidMount() {
+        document.addEventListener('click', this.handleBlur);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleBlur);
+    }
 
     handleOnChange = (event) => {
         const { value } = event.target;
+
+        this.setState({
+            value
+        });
 
         if (value.length < 3) {
             return;
         }
 
-        chayns.findPerson(value)
-            .then((data) => {
-                this.setState({
-                    results: data.Value
-                });
+        const { showPersons, showSites } = this.props;
+        Promise.all([
+            showPersons ? chayns.findPerson(value) : Promise.resolve({ Value: [] }),
+            showSites ? chayns.findSite(value) : Promise.resolve({ Value: [] })
+        ]).then(([persons, sites]) => {
+            this.setState({
+                persons: persons.Value || [],
+                sites: sites.Value || []
             });
+        });
+    };
+
+    handleFocus = () => {
+        this.setState({
+            showPopup: true
+        });
+    };
+
+    handleBlur = (e) => {
+        if (isDescendant(this.ref, e.target) || e.target === this.input) {
+            return;
+        }
+
+        this.setState({
+            showPopup: false
+        });
+    };
+
+    handleItemClick = (r) => {
+        this.setState({
+            showPopup: false,
+            value: r.name || r.appstoreName
+        });
+
+        const { onChange } = this.props;
+        if (onChange) {
+            onChange(r);
+        }
     };
 
     render() {
-        const { className, ...props } = this.props;
-        const { results } = this.state;
+        const { className, showPersons, showSites, ...props } = this.props;
+        const { persons, sites, showPopup, value } = this.state;
 
         const classNames = classnames('input', className);
 
@@ -47,26 +100,55 @@ export default class PersonFinder extends Component {
                 <input
                     type="text"
                     className={classNames}
+                    value={value}
                     {...props}
+                    ref={ref => this.input = ref}
                     onChange={this.handleOnChange}
+                    onFocus={this.handleFocus}
                 />
-                <div className="person-finder__results scrollbar">
-                    {results.map(r => (
-                        <div className="result">
-                            <div className="img">
-                                <img src={`https://sub60.tobit.com/u/${r.personId}?size=40`} alt="img"/>
-                            </div>
-                            <div className="text">
-                                <div className="name">
-                                    {r.name}
+                {showPopup && (persons.length > 0 || sites.length > 0) ? (
+                    <div className="person-finder__results scrollbar" ref={ref => this.ref = ref}>
+                        {showPersons && persons.map(r => (
+                            <div key={r.personId} className="result" onClick={() => this.handleItemClick(r)}>
+                                <div className="img">
+                                    <img
+                                        src={`https://sub60.tobit.com/u/${r.personId}?size=40`}
+                                        onError={(e) => {
+                                            e.target.onError = () => {};
+                                                e.target.src = '//sub60.tobit.com/Content/unknown_user.png';
+                                        }}
+                                    />
                                 </div>
-                                <div className="person-id">
-                                    {`chayns® ID: ${r.personId}`}
+                                <div className="text">
+                                    <div className="name">
+                                        {r.name}
+                                    </div>
+                                    <div className="person-id">
+                                        {`chayns® ID: ${r.personId}`}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
+                        ))}
+                        {showSites && sites.map(r => (
+                            <div key={r.siteId} className="result" onClick={() => this.handleItemClick(r)}>
+                                <div className="img">
+                                    <img
+                                        src={`https://sub60.tobit.com/l/${r.siteId}?size=40`}
+                                        onError={(e) => {
+                                            e.target.onError = () => {};
+                                            e.target.src = `//graph.facebook.com/${r.facebookId}/picture`;
+                                        }}
+                                    />
+                                </div>
+                                <div className="text">
+                                    <div className="name">
+                                        {r.appstoreName}
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : false}
             </div>
         );
     }
---
+++ src/utils/isDescendant.js
@@ -0,0 +1,10 @@
+export default function isDescendant(parent, child) {
+    let node = child.parentNode;
+    while (node) {
+        if (node === parent) {
+            return true;
+        }
+        node = node.parentNode;
+    }
+    return false;
+}
