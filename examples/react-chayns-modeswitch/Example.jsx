81154c155e59019c0741a67d62030b6eb1ce18f5
Jonas Gossens <jg@tobit.software>
2018-09-18 16:25:11 +0200

    Update Modeswitch (WIP)

--- examples/react-chayns-modeswitch/Example.jsx
+++ examples/react-chayns-modeswitch/Example.jsx
@@ -1,78 +1,108 @@
 import React, { Component } from 'react';
+import { ModeSwitch } from '../../src/index';
+// import { ModeSwitchHelper, ModeSwitch, Mode } from '../../src/index';
 
-import { ModeSwitchHelper, ModeSwitch, Mode } from '../../src/index';
-
-import ExampleDecorator from './ExampleDecorator';
+// import ExampleDecorator from './ExampleDecorator';
 import ExampleContainer from '../ExampleContainer';
+import Button from '../../src/react-chayns-button/component/Button';
 
 export default class Example extends Component {
     constructor() {
         super();
-
-        console.log(ModeSwitchHelper.isUserInGroup(1), ModeSwitchHelper.isUserInGroup(2), ModeSwitchHelper.isUserInGroup(3), ModeSwitchHelper.isChaynsManager());
-
-        window.setTimeout(() => {
-            ModeSwitch.init({
-                groups: [{
-                    id: 1,
-                    uacIds: [1, 34542],
-                    name: 'chayns® Manager'
-                }, {
-                    id: 2,
-                    name: 'Employee'
-                }],
-                save: true,
-                onChange: (data) => {
-                    console.log('mode', data);
-                }
-            });
-        }, 1000);
+        this.state = { mode: null };
     }
 
-    getModeSwitchStatus = () => {
-        window.chayns.dialog.alert(JSON.stringify(ModeSwitch.getCurrentMode(), null, 3));
-    };
+    // constructor() {
+    //     super();
+    //
+    //     console.log('ModeSwitch Example', ModeSwitchHelper.isUserInGroup(1), ModeSwitchHelper.isUserInGroup(2), ModeSwitchHelper.isUserInGroup(3), ModeSwitchHelper.isChaynsManager());
+    //
+    //     window.setTimeout(() => {
+    //         ModeSwitch.init({
+    //             groups: [{
+    //                 id: 1,
+    //                 uacIds: [1, 34542],
+    //                 name: 'chayns® Manager'
+    //             }, {
+    //                 id: 2,
+    //                 name: 'Employee'
+    //             }],
+    //             save: true,
+    //             onChange: (data) => {
+    //                 console.log('mode', data);
+    //             }
+    //         });
+    //     }, 1000);
+    // }
+    //
+    // getModeSwitchStatus = () => {
+    //     window.chayns.dialog.alert(JSON.stringify(ModeSwitch.getCurrentMode(), null, 3));
+    // };
 
     render() {
-        return(
+        const { mode } = this.state;
+        return (
             <ExampleContainer headline="ModeSwitch">
-                <Mode mode={0}>
-                    <div>
-                        Hello world
-                    </div>
-                </Mode>
+                <ModeSwitch onChange={(m) => {
+                    console.log(m);
+                    this.setState({ mode: m });
+                }}
+                />
+                {mode ? <p>{`Mode: ${mode.id} ${mode.name} UacId: ${mode.uacId}`}</p> : null}
+                <Button onClick={() => {
+                    console.log(ModeSwitch.getCurrentMode());
+                }}
+                >
 
-                <Mode mode={1}>
-                    <div>
-                        Administration
-                    </div>
 
-                    <div>
-                        Test
-                    </div>
-
-                    <div>
-                        Hi
-                    </div>
-                </Mode>
-
-                <Mode modes={[0, 1]}>
-                    <div>
-                        <button onClick={this.getModeSwitchStatus}>ModeSwitch Status</button>
-                    </div>
-                </Mode>
-
-                <Mode modes={[2]}>
-                    <div>
-                        Mitarbeiter
-                    </div>
-                </Mode>
-
-                <div className="button" onClick={ModeSwitch.show} >Show ModeSwitch</div>
-                <div className="button" onClick={ModeSwitch.hide} >Hide ModeSwitch</div>
-
-                <ExampleDecorator test="1" hallo="hi" />
+                    console.log(ModeSwitch.getCurrentMode());
+                </Button>
             </ExampleContainer>
         );
+        // return(
+        //     <ExampleContainer headline="ModeSwitch">
+        //         <Mode mode={0}>
+        //             <div>
+        //
+        //                 Hello world
+        //             </div>
+        //         </Mode>
+        //
+        //         <Mode mode={1}>
+        //             <div>
+        //
+        //                 Administration
+        //             </div>
+        //
+        //             <div>
+        //
+        //                 Test
+        //             </div>
+        //
+        //             <div>
+        //
+        //                 Hi
+        //             </div>
+        //         </Mode>
+        //
+        //         <Mode modes={[0, 1]}>
+        //             <div>
+        //                 <button onClick={this.getModeSwitchStatus}>ModeSwitch Status</button>
+        //             </div>
+        //         </Mode>
+        //
+        //         <Mode modes={[2]}>
+        //             <div>
+        //
+        //                 Mitarbeiter
+        //             </div>
+        //         </Mode>
+        //
+        //         <div className="button" onClick={ModeSwitch.show} >Show ModeSwitch</div>
+        //         <div className="button" onClick={ModeSwitch.hide} >Hide ModeSwitch</div>
+        //
+        //         <ExampleDecorator test="1" hallo="hi" />
+        //     </ExampleContainer>
+        // );
     }
 }
--- src/react-chayns-modeswitch/component/ModeSwitch.jsx
+++ src/react-chayns-modeswitch/component/ModeSwitch.jsx
@@ -1,70 +1,142 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-import ModeSwitchHelper from './ModeSwitchHelper';
+import './modeSwitch.scss';
 
-class ModeSwitch extends Component {
-    static init(config) {
-        if(!config) return false;
+import RadioButton from '../../react-chayns-radiobutton/component/RadioButton';
 
-        ModeSwitchHelper.init(config);
+let globalState = { modes: [], activeModeId: 0 };
 
-        return true;
-    }
-
-    static isUserInGroup(groupId) {
-        if(!window.chayns.env.user.isAuthenticated) return false;
-
-        for (let j = 0, userGroups = window.chayns.env.user.groups, k = userGroups.length; j < k; j += 1) {
-            if(groupId === userGroups[j].id) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
+export default class ModeSwitch extends Component {
     static propTypes = {
-        groups: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number, PropTypes.object])),
+        modes: PropTypes.arrayOf(PropTypes.oneOf([
+            PropTypes.shape({ id: PropTypes.number, name: PropTypes.string }),
+            PropTypes.shape({ id: PropTypes.number, name: PropTypes.string, uacId: PropTypes.number })
+        ])),
         save: PropTypes.bool,
         onChange: PropTypes.func,
         defaultMode: PropTypes.number
     };
 
     static defaultProps = {
-        groups: null,
+        modes: null,
         save: false,
         onChange: null,
         defaultMode: null
     };
 
-    componentDidMount() {
-        const {
-            groups,
-            save,
-            onChange,
-            defaultMode,
-        } = this.props;
+    static getCurrentMode() {
+        const { modes, activeModeId } = globalState;
+        return modes.find(mode => mode.id === activeModeId || null);
+    }
 
-        ModeSwitchHelper.init({
-            groups,
-            save,
-            onChange,
-            defaultMode,
+    constructor(props) {
+        super(props);
+
+        this.toggleModeSwitch = this.toggleModeSwitch.bind(this);
+        this.switchMode = this.switchMode.bind(this);
+
+        window.chayns.ready.then(() => {
+            globalState = { modes: this.setModes(props.modes), activeModeId: props.defaultMode || 0 };
+            this.state = { ...globalState, open: false };
+
+            const isAdminMode = chayns.addAdminSwitchListener(this.switchMode);
+            if (!props.save && !props.defaultMode) {
+                if (isAdminMode) {
+                    globalState.activeModeId = 1;
+                    this.state.activeModeId = 1;
+                } else {
+                    globalState.activeModeId = 0;
+                    this.state.activeModeId = 0;
+                }
+            }
+            const { modes, activeModeId } = this.state;
+            props.onChange(modes.find(mode => mode.id === activeModeId));
         });
     }
 
+    componentWillReceiveProps(nextProps) {
+        const { modes } = this.props;
+        if (nextProps.modes !== modes) {
+            this.setState({ modes: this.setModes(nextProps.modes) });
+        }
+    }
+
+    setModes(modes) {
+        let newModes = modes || [];
+        const user = newModes.filter(mode => mode.id === 0);
+        const admin = newModes.filter(mode => mode.id === 1);
+        if (admin.length === 0) {
+            newModes.unshift({ id: 1, name: 'chayns® Manager', uacId: 1 });
+        }
+        if (user.length === 0) {
+            newModes.unshift({ id: 0, name: window.chayns.env.user.name, uacId: 0 });
+        }
+        newModes = newModes.filter(mode => !mode.uacId || this.isUserInGroup(mode.uacId));
+        return newModes;
+    }
+
+    isUserInGroup(uacId) {
+        return window.chayns.env.user.groups.indexOf(group => group.id === uacId) >= 0;
+    }
+
+    switchMode(id) {
+        const { onChange } = this.props;
+        const { modes } = this.state;
+        if (id.mode !== undefined) {
+            this.setState({ activeModeId: id.mode });
+            globalState.activeModeId = id.mode;
+            if (onChange) {
+                onChange(modes.find(mode => mode.id === id.mode));
+            }
+        } else {
+            if (id === 0) {
+                chayns.deactivateAdminMode();
+            } else if (id === 1) {
+                chayns.activateAdminMode();
+            } else if (onChange) {
+                onChange(modes.find(mode => mode.id === id));
+            }
+            this.setState({ activeModeId: id, open: false });
+            globalState.activeModeId = id;
+        }
+    }
+
+    toggleModeSwitch() {
+        const { open } = this.state;
+        this.setState({ open: !open });
+    }
+
     render() {
+        if (this.state) {
+            const { modes, open, activeModeId } = this.state;
+            return (
+                <div className={classNames('cc__modeswitch', { 'cc__modeswitch--open': open })}>
+                    <div className="cc__modeswitch__content">
+                        <h2>Diese Seite verwenden als:</h2>
+                        {
+                            modes.map(mode => (
+                                <div className="grid__item col-1-2-desktop col-1-1-mobile">
+                                    <RadioButton
+                                        name="modeSwitchRadioButtons"
+                                        value={mode.id}
+                                        onChange={this.switchMode}
+                                        checked={mode.id === activeModeId}
+                                    >
+                                        {mode.name}
+                                    </RadioButton>
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div
+                        className={classNames('cc__modeswitch__trigger', { 'cc__modeswitch__trigger--red': activeModeId !== 0 })}
+                        onClick={this.toggleModeSwitch}
+                    />
+                </div>
+            );
+        }
         return null;
     }
 }
-
-ModeSwitch.getCurrentMode = ModeSwitchHelper.getCurrentMode;
-ModeSwitch.addChangeListener = ModeSwitchHelper.onChange;
-ModeSwitch.removeChangeListener = ModeSwitchHelper.unregisterOnChange;
-ModeSwitch.show = ModeSwitchHelper.show;
-ModeSwitch.hide = ModeSwitchHelper.hide;
-ModeSwitch.isUserInGroup = ModeSwitchHelper.isUserInGroup;
-ModeSwitch.isChaynsManager = ModeSwitchHelper.isChaynsManager;
-
-export default ModeSwitch;
