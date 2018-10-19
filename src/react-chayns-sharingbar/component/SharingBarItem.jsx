4f9680fdfe6e30d850e52052b14aceaaecf5c48a
Jonas Gossens <jg@tobit.software>
2018-10-19 15:38:01 +0200

    Add tooltip to sharingBar

--- src/react-chayns-sharingbar/component/SharingBarItem.jsx
+++ src/react-chayns-sharingbar/component/SharingBarItem.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import share from './sharingActions';
 import Icon from '../../react-chayns-icon/component/Icon';
 import Button from '../../react-chayns-button/component/Button';
+import Tooltip from '../../react-chayns-tooltip/component/Tooltip';
 
 export default class SharingBarItem extends Component {
-    static propTypes ={
+    static propTypes = {
         icon: PropTypes.oneOfType([PropTypes.string.isRequired, PropTypes.object.isRequired]).isRequired,
         name: PropTypes.string.isRequired,
         provider: PropTypes.object.isRequired,
@@ -19,8 +20,27 @@ export default class SharingBarItem extends Component {
     };
 
     render() {
-        const { name, icon } = this.props;
-
+        const { name, icon, provider } = this.props;
+        if (provider.action === 0) {
+            return (
+                <Tooltip content={{ text: 'Kopiert.' }} ref={ref => this.tooltip = ref} removeIcon>
+                    <Button
+                        className="sharing-bar__item"
+                        title={name}
+                        onClick={() => {
+                            this.onClick();
+                            this.tooltip.show();
+                            clearTimeout(this.timeout)
+                            this.timeout=setTimeout(()=>{
+                                this.tooltip.hide();
+                            }, 3000);
+                        }}
+                    >
+                        <Icon icon={icon}/>
+                    </Button>
+                </Tooltip>
+            );
+        }
         return (
             <Button
                 className="sharing-bar__item"
