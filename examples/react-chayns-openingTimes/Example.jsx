6b71c8fd940f13aa3c62c680067b2c6703165c15
Unknown <ruben.ollesch@protonmail.com>
2018-12-19 13:43:41 +0100

    Instead of exposing onAdd, onRemove,... , just use onChange

--- examples/react-chayns-openingTimes/Example.jsx
+++ examples/react-chayns-openingTimes/Example.jsx
@@ -36,74 +36,20 @@ export default class Example extends PureComponent {
                 }
             ]
         };
-
-        this.onAdd = this.onAdd.bind(this);
-        this.onRemove = this.onRemove.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.onDayActivation = this.onDayActivation.bind(this);
-    }
-
-    onAdd(day, start, end) {
-        console.log('add', day, start, end);
-        this.setState({
-            times: [
-                ...this.state.times,
-                {
-                    weekDay: day,
-                    start,
-                    end
-                }
-            ]
-        });
-    }
-
-    onRemove(day, span) {
-        console.log('remove', day, span);
-        // eslint-disable-next-line no-nested-ternary
-        const elm = this.state.times.filter(t => t.weekDay === day).sort((a, b) => (a.start < b.start ? -1 : a.start > b.start ? 1 : 0))[span];
-        this.setState({
-            times: this.state.times.filter(t => !(t.weekDay === elm.weekDay && t.start === elm.start && t.end === elm.end))
-        });
-    }
-
-    onChange(day, span, start, end) {
-        console.log('change', day, span, start, end);
-        // eslint-disable-next-line no-nested-ternary
-        const elm = this.state.times.filter(t => t.weekDay === day).sort((a, b) => (a.start < b.start ? -1 : a.start > b.start ? 1 : 0))[span];
-        const times = this.state.times.slice();
-        times.forEach((t) => {
-            if (t.weekDay === elm.weekDay && t.start === elm.start && t.end === elm.end) {
-                t.start = start;
-                t.end = end;
-            }
-        });
-        this.setState({
-            times
-        });
-    }
-
-    onDayActivation(day, status) {
-        console.log('dayActivation', day, status);
-        const times = this.state.times.slice();
-        times.forEach((t) => {
-            if (t.weekDay === day) {
-                t.disabled = !status;
-            }
-        });
-        this.setState({
-            times
-        });
     }
 
     render() {
+        const { times } = this.state;
         return (
             <ExampleContainer headline="OpeningTimes">
                 <OpeningTimes
-                    times={this.state.times}
-                    onAdd={this.onAdd}
-                    onRemove={this.onRemove}
-                    onChange={this.onChange}
-                    onDayActivation={this.onDayActivation}
+                    times={times}
+                    onChange={(newTimes) => {
+                        console.log(times, newTimes);
+                        this.setState({
+                            times: newTimes
+                        });
+                    }}
                 />
             </ExampleContainer>
         );
--- src/react-chayns-openingtimes/component/OpeningTimes.jsx
+++ src/react-chayns-openingtimes/component/OpeningTimes.jsx
@@ -1,58 +1,133 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Day from './Day';
 import './styles.scss';
 
-const weekdays = [
-    'Montag',
-    'Dienstag',
-    'Mittwoch',
-    'Donnerstag',
-    'Freitag',
-    'Samstag',
-    'Sonntag',
-];
 
-const OpeningTimes = ({ times, onDayActivation, onAdd, onRemove, onChange }) => (
-    <div style={{
-        margin: '10px 25px 15px 25px'
-    }}
-    >
-        {
-            weekdays.map((day, index) => (
-                <Day
-                    // eslint-disable-next-line react/no-array-index-key
-                    key={index}
-                    weekday={{
-                        name: day,
-                        number: index
-                    }}
-                    times={times.filter(t => t.weekDay === index)}
-                    onDayActivation={onDayActivation}
-                    onAdd={onAdd}
-                    onRemove={onRemove}
-                    onChange={onChange}
-                />
-            ))
+class OpeningTimes extends Component {
+    static propTypes = {
+        times: PropTypes.arrayOf(PropTypes.shape({
+            weekDay: PropTypes.number.isRequired,
+            start: PropTypes.string.isRequired,
+            end: PropTypes.string.isRequired,
+            disabled: PropTypes.bool
+        })).isRequired,
+        onChange: PropTypes.func
+    };
+
+    static defaultProps = {
+        onChange: null
+    }
+
+    static weekdays = [
+        'Montag',
+        'Dienstag',
+        'Mittwoch',
+        'Donnerstag',
+        'Freitag',
+        'Samstag',
+        'Sonntag',
+    ]
+
+    constructor(props) {
+        super(props);
+
+        this.onAdd = this.onAdd.bind(this);
+        this.onRemove = this.onRemove.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onDayActivation = this.onDayActivation.bind(this);
+    }
+
+    onAdd(weekDay, start, end) {
+        console.log('add', weekDay, start, end);
+        const { times, onChange } = this.props;
+        if (onChange) {
+            onChange(times.concat({
+                weekDay,
+                start,
+                end,
+                disabled: false
+            }));
         }
-    </div>
-);
+    }
 
-OpeningTimes.propTypes = {
-    times: PropTypes.arrayOf(PropTypes.shape({
-        start: PropTypes.string.isRequired,
-        end: PropTypes.string.isRequired
-    })).isRequired,
-    onDayActivation: PropTypes.func.isRequired,
-    onAdd: PropTypes.func,
-    onRemove: PropTypes.func,
-    onChange: PropTypes.func
-};
+    onRemove(day, span) {
+        console.log('remove', day, span);
+        const { times, onChange } = this.props;
+        if (onChange) {
+            const timesOfDay = times.filter(t => t.weekDay === day);
+            // eslint-disable-next-line no-nested-ternary
+            const elm = timesOfDay.sort((a, b) => (a.start < b.start ? -1 : a.start > b.start ? 1 : 0))[span];
+            const newTimes = times.slice().filter(t => !(t.weekDay === elm.weekDay && t.start === elm.start && t.end === elm.end));
 
-OpeningTimes.defaultProps = {
-    onAdd: null,
-    onRemove: null,
-    onChange: null
-};
+            onChange(newTimes);
+        }
+    }
+
+    onChange(day, span, start, end) {
+        console.log('change', day, span, start, end);
+        // eslint-disable-next-line no-nested-ternary
+        const { times, onChange } = this.props;
+        if (onChange) {
+            const timesOfDay = times.filter(t => t.weekDay === day);
+            // eslint-disable-next-line no-nested-ternary
+            const elm = timesOfDay.sort((a, b) => (a.start < b.start ? -1 : a.start > b.start ? 1 : 0))[span];
+            const newTimes = times.slice();
+            for (let i = 0; i < newTimes.length; i += 1) {
+                const t = newTimes[i];
+                if (t.weekDay === elm.weekDay && t.start === elm.start && t.end === elm.end) {
+                    t.start = start;
+                    t.end = end;
+                    break;
+                }
+            }
+            onChange(newTimes);
+        }
+    }
+
+    onDayActivation(day, status) {
+        console.log('dayActivation', day, status);
+        const { times, onChange } = this.props;
+        if (onChange) {
+            const newTimes = times.slice();
+            newTimes.forEach((t) => {
+                if (t.weekDay === day) {
+                    // eslint-disable-next-line no-param-reassign
+                    t.disabled = !status;
+                }
+            });
+            onChange(newTimes);
+        }
+    }
+
+    render() {
+        const { times } = this.props;
+        return (
+            <div
+                style={{
+                    margin: '10px 25px 15px 25px'
+                }}
+            >
+                {
+                    OpeningTimes.weekdays.map((day, index) => (
+                        <Day
+                            // eslint-disable-next-line react/no-array-index-key
+                            key={index}
+                            weekday={{
+                                name: day,
+                                number: index
+                            }}
+                            times={times.filter(t => t.weekDay === index)}
+                            onDayActivation={this.onDayActivation}
+                            onAdd={this.onAdd}
+                            onRemove={this.onRemove}
+                            onChange={this.onChange}
+                        />
+                    ))
+                }
+            </div>
+        );
+    }
+}
 
 export default OpeningTimes;
