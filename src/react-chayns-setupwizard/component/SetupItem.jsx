50006299a0d82fe996140cd7d2f3a9b600b3f6d0
Michael Braun <michael.braun@tobit.software>
2019-02-11 17:14:00 +0100

    Ignore required, unused prop

--- src/react-chayns-setupwizard/component/SetupItem.jsx
+++ src/react-chayns-setupwizard/component/SetupItem.jsx
@@ -41,7 +41,7 @@ SetupItem.propTypes = {
     onClick: PropTypes.func,
     contentStyle: PropTypes.object,
     children: PropTypes.element,
-    required: PropTypes.bool
+    required: PropTypes.bool,  /* eslint-disable-line react/no-unused-prop-types */
 };
 
 SetupItem.defaultProps = {
