3ec0212d4328249c4a90e2051f6050494f9a7ebc
Michael Braun <michael.braun@tobit.software>
2019-02-12 15:24:27 +0100

    :sparkles: Add ExpandableListHeader

--- examples/react-chayns-list/Example.jsx
+++ examples/react-chayns-list/Example.jsx
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { ExpandableList } from '../../src/index';
 import ExampleContainer from '../ExampleContainer';
 import ExpandableListItem from '../../src/react-chayns-list/component/ExpandableList/ExpandableListItem';
+import ExpandableListHeader from '../../src/react-chayns-list/component/ListItem/ExpandableListHeader';
+
+import './index.scss';
 
 export default class Example extends Component {
     render() {
@@ -20,6 +23,13 @@ export default class Example extends Component {
                         {'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'}
                     </ExpandableListItem>
                 </ExpandableList>
+                <div className="list-item--expanded">
+                    <ExpandableListHeader
+                        image="https://chayns.tobit.com/storage/75508-06235/Images/icon-72.png"
+                        title="ListItem (als Accordion, mit Bild) sadashd sda dgsagdasgd hgasdjajsdkgajhgd as dgashgdasg jkgdsgadjhsgajhd gdgdjksagdjkagdsa sjkd agjhgk gakdsa"
+                        subtitle="Beschreibung"
+                    />
+                </div>
             </ExampleContainer>
         );
     }
---
+++ src/react-chayns-list/component/ListItem/ExpandableListHeader.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ExpandableListHeader extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string,
+        image: PropTypes.string,
+    };
+
+    static defaultProps = {
+        image: null,
+        subtitle: null,
+    };
+
+    render() {
+        const {
+            title,
+            subtitle,
+            image,
+        } = this.props;
+
+        return (
+            <div className="list-item__header">
+                <div
+                    className="list-item__image"
+                    style={{
+                        backgroundImage: `url(${image})`
+                    }}
+                />
+                <div className="list-item__titles">
+                    <div className="list-item__title ellipsis">
+                        {title}
+                    </div>
+                    <div className="list-item__subtitle ellipsis">
+                        {subtitle}
+                    </div>
+                </div>
+                <div className="list-item__spacer" />
+                <div className="list-item__right">Test</div>
+            </div>
+        );
+    }
+}
