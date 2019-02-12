b27022b8cb461882af6aee9710295340d012505c
Michael Braun <michael.braun@tobit.software>
2019-02-12 13:47:02 +0100

    :rewind: Uncomment whyDidYouUpdate inside examples

--- examples/index.jsx
+++ examples/index.jsx
@@ -32,7 +32,7 @@ import './react-chayns-tooltip/index';
 import './react-chayns-upload/index';
 import React from 'react';
 
-// if (process.env.NODE_ENV !== 'production') {
-//     const { whyDidYouUpdate } = require('why-did-you-update');
-//     whyDidYouUpdate(React);
-// }
+if (process.env.NODE_ENV !== 'production') {
+    const { whyDidYouUpdate } = require('why-did-you-update');
+    whyDidYouUpdate(React);
+}
