e3b0666f705c0364a229a76774c4c0b9adb57cd6
Michael Braun <michael.braun@tobit.software>
2019-02-12 17:13:43 +0100

    Add simple List and ListItem

---
+++ src/react-chayns-list/component/List.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+
+const List = ({ className, children }) => (
+    <div className={classnames('list', className)}>
+        {children}
+    </div>
+);
+
+List.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([
+        PropTypes.node,
+        PropTypes.arrayOf(PropTypes.node)
+    ]),
+};
+
+List.defaultProps = {
+    className: null,
+    children: null,
+};
+
+export default List;
