7e205afb745faf6d8712ade44fa04d475a08f8fc
Michael Braun <michael.braun@tobit.software>
2019-02-18 16:23:56 +0100

    Fix InputBox-example

--- examples/react-chayns-input_box/Example.jsx
+++ examples/react-chayns-input_box/Example.jsx
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 
 import { Input } from '../../src/index';
-import ExampleContainer from '../ExampleContainer';
 import InputBox from '../../src/react-chayns-input_box/component/InputBox';
 
 export default class InputBoxExample extends PureComponent {
@@ -25,17 +24,15 @@ export default class InputBoxExample extends PureComponent {
         const { value } = this.state;
 
         return (
-            <ExampleContainer headline="InputBox">
-                <InputBox
-                    type="password"
-                    inputComponent={Input}
-                    value={value}
-                    onChange={this.onChange}
-                    placeholder="input"
-                >
-                    {`Password: ${value}`}
-                </InputBox>
-            </ExampleContainer>
+            <InputBox
+                type="password"
+                inputComponent={Input}
+                value={value}
+                onChange={this.onChange}
+                placeholder="input"
+            >
+                {`Password: ${value}`}
+            </InputBox>
         );
     }
 }
