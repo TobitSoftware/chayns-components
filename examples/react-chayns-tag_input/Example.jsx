bb3c295dde161833369b3e0c331194b91e0e7118
Michael Braun <michael.braun@tobit.software>
2019-02-18 09:30:11 +0100

    Make TagInput uncontrolled

--- examples/react-chayns-tag_input/Example.jsx
+++ examples/react-chayns-tag_input/Example.jsx
@@ -3,15 +3,42 @@ import React, { PureComponent } from 'react';
 import ExampleContainer from '../ExampleContainer';
 import TagInput from '../../src/react-chayns-tag_input/component/TagInput';
 
-
 export default class TagInputExample extends PureComponent {
+    state = {
+        tags: [],
+    };
+
+    handleTagAdd = (tag) => {
+        const { tags } = this.state;
+
+        if (tags.indexOf(tag) === -1) {
+            this.setState({
+                tags: [...tags, tag],
+            });
+        }
+    };
+
+    handleTagRemove = (tag) => {
+        const { tags } = this.state;
+
+        this.setState({
+            tags: tags.filter(t => tag.text === t.text),
+        });
+    };
+
     render() {
+        const { tags } = this.state;
+
         return (
             <ExampleContainer
                 headline="TagInput"
                 id="react-chayns-tag_input"
             >
-                <TagInput />
+                <TagInput
+                    tags={tags}
+                    onAddTag={this.handleTagAdd}
+                    onRemoveTag={this.handleTagRemove}
+                />
             </ExampleContainer>
         );
     }
--- src/react-chayns-tag_input/component/TagInput.jsx
+++ src/react-chayns-tag_input/component/TagInput.jsx
@@ -1,17 +1,109 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Tag from './Tag';
+import Input from '../../react-chayns-input/component/Input';
+
+const KEY_ENTER = 13;
 
 export default class TagInput extends Component {
     static propTypes = {
+        tags: PropTypes.arrayOf(PropTypes.shape({
+            text: PropTypes.string,
+        })).isRequired,
+        onAddTag: PropTypes.func,
+        onRemoveTag: PropTypes.func,
     };
 
     static defaultProps = {
+        onAddTag: null,
+        onRemoveTag: null,
     };
 
+    state = {
+        inputValue: '',
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.setInputRef = this.setInputRef.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
+        this.handleTagRemove = this.handleTagRemove.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    setInputRef(ref) {
+        this.input = ref;
+    }
+
+    handleChange(value) {
+        this.setState({
+            inputValue: value,
+        });
+    }
+
+    handleKeyUp(event) {
+        const { inputValue } = this.state;
+
+        if (event.keyCode !== KEY_ENTER || !inputValue) {
+            return;
+        }
+
+        const tag = {
+            text: inputValue,
+        };
+
+        this.setState({
+            inputValue: '',
+        });
+
+        this.handleTagAdd(tag);
+    }
+
+    handleTagAdd(tag) {
+        const { onAddTag } = this.props;
+
+        if (onAddTag) {
+            onAddTag(tag);
+        }
+    }
+
+    handleTagRemove(tag) {
+        const { onRemoveTag } = this.props;
+
+        if (onRemoveTag) {
+            onRemoveTag(tag);
+        }
+    }
+
+    handleClick() {
+        if (this.input) {
+            this.input.focus();
+        }
+    }
+
     render() {
+        const { tags } = this.props;
+        const { inputValue } = this.state;
+
         return (
-            <div>
-                {'Hello World!'}
+            <div
+                onClick={this.handleClick}
+                className="cc__tag-input input"
+            >
+                {tags && tags.map(tag => (
+                    <Tag value={tag} onDelete={this.handleDelete}>
+                        {tag.text}
+                    </Tag>
+                ))}
+                <Input
+                    inputRef={this.setInputRef}
+                    className="cc__tag-input__input"
+                    value={inputValue}
+                    onChange={this.handleChange}
+                    onKeyUp={this.handleKeyUp}
+                />
             </div>
         );
     }
