71e8c3e25d03e2f01357eb1b3427b4aa17a12337
Michael Braun <michael.braun@tobit.software>
2019-02-26 16:50:46 +0100

    Export formatters

---
+++ src/react-chayns-formatted_input/utils/index.js
@@ -0,0 +1,11 @@
+import Formatter from './Formatter';
+import IntegerFormatter from './IntegerFormatter';
+import NumericFormatter from './NumericFormatter';
+import PriceFormatter from './PriceFormatter';
+
+export {
+    Formatter,
+    IntegerFormatter,
+    NumericFormatter,
+    PriceFormatter,
+};
