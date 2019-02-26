79c3443e097a57516da7fabd298307f9df58d4d5
Michael Braun <michael.braun@tobit.software>
2019-02-26 15:19:16 +0100

    Remove debounce

--- src/react-chayns-formatted_input/FormattedInput.jsx
+++ src/react-chayns-formatted_input/FormattedInput.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import debounce from 'lodash.debounce';
 
 import Input from '../react-chayns-input/component/Input';
 import Formatter from './utils/Formatter';
@@ -8,13 +7,11 @@ import Formatter from './utils/Formatter';
 export default class FormattedInput extends Component {
     static propTypes = {
         initialFormatter: PropTypes.instanceOf(Formatter).isRequired,
-        debounce: PropTypes.number,
         onChange: PropTypes.func,
         defaultValue: PropTypes.any, /* eslint-disable-line react/forbid-prop-types */
     };
 
     static defaultProps = {
-        debounce: 5000,
         onChange: null,
         defaultValue: null,
     };
@@ -29,7 +26,6 @@ export default class FormattedInput extends Component {
         };
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleTimeChange = debounce(this.handleChange, props.debounce);
     }
 
     componentDidUpdate() {
@@ -64,13 +60,19 @@ export default class FormattedInput extends Component {
             value: newValue,
         });
 
-        this.handleTimeChange(newValue, ...args);
+        const parsedValue = formatter.parse(newValue);
+        this.handleChangeEvent(parsedValue, ...args);
+    };
+
+    handleChangeEvent = (value, ...args) => {
+        const { onChange } = this.props;
+
+        if (onChange) {
+            onChange(value, ...args);
+        }
     };
 
     handleChange = (value, ...args) => {
-        this.handleTimeChange.cancel();
-
-        const { onChange } = this.props;
         const { formatter } = this;
 
         if (!(formatter instanceof Formatter)) {
@@ -83,16 +85,13 @@ export default class FormattedInput extends Component {
             value: formatter.format(parsedValue),
         });
 
-        if (onChange) {
-            onChange(parsedValue, ...args);
-        }
+        this.handleChangeEvent(parsedValue, ...args);
     };
 
     render() {
         const { value } = this.state;
         const {
             defaultValue,
-            debounce: debounceProp,
             initialFormatter,
             ...props
         } = this.props;
