4cd0002c8c80d09b8db0146bcc9a4c868265c528
Michael Braun <michael.braun@tobit.software>
2019-04-03 11:11:15 +0200

    :ok_hand: Change promise to async-await

--- src/react-chayns-gallery/utils/getImageMetaData.js
+++ src/react-chayns-gallery/utils/getImageMetaData.js
@@ -1,20 +1,19 @@
-export function getImageMetaDataFromApi(url) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await fetch(url, {
-                method: 'HEAD'
-            });
-            const height = parseInt(response.headers.get('x-amz-meta-height'), 10);
-            const width = parseInt(response.headers.get('x-amz-meta-width'), 10);
-            if (height && width) {
-                resolve({ height, width });
-            } else {
-                reject();
-            }
-        } catch (err) {
-            reject(err);
-        }
+export async function getImageMetaDataFromApi(url) {
+    const response = await fetch(url, {
+        method: 'HEAD',
     });
+
+    const height = parseInt(response.headers.get('x-amz-meta-height'), 10);
+    const width = parseInt(response.headers.get('x-amz-meta-width'), 10);
+
+    if (height && width) {
+        return {
+            height,
+            width,
+        };
+    }
+
+    throw new Error();
 }
 
 export function getImageMetaDataFromPreview(url) {
