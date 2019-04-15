825a8e1036acaca5abd7a8e9a23f9318de1c3ba6
Yannick Scho <yannickscho@hotmail.de>
2019-04-15 09:58:05 +0200

    :memo: ImageAccordion fixxed key of undefined

--- src/react-chayns-image_accordion/component/ImageAccordionGroup.jsx
+++ src/react-chayns-image_accordion/component/ImageAccordionGroup.jsx
@@ -130,7 +130,11 @@ export default class ImageAccordionGroup extends React.Component {
             ImageAccordionGroup.dataGroups[dataGroup] = this;
         }
 
-        const findChild = children.findIndex(g => g.key === key);
+        let findChild = -1;
+        if (children) {
+            findChild = children.findIndex(g => g.key === key);
+        }
+
         this.setState(state => ({
             currentState: findChild !== -1 && children[findChild].props.children ? key : null,
             prevOpen: !sameRow && state.currentState,
