b3ca399af2ebff09be7444851741df33ef4cd551
Jonas Gossens <jg@tobit.software>
2019-04-24 11:30:57 +0200

    :bug: Fix bug that tappPortal renders into different dom nodes

--- src/react-chayns-tapp_portal/component/TappPortal.jsx
+++ src/react-chayns-tapp_portal/component/TappPortal.jsx
@@ -1,10 +1,25 @@
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 
-const TappPortal = ({ children, parent }) => createPortal(
-    children,
-    parent || document.getElementsByClassName('tapp')[0] || document.body,
-);
+const defaultParent = document.getElementsByClassName('tapp')[0] || document.body;
+let destroyed = false;
+
+const TappPortal = ({ children, parent }) => {
+    const parentUsed = document.getElementsByClassName('tapp')[0] || document.body;
+
+    if (!parent && parentUsed !== defaultParent) {
+        destroyed = true;
+    }
+
+    if (!parent && destroyed) {
+        return null;
+    }
+
+    return createPortal(
+        children,
+        parent || parentUsed,
+    );
+};
 
 TappPortal.propTypes = {
     children: PropTypes.oneOfType([
