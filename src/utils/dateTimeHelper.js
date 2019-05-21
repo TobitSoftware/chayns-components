50d799bcfae36b9fd117ab694b90d108e93b6e31
Niklas Knoll <Niklas.Knoll@tobit.software>
2019-05-21 16:44:21 +0200

    ESLint fix in dateTimeHelper

--- src/utils/dateTimeHelper.js
+++ src/utils/dateTimeHelper.js
@@ -1,18 +1,15 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable radix */
-// eslint-disable-next-line import/prefer-default-export
 export const getTimeStringMinutes = (timeStr) => {
     const parts = timeStr.split(':');
 
-    const hours = parseInt(parts[0]);
-    const minutes = parseInt(parts[1]);
+    const hours = parseInt(parts[0], 0);
+    const minutes = parseInt(parts[1], 0);
     const totalMin = hours * 60 + minutes;
 
     return totalMin;
 };
 
-export const getTimeStringFromMinutes = (totalMinutes) => {
-    if (totalMinutes > 24 * 60) totalMinutes -= 24 * 60;
+export const getTimeStringFromMinutes = (total) => {
+    const totalMinutes = total > 24 * 60 ? total - 24 * 60 : total;
 
     let hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes - hours * 60;
