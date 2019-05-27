f3091ae22cd09d5977738fb9595776d4fdded42c
Jonas Gossens <jg@tobit.software>
2019-05-27 13:45:20 +0200

    :rotating_light: Fix eslint error

--- src/react-chayns-list/component/ListItem/ListItemHeader.jsx
+++ src/react-chayns-list/component/ListItem/ListItemHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
-import Icon from "../../../react-chayns-icon/component/Icon";
+import Icon from '../../../react-chayns-icon/component/Icon';
 
 const ListItemHeader = ({
     title,
