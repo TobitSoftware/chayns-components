3ccc0c68a1c0dc39efd75d2bff0435412e060a52
Jonas Gossens <jonas.gossens@tobit.software>
2019-07-15 11:32:55 +0200

    :construction: Add color area

--- src/react-chayns-color_picker/component/ColorPicker.jsx
+++ src/react-chayns-color_picker/component/ColorPicker.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import './ColorPicker.scss';
+import ColorArea from './colorArea/ColorArea';
 
 export default class ColorPicker extends Component {
     render() {
         return (
             <div>
-                ColorPicker
+                <ColorArea
+                    color={chayns.env.site.color}
+                    onMove={console.log}
+                    onUp={console.log}
+                    preselectedColor={{
+                        r: 0, g: 0, b: 0, a: 255,
+                    }}
+                />
             </div>
         );
     }
---
+++ src/react-chayns-color_picker/component/colorArea/ColorArea.jsx
@@ -0,0 +1,183 @@
+import React, { Component } from 'react';
+import './ColorArea.scss';
+import PropTypes from 'prop-types';
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+export default class ColorArea extends Component {
+    static propTypes = {
+        color: PropTypes.string.isRequired, // TODO rename to baseColor?
+        preselectedColor: PropTypes.shape({
+            r: PropTypes.number.isRequired,
+            b: PropTypes.number.isRequired,
+            g: PropTypes.number.isRequired,
+        }).isRequired, // TODO search selected color in canvas
+        height: PropTypes.number,
+        width: PropTypes.number,
+        onMove: PropTypes.func, // TODO rename to onChange?
+        onUp: PropTypes.func,
+    };
+
+    static defaultProps = {
+        height: 200,
+        width: 300,
+        onMove: null,
+        onUp: null,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = { top: 0, left: 0 };
+        this.canvas = React.createRef();
+    }
+
+    componentDidMount() {
+        // draw canvas and set selector after first mount
+        this.drawCanvas();
+        this.setSelector();
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps !== this.props) {
+            this.drawCanvas();
+            const { color, preselectedColor } = this.props;
+            // call onMove if color changed
+            if (prevProps.color !== color) {
+                this.move();
+            }
+            // move selector if preselected color changed
+            if (prevProps.preselectedColor !== preselectedColor) {
+                this.setSelector();
+            }
+        }
+    }
+
+    setSelector() { // set selector to position of preselected color
+        const { preselectedColor, height: heightProp, width: widthProp } = this.props;
+        const ctx = this.canvas.current.getContext('2d');
+        const { data, height, width } = ctx.getImageData(0, 0, widthProp, heightProp);
+        for (let top = 0; top < height; top += 1) {
+            for (let left = 0; left < width; left += 1) {
+                const startIndex = (top * width + left) * 4;
+                if ((data[startIndex] === preselectedColor.r && data[startIndex + 1] === preselectedColor.g && data[startIndex + 2] === preselectedColor.b)) {
+                    this.setState({ top, left });
+                    return;
+                }
+            }
+        }
+    }
+
+    drawCanvas = () => { // draw the canvas
+        const { color, height, width } = this.props;
+        const ctx = this.canvas.current.getContext('2d');
+        const gradient1 = ctx.createLinearGradient(0, 0, width - 1, 0);
+        gradient1.addColorStop(0, '#fff');
+        gradient1.addColorStop(1, color);
+        ctx.fillStyle = gradient1;
+        ctx.fillRect(0, 0, width, height);
+        const gradient2 = ctx.createLinearGradient(0, 0, 0, height - 1);
+        gradient2.addColorStop(0, 'transparent');
+        gradient2.addColorStop(1, '#000');
+        ctx.fillStyle = gradient2;
+        ctx.fillRect(0, 0, width, height);
+    };
+
+    getColor = () => { // get selected color from canvas
+        const { left, top } = this.state;
+        const imageData = this.canvas.current.getContext('2d').getImageData(left, top, 1, 1);
+        return {
+            r: imageData.data[0],
+            g: imageData.data[1],
+            b: imageData.data[2],
+            a: imageData.data[3],
+        };
+    };
+
+    down = (event) => {
+        // prevent touch scroll
+        event.preventDefault();
+        // move the selector
+        window.addEventListener('mousemove', this.move);
+        window.addEventListener('touchmove', this.move);
+        // end of moving
+        window.addEventListener('mouseup', this.up);
+        window.addEventListener('touchend', this.up);
+        window.addEventListener('touchcancel', this.up);
+        window.addEventListener('blur', this.up);
+        document.addEventListener('mouseenter', this.mouseenter);
+        // rect is used to get coordinates of area html element
+        this.rect = event.target.getBoundingClientRect();
+        // call onMove
+        this.move(event);
+    };
+
+    move = (event) => {
+        const { onMove } = this.props;
+        const { rect } = this;
+        let { top, left } = this.state;
+        // set the user's coordinates
+        if (event) {
+            top = (event.changedTouches ? event.changedTouches[0].clientY : event.clientY) - rect.top;
+            left = (event.changedTouches ? event.changedTouches[0].clientX : event.clientX) - rect.left;
+        }
+        // user leaves area
+        if (top < 0) {
+            top = 0;
+        } else if (top >= rect.height) {
+            top = rect.height - 1;
+        }
+        if (left < 0) {
+            left = 0;
+        } else if (left >= rect.width) {
+            left = rect.width - 1;
+        }
+        // move selector
+        this.setState({ top, left });
+        // call onMove
+        if (onMove) {
+            onMove(this.getColor());
+        }
+    };
+
+    mouseenter = (event) => { // mouse enters window/webview/iframe
+        if (event.buttons !== 1) { // left mouse click
+            this.up();
+        }
+    };
+
+    up = () => {
+        const { onUp } = this.props;
+        // remove event listeners
+        window.removeEventListener('mousemove', this.move);
+        window.removeEventListener('touchmove', this.move);
+        window.removeEventListener('mouseup', this.up);
+        window.removeEventListener('touchend', this.up);
+        window.removeEventListener('touchcancel', this.up);
+        window.removeEventListener('blur', this.up);
+        document.removeEventListener('mouseenter', this.mouseenter);
+        // call onUp
+        if (onUp) {
+            onUp(this.getColor());
+        }
+    };
+
+    render() {
+        const { height, width } = this.props;
+        const { top, left } = this.state;
+
+        return (
+            <div className="cc__colorArea" onMouseDown={this.down} onTouchStart={this.down}>
+                <canvas
+                    className="cc__colorArea__area"
+                    ref={this.canvas}
+                    height={height}
+                    width={width}
+                    onTouchStart={preventDefault}
+                />
+                <div className="cc__colorArea__selector" style={{ top, left }} onTouchStart={preventDefault} />
+            </div>
+        );
+    }
+}
