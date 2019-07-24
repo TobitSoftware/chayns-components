75d0909b0730a51a4cca9c0a4123d17f8f57bb59
Michael Braun <michael.braun@tobit.software>
2019-07-24 11:20:09 +0200

    :art: Use react-hooks for AmountControl example

--- examples/react-chayns-amountcontrol/Example.jsx
+++ examples/react-chayns-amountcontrol/Example.jsx
@@ -1,160 +1,146 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 import { AmountControl } from '../../src/index';
 import Accordion from '../../src/react-chayns-accordion/component/Accordion';
 
-export default class AmountControlExample extends Component {
-    constructor() {
-        super();
+const AmountControlExample = () => {
+    const [amount, setAmount] = useState(0);
 
-        this.state = {
-            amount: 0
-        };
-    }
+    return (
+        <div data-equalize="group-1">
+            <AmountControl
+                autoInput
+                disabled={false}
+                amount={amount}
+                onChange={setAmount}
+                equalize="group-1"
+                buttonText="0,15"
+                addColor="#20C65A"
+                removeColor="#E71E28"
+                iconColor="#20C65A"
+                focusOnClick={false}
+                icon={faShoppingCart}
+                stopPropagation
+            />
 
-    onChange = (value) => {
-        this.setState({
-            amount: value
-        });
-    };
+            <br />
 
-    render() {
-        const { amount } = this.state;
+            <AmountControl
+                disabled={false}
+                amount={amount}
+                onChange={setAmount}
+                contentWidth={200}
+                buttonText="mtl. 15,95 €"
+                icon="ts-tobit"
+                plusIcon="ts-tobit"
+                minusIcon="ts-tobit"
+                removeIcon="ts-tobit"
+            />
 
-        return (
-            <div data-equalize="group-1">
+            <br />
+
+            <AmountControl
+                disabled
+                amount={amount}
+                onChange={setAmount}
+                equalize="group-1"
+                buttonText="test-article"
+            />
+
+            <br />
+
+            <Accordion
+                right={(
+                    <AmountControl
+                        className="accordion--no-trigger"
+                        icon="ts-bamboo"
+                        amount={amount}
+                        onChange={setAmount}
+                        equalize="group-1"
+                        buttonText="2,20€"
+                        iconColor="blue"
+                    />
+                )}
+                head="Afri Cola"
+            >
+                Test
+            </Accordion>
+
+            <br />
+
+            <AmountControl
+                amount={amount}
+                onChange={setAmount}
+                equalize="group-1"
+                buttonText="mtl. 15,95 €"
+                disableInput
+            />
+
+            <br />
+
+            <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                <AmountControl
+                    amount={amount}
+                    onChange={setAmount}
+                    equalize="group-1"
+                    buttonText="mtl. 15,95 €"
+                    disableRemove
+                />
+
+                <AmountControl
+                    amount={amount}
+                    onChange={setAmount}
+                    equalize="group-1"
+                    buttonText="mtl. 15,95 €"
+                    disableAdd
+                />
+
+                <AmountControl
+                    amount={amount}
+                    onChange={setAmount}
+                    equalize="group-1"
+                    buttonText="mtl. 15,95 €"
+                    disableAdd
+                    disableRemove
+                />
+            </div>
+
+            <AmountControl
+                amount={amount}
+                onChange={setAmount}
+                buttonFormatHandler={({ amount: a }) => `${a} h`}
+                disableInput
+            />
+
+            <br />
+
+            <AmountControl
+                amount={amount}
+                onChange={setAmount}
+                buttonText="(Amount > 0) => Input"
+                showInput={amount > 0}
+            />
+
+            <div style={{ textAlign: 'right' }}>
                 <AmountControl
                     autoInput
                     disabled={false}
                     amount={amount}
-                    onChange={this.onChange}
-                    equalize="group-1"
+                    onChange={setAmount}
                     buttonText="0,15"
-                    addColor="#20C65A"
-                    removeColor="#E71E28"
-                    iconColor="#20C65A"
-                    focusOnClick={false}
-                    icon={faShoppingCart}
-                    stopPropagation
-                />
-
-                <br/>
-
-                <AmountControl
-                    disabled={false}
-                    amount={amount}
-                    onChange={this.onChange}
-                    contentWidth={200}
-                    buttonText="mtl. 15,95 €"
-                    icon="ts-tobit"
-                    plusIcon="ts-tobit"
-                    minusIcon="ts-tobit"
-                    removeIcon="ts-tobit"
-                />
-
-                <br/>
-
-                <AmountControl
-                    disabled
-                    amount={amount}
-                    onChange={this.onChange}
-                    equalize="group-1"
-                    buttonText="test-article"
-                />
-
-                <br/>
-
-                <Accordion
-                    right={(
-                        <AmountControl
-                            className="accordion--no-trigger"
-                            icon="ts-bamboo"
-                            amount={amount}
-                            onChange={this.onChange}
-                            equalize="group-1"
-                            buttonText="2,20€"
-                            iconColor="blue"
-                        />
-                    )}
-                    head="Afri Cola"
-                >
-                    Test
-                </Accordion>
-
-                <br/>
-
-                <AmountControl
-                    amount={amount}
-                    onChange={this.onChange}
-                    equalize="group-1"
-                    buttonText="mtl. 15,95 €"
-                    disableInput
-                />
-
-                <br/>
-
-                <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                    <AmountControl
-                        amount={amount}
-                        onChange={this.onChange}
-                        equalize="group-1"
-                        buttonText="mtl. 15,95 €"
-                        disableRemove
-                    />
-
-                    <AmountControl
-                        amount={amount}
-                        onChange={this.onChange}
-                        equalize="group-1"
-                        buttonText="mtl. 15,95 €"
-                        disableAdd
-                    />
-
-                    <AmountControl
-                        amount={amount}
-                        onChange={this.onChange}
-                        equalize="group-1"
-                        buttonText="mtl. 15,95 €"
-                        disableAdd
-                        disableRemove
-                    />
-                </div>
-
-                <AmountControl
-                    amount={amount}
-                    onChange={this.onChange}
-                    buttonFormatHandler={({ amount: a }) => `${a} h`}
-                    disableInput
-                />
-
-                <br/>
-
-                <AmountControl
-                    amount={amount}
-                    onChange={this.onChange}
-                    buttonText="(Amount > 0) => Input"
-                    showInput={amount > 0}
-                />
-
-                <div style={{ textAlign: 'right' }}>
-                    <AmountControl
-                        autoInput
-                        disabled={false}
-                        amount={amount}
-                        onChange={this.onChange}
-                        buttonText="0,15"
-                    />
-                </div>
-
-                <AmountControl
-                    amount={amount}
-                    onInput={this.onChange}
-                    buttonText="0,10"
-                    showInput={amount > 0}
                 />
             </div>
-        );
-    }
-}
+
+            <AmountControl
+                amount={amount}
+                onInput={setAmount}
+                buttonText="0,10"
+                showInput={amount > 0}
+            />
+        </div>
+    );
+};
+
+export default AmountControlExample;
