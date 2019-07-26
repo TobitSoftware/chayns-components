8027efb29f36281ff6758644527491fdafb0c79d
Michael Braun <michael.braun@tobit.software>
2019-07-26 15:18:38 +0200

    :recycle: Add Relation component

--- src/react-chayns-personfinder/component/PersonFinderResultItem.jsx
+++ src/react-chayns-personfinder/component/PersonFinderResultItem.jsx
@@ -6,8 +6,15 @@ import PropTypes from 'prop-types';
 import getText from '../utils/getText';
 import { PERSON_RELATION, LOCATION_RELATION, FRIEND_RELATION } from '../constants/relationTypes';
 import Identifier from './result-item/Identifier';
+import Relation from './result-item/Relation';
 
-const SHOW_RELATIONS_COUNT = 5;
+function getRelationName(type, relation) {
+    return type === LOCATION_RELATION ? relation.name : `${relation.firstName} ${relation.lastName}`;
+}
+
+function getRelationUrl(type, relation) {
+    return type === LOCATION_RELATION ? `https://sub60.tobit.com/l/${relation.siteId}?size=40` : `https://sub60.tobit.com/u/${relation.personId}?size=40`;
+}
 
 export default class PersonFinderResultItem extends PureComponent {
     static propTypes = {
@@ -30,70 +37,8 @@ export default class PersonFinderResultItem extends PureComponent {
         type: PropTypes.oneOf([PERSON_RELATION, LOCATION_RELATION, FRIEND_RELATION]).isRequired,
     };
 
-    static getRelations(data, type) {
-        if (!data) {
-            return null;
-        }
-
-        const { length } = data;
-        const show = Math.min(length, SHOW_RELATIONS_COUNT);
-        let relationString = '';
-
-        for (let i = 0; i < show; i += 1) {
-            if (type === PERSON_RELATION || type === FRIEND_RELATION) {
-                relationString += data[i].type === 'LIVING_IN' ? `${getText(data[i].type, data[i].name)}, ` : `${data[i].name}, `;
-            } else {
-                relationString += `${getText(data[i].type)}, `;
-            }
-        }
-        relationString = relationString.slice(0, -2);
-
-        return relationString;
-    }
-
-    static getFurtherRelations(relation) {
-        if (!relation || !relation.relations) {
-            return null;
-        }
-
-        const { length } = relation.relations;
-        const furtherRelationCount = Math.max(relation.relationCount - SHOW_RELATIONS_COUNT, length - SHOW_RELATIONS_COUNT, 0) || 0;
-
-        if (furtherRelationCount <= 0) {
-            return '';
-        }
-
-        return ` +${String((furtherRelationCount) || 0)}`;
-    }
-
-    static convertRelation(type, relation) {
-        if (type === LOCATION_RELATION) {
-            return {
-                siteId: relation.siteId,
-                locationId: relation.locationId,
-                score: relation.score,
-                name: relation.name,
-                relations: relation && relation.relations ? relation.relations : [],
-                relationCount: relation.relationCount,
-                image: `https://sub60.tobit.com/l/${relation.siteId}?size=40`,
-            };
-        }
-
-        if (type === PERSON_RELATION || type === FRIEND_RELATION) {
-            return {
-                personId: relation.personId,
-                userId: relation.userId,
-                name: `${relation.firstName} ${relation.lastName}`,
-                firstName: relation.firstName,
-                lastName: relation.lastName,
-                score: relation.score,
-                relationCount: relation.relationCount,
-                image: `https://sub60.tobit.com/u/${relation.personId}?size=40`,
-                relations: relation.relations,
-            };
-        }
-
-        return null;
+    static hasRelations(data) {
+        return !!(data && data.length > 0);
     }
 
     constructor(props) {
@@ -111,54 +56,31 @@ export default class PersonFinderResultItem extends PureComponent {
         });
     }
 
-    renderSubtitle(relationString, furtherRelationsString) {
-        const {
-            relation,
-            type,
-        } = this.props;
-
-        if (!relationString) {
-            return (
-                <Identifier type={type} relation={relation} />
-            );
-        }
-
-        return (
-            <span className="relation">
-                {relationString}
-                {furtherRelationsString && (
-                    <span style={{ fontWeight: 'bold' }}>
-                        {furtherRelationsString}
-                    </span>
-                )}
-            </span>
-        );
-    }
-
     render() {
         const {
             relation,
             type,
         } = this.props;
 
-        const convertedRelation = PersonFinderResultItem.convertRelation(type, relation);
-
-        const relationString = PersonFinderResultItem.getRelations(convertedRelation.relations, type);
-        const furtherRelationsString = PersonFinderResultItem.getFurtherRelations(convertedRelation);
+        const hasRelations = PersonFinderResultItem.hasRelations(relation && relation.relations ? relation.relations : []);
 
         return (
             <div className="result-item" onClick={this.handleClick}>
-                <div className="img" style={{ backgroundImage: `url(${convertedRelation.image})` }} />
+                <div className="img" style={{ backgroundImage: `url(${getRelationUrl(type, relation)})` }} />
                 <div className="text">
                     <div
                         className="title"
                     >
-                        <div className="name">{convertedRelation.name}</div>
-                        {relationString && (
+                        <div className="name">{getRelationName(type, relation)}</div>
+                        {hasRelations && (
                             <Identifier type={type} relation={relation} />
                         )}
                     </div>
-                    {this.renderSubtitle(relationString, furtherRelationsString)}
+                    {hasRelations ? (
+                        <Relation type={type} relation={relation} />
+                    ) : (
+                        <Identifier type={type} relation={relation} />
+                    )}
                 </div>
             </div>
         );
---
+++ src/react-chayns-personfinder/component/result-item/Relation.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FRIEND_RELATION, LOCATION_RELATION, PERSON_RELATION } from '../../constants/relationTypes';
+import getText from '../../utils/getText';
+
+const SHOW_RELATIONS_COUNT = 5;
+
+function getRelations(data, type) {
+    if (!data) {
+        return null;
+    }
+
+    const { length } = data;
+    const show = Math.min(length, SHOW_RELATIONS_COUNT);
+    let relationString = '';
+
+    for (let i = 0; i < show; i += 1) {
+        if (type === PERSON_RELATION || type === FRIEND_RELATION) {
+            relationString += data[i].type === 'LIVING_IN' ? `${getText(data[i].type, data[i].name)}, ` : `${data[i].name}, `;
+        } else {
+            relationString += `${getText(data[i].type)}, `;
+        }
+    }
+    relationString = relationString.slice(0, -2);
+
+    return relationString;
+}
+
+function getFurtherRelations(relation) {
+    if (!relation || !relation.relations) {
+        return null;
+    }
+
+    const { length } = relation.relations;
+    const furtherRelationCount = Math.max(relation.relationCount - SHOW_RELATIONS_COUNT, length - SHOW_RELATIONS_COUNT, 0) || 0;
+
+    if (furtherRelationCount <= 0) {
+        return '';
+    }
+
+    return ` +${String((furtherRelationCount) || 0)}`;
+}
+
+function convertRelation(type, relation) {
+    if (type === LOCATION_RELATION) {
+        return {
+            siteId: relation.siteId,
+            locationId: relation.locationId,
+            score: relation.score,
+            name: relation.name,
+            relations: relation && relation.relations ? relation.relations : [],
+            relationCount: relation.relationCount,
+            image: `https://sub60.tobit.com/l/${relation.siteId}?size=40`,
+        };
+    }
+
+    if (type === PERSON_RELATION || type === FRIEND_RELATION) {
+        return {
+            personId: relation.personId,
+            userId: relation.userId,
+            name: `${relation.firstName} ${relation.lastName}`,
+            firstName: relation.firstName,
+            lastName: relation.lastName,
+            score: relation.score,
+            relationCount: relation.relationCount,
+            image: `https://sub60.tobit.com/u/${relation.personId}?size=40`,
+            relations: relation.relations,
+        };
+    }
+
+    return null;
+}
+
+const Relation = ({ type, relation }) => {
+    const convertedRelation = convertRelation(type, relation);
+    const relationString = getRelations(convertedRelation.relations, type);
+    const furtherRelationsString = getFurtherRelations(convertedRelation);
+
+    return (
+        <span className="relation">
+            {relationString}
+            {furtherRelationsString && (
+                <span style={{ fontWeight: 'bold' }}>
+                    {furtherRelationsString}
+                </span>
+            )}
+        </span>
+    );
+};
+
+Relation.propTypes = {
+    type: PropTypes.oneOf([PERSON_RELATION, LOCATION_RELATION, FRIEND_RELATION]).isRequired,
+    relation: PropTypes.object.isRequired,
+};
+
+export default Relation;
