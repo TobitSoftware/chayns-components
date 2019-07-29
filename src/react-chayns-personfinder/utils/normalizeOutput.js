a96f3a13dc3a80d18ca7bc2fec5268c453c24533
Michael Braun <michael.braun@tobit.software>
2019-07-29 11:41:17 +0200

    :bug: Add normalization of type to prevent wrong types

--- src/react-chayns-personfinder/utils/normalizeOutput.js
+++ src/react-chayns-personfinder/utils/normalizeOutput.js
@@ -1,6 +1,15 @@
+import {
+    FRIEND_RELATION,
+    LOCATION_RELATION,
+    PERSON_RELATION,
+    PERSON_UNRELATED,
+} from '../constants/relationTypes';
+
 export default function normalizeOutput(type, value) {
+    const newType = (type === PERSON_RELATION || type === PERSON_UNRELATED || type === FRIEND_RELATION) ? PERSON_RELATION : LOCATION_RELATION;
+
     return {
-        type,
+        type: newType,
         name: value.name,
         firstName: value.firstName,
         lastName: value.lastName,
