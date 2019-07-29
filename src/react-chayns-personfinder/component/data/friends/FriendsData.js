c7058a2a005d96afe8e26abfce3c2595deae618a
Michael Braun <michael.braun@tobit.software>
2019-07-29 13:43:54 +0200

    :zap: Add caching to FriendsData

--- src/react-chayns-personfinder/component/data/friends/FriendsData.js
+++ src/react-chayns-personfinder/component/data/friends/FriendsData.js
@@ -1,7 +1,13 @@
+const CACHE_TIME = 10 * 1000;
+
 class FriendsData {
     #friendsList = [];
 
     #friendsObject = {};
+
+    #lastFetchTime = null;
+
+    #activeFetch = null;
 
     constructor() {
         this.isFriend = this.isFriend.bind(this);
@@ -17,8 +23,7 @@ class FriendsData {
         return this.#friendsList;
     }
 
-    /* eslint-disable-next-line no-unused-vars */
-    async fetch(noCache = false) {
+    #fetch = async () => {
         const config = {
             method: 'GET',
             headers: {
@@ -30,21 +35,29 @@ class FriendsData {
         const response = await fetch('https://webapi.tobit.com/AccountService/v1.0/chayns/friends', config);
 
         if (response.status === 200) {
-            const json = await response.json();
-
-            this.#friendsList = json;
-            this.#friendsObject = {};
-            json.forEach((e) => {
-                this.#friendsObject[e.personId] = e;
-            });
-
-            return json;
+            this.#lastFetchTime = Date.now();
+            return response.json();
         }
 
-        this.#friendsList = [];
-        this.#friendsObject = {};
-
         return [];
+    };
+
+    /* eslint-disable-next-line no-unused-vars */
+    async fetch(noCache = false) {
+        if (!noCache && ((this.#lastFetchTime && this.#lastFetchTime + CACHE_TIME <= Date.now()) || this.#activeFetch)) {
+            return this.#activeFetch;
+        }
+
+        this.#activeFetch = this.#fetch();
+        const friendsList = await this.#activeFetch;
+
+        this.#friendsList = friendsList;
+        this.#friendsObject = {};
+        this.#friendsList.forEach((e) => {
+            this.#friendsObject[e.personId] = e;
+        });
+
+        return friendsList;
     }
 }
 
