7ab65ef83822c41edbd43081b56d3b4b225247f4
Michael Braun <michael.braun@tobit.software>
2019-07-29 11:23:37 +0200

    :recycle: Change PersonFinderResults to a functional component

--- src/react-chayns-personfinder/component/PersonFinderResults.jsx
+++ src/react-chayns-personfinder/component/PersonFinderResults.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {
     PERSON_RELATION,
@@ -9,109 +9,100 @@ import {
 } from '../constants/relationTypes';
 import ResultItemList from './ResultItemList';
 
-export default class PersonFinderResults extends PureComponent {
-    static propTypes = {
-        persons: PropTypes.object,
-        sites: PropTypes.object,
-        onSelect: PropTypes.func,
-        showSeparators: PropTypes.bool,
-        onLoadMore: PropTypes.func.isRequired,
-        moreRelatedSites: PropTypes.bool,
-        moreRelatedPersons: PropTypes.bool,
-        moreUnrelatedPersons: PropTypes.bool,
-        showWaitCursor: PropTypes.bool,
-        showFriends: PropTypes.bool,
-    };
-
-    static defaultProps = {
-        persons: { related: [], unrelated: [], friends: [] },
-        sites: { related: [], unrelated: [] },
-        onSelect: null,
-        showSeparators: false,
-        moreRelatedSites: false,
-        moreRelatedPersons: false,
-        moreUnrelatedPersons: false,
-        showWaitCursor: false,
-        showFriends: false,
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(value) {
-        const { onSelect } = this.props;
-
+const PersonFinderResults = ({
+    persons,
+    sites,
+    moreRelatedPersons,
+    moreRelatedSites,
+    moreUnrelatedPersons,
+    showFriends,
+    showSeparators,
+    onLoadMore,
+    showWaitCursor,
+    onSelect,
+}) => {
+    const handleClick = useCallback((value) => {
         if (onSelect) {
             onSelect(value.type, value.relation);
         }
-    }
+    }, [onSelect]);
 
-    render() {
-        const {
-            persons,
-            sites,
-            moreRelatedPersons,
-            moreRelatedSites,
-            moreUnrelatedPersons,
-            showFriends,
-            showSeparators,
-            onLoadMore,
-            showWaitCursor,
-        } = this.props;
-
-        return (
-            <div className="cc__person-finder__results">
-                {(showFriends && persons.friends && persons.friends.length) ? (
-                    <ResultItemList
-                        type={FRIEND_RELATION}
-                        hasMore={false}
-                        showSeparators={showSeparators}
-                        onLoadMore={onLoadMore}
-                        showWaitCursor={showWaitCursor}
-                        onClick={this.handleClick}
-                        relations={persons.friends}
-                    />
-                ) : null}
+    return (
+        <div className="cc__person-finder__results">
+            {(showFriends && persons.friends && persons.friends.length) ? (
                 <ResultItemList
-                    type={PERSON_RELATION}
-                    hasMore={moreRelatedPersons}
-                    showSeparators={showSeparators}
-                    onLoadMore={onLoadMore}
-                    showWaitCursor={showWaitCursor}
-                    onClick={this.handleClick}
-                    relations={persons.related}
-                />
-                <ResultItemList
-                    type={LOCATION_RELATION}
-                    hasMore={moreRelatedSites}
-                    showSeparators={showSeparators}
-                    onLoadMore={onLoadMore}
-                    showWaitCursor={showWaitCursor}
-                    onClick={this.handleClick}
-                    relations={sites.related}
-                />
-                <ResultItemList
-                    type={PERSON_UNRELATED}
-                    hasMore={moreUnrelatedPersons}
-                    showSeparators={showSeparators}
-                    onLoadMore={onLoadMore}
-                    showWaitCursor={showWaitCursor}
-                    onClick={this.handleClick}
-                    relations={persons.unrelated}
-                />
-                <ResultItemList
-                    type={LOCATION_UNRELATED}
+                    type={FRIEND_RELATION}
                     hasMore={false}
-                    showSeparators
-                    onLoadMore={null}
-                    showWaitCursor={false}
-                    onClick={this.handleClick}
-                    relations={sites.unrelated}
+                    showSeparators={showSeparators}
+                    onLoadMore={onLoadMore}
+                    showWaitCursor={showWaitCursor}
+                    onClick={handleClick}
+                    relations={persons.friends}
                 />
-            </div>
-        );
-    }
-}
+            ) : null}
+            <ResultItemList
+                type={PERSON_RELATION}
+                hasMore={moreRelatedPersons}
+                showSeparators={showSeparators}
+                onLoadMore={onLoadMore}
+                showWaitCursor={showWaitCursor}
+                onClick={handleClick}
+                relations={persons.related}
+            />
+            <ResultItemList
+                type={LOCATION_RELATION}
+                hasMore={moreRelatedSites}
+                showSeparators={showSeparators}
+                onLoadMore={onLoadMore}
+                showWaitCursor={showWaitCursor}
+                onClick={handleClick}
+                relations={sites.related}
+            />
+            <ResultItemList
+                type={PERSON_UNRELATED}
+                hasMore={moreUnrelatedPersons}
+                showSeparators={showSeparators}
+                onLoadMore={onLoadMore}
+                showWaitCursor={showWaitCursor}
+                onClick={handleClick}
+                relations={persons.unrelated}
+            />
+            <ResultItemList
+                type={LOCATION_UNRELATED}
+                hasMore={false}
+                showSeparators
+                onLoadMore={null}
+                showWaitCursor={false}
+                onClick={handleClick}
+                relations={sites.unrelated}
+            />
+        </div>
+    );
+};
+
+PersonFinderResults.propTypes = {
+    persons: PropTypes.object,
+    sites: PropTypes.object,
+    onSelect: PropTypes.func,
+    showSeparators: PropTypes.bool,
+    onLoadMore: PropTypes.func.isRequired,
+    moreRelatedSites: PropTypes.bool,
+    moreRelatedPersons: PropTypes.bool,
+    moreUnrelatedPersons: PropTypes.bool,
+    showWaitCursor: PropTypes.bool,
+    showFriends: PropTypes.bool,
+};
+
+PersonFinderResults.defaultProps = {
+    persons: { related: [], unrelated: [], friends: [] },
+    sites: { related: [], unrelated: [] },
+    onSelect: null,
+    showSeparators: false,
+    moreRelatedSites: false,
+    moreRelatedPersons: false,
+    moreUnrelatedPersons: false,
+    showWaitCursor: false,
+    showFriends: false,
+};
+
+export default PersonFinderResults;
