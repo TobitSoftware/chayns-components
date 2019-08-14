dc47f000d662a2ac867e4d7952ad362ae726361a
Michael Braun <michael.braun@tobit.software>
2019-08-14 09:47:04 +0200

    :bug: Fix compatibility file-input call

--- src/react-chayns-file_input/utils/fileInputCall.js
+++ src/react-chayns-file_input/utils/fileInputCall.js
@@ -1,10 +1,11 @@
 export default async function fileInputCall() {
     const uploadResult = await chayns.uploadCloudImage();
-    const type = uploadResult.url.match(/(\.[a-z]+)/g)[0];
+    const matches = uploadResult.url.match(/(\.[a-z]+)/g);
+    const type = (matches && matches.length) ? matches[matches.length - 1] : '';
     const response = await fetch(uploadResult.url);
     const data = await response.blob();
     const metadata = {
-        type: `image/${type}`,
+        type: `image/${type.slice(1)}`,
     };
     const file = new File([data], `androidCompatibilityUpload${type}`, metadata);
 
