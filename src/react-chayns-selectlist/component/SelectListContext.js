d0b6bae77ae4141a5773015954286ff01816fd6a
Jonas Gossens <jonas.gossens@tobit.software>
2019-08-29 16:06:27 +0200

    :truck: Rename selectlistcontext file

--- src/react-chayns-selectlist/component/SelectListContext.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const SelectListContext = createContext({
-    selectListSelectedId: null,
-    changeListItem: () => {
-    },
-});
-
-export default SelectListContext;
