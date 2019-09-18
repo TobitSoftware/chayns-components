f78d200a1bb5a875852472b38a3b019aac09a8b8
Michael Braun <michael.braun@tobit.software>
2019-09-18 10:52:16 +0200

    :pencil: Change TextAreaExample to respect value-prop changes

--- examples/react-chayns-textarea/Example.jsx
+++ examples/react-chayns-textarea/Example.jsx
@@ -1,9 +1,25 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import { TextArea } from '../../src';
 
-export default class TextAreaExample extends PureComponent {
-    render() {
-        return (
+const TextAreaExample = () => {
+    const [value, setValue] = useState('');
+
+    return (
+        <div>
+            <TextArea
+                stopPropagation
+                placeholder="Input"
+                onChange={setValue}
+                value={value}
+                autogrow
+            />
+            <TextArea
+                stopPropagation
+                placeholder="Input"
+                onChange={setValue}
+                value={value}
+                autogrow
+            />
             <TextArea
                 stopPropagation
                 placeholder="Input"
@@ -13,6 +29,8 @@ export default class TextAreaExample extends PureComponent {
                 defaultValue="Test"
                 autogrow
             />
-        );
-    }
-}
+        </div>
+    );
+};
+
+export default TextAreaExample;
