6c9da12ad3bc2c7a2b8e439e5e4e502b5e7662ed
Jonas Gossens <jonas.gossens@tobit.software>
2020-02-12 14:53:21 +0100

    :sparkles: Add filterButton example

--- examples/react-chayns-filterbutton/Example.jsx
+++ examples/react-chayns-filterbutton/Example.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import FilterButton from '../../src/react-chayns-filterbutton/component/FilterButton';
 
-const FilterButtonExample = () => {
-    return <div>FilterButton</div>;
-};
+const FilterButtonExample = () => (
+    <div>
+        <FilterButton icon="ts-tobit" checked count={33} label="Tobit.Software" />
+        <FilterButton icon="ts-chayns" count={5} label="chayns" />
+        <FilterButton icon="ts-david" count={3} label="david" />
+    </div>
+);
 
 export default FilterButtonExample;
