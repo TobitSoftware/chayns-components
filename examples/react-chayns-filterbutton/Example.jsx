a7d56450a90348ca903e34c1e084f1826ed02a78
Jonas Gossens <jonas.gossens@tobit.software>
2020-02-24 17:27:30 +0100

    :sparkles: Improve FilterButton example

--- examples/react-chayns-filterbutton/Example.jsx
+++ examples/react-chayns-filterbutton/Example.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FilterButton from '../../src/react-chayns-filterbutton/component/FilterButton';
 
-const FilterButtonExample = () => (
-    <div>
-        <FilterButton icon="ts-tobit" checked count={33} label="Tobit.Software" onChange={console.log}/>
-        <FilterButton icon="ts-chayns" count={5} label="chayns"/>
-        <FilterButton icon="ts-david" count={3} label="david"/>
-    </div>
-);
+const items = [
+    {
+        icon: 'ts-tobit',
+        count: 33,
+        label: 'Tobit.Software',
+    },
+    {
+        icon: 'ts-chayns',
+        count: 5,
+        label: 'chayns',
+    },
+    {
+        icon: 'ts-david',
+        count: 3,
+        label: 'david',
+    },
+];
+
+const FilterButtonExample = () => {
+    const [selected, setSelected] = useState([0]);
+
+    const toggleItem = (index) => {
+        const newSelected = [...selected];
+        if (newSelected.includes(index)) {
+            newSelected.splice(newSelected.indexOf(index), 1);
+        } else {
+            newSelected.push(index);
+        }
+        setSelected(newSelected);
+    };
+
+    return items.map((item, index) => (
+        <FilterButton
+            checked={selected.includes(index)}
+            onChange={() => {
+                toggleItem(index);
+            }}
+            {...item}
+        />
+    ));
+};
 
 export default FilterButtonExample;
