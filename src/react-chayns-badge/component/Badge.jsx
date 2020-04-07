762ece6e484e2e85e120d13bf6fdb7d0ead87e6a
Leonhard Driesch <leo.driesch@gmail.com>
2020-04-07 15:17:21 +0200

    :sparkles: Re-add badgeRef prop

--- src/react-chayns-badge/component/Badge.jsx
+++ src/react-chayns-badge/component/Badge.jsx
@@ -2,31 +2,36 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { forwardRef, useCallback, useState } from 'react';
 
-const Badge = forwardRef(({ children, className, style, ...other }, ref) => {
-    const [minWidth, setMinWidth] = useState();
+const Badge = forwardRef(
+    ({ children, className, style, badgeRef, ...other }, ref) => {
+        const [minWidth, setMinWidth] = useState();
 
-    const measureRef = useCallback((node) => {
-        if (node) {
-            setMinWidth(node.getBoundingClientRect().height);
-        }
-    }, []);
+        const measureRef = useCallback((node) => {
+            if (node) {
+                setMinWidth(node.getBoundingClientRect().height);
+            }
+        }, []);
 
-    return (
-        <div
-            className={classNames(className, 'badge')}
-            ref={(node) => {
-                measureRef(node);
-                if (ref) {
-                    ref(node);
-                }
-            }}
-            style={{ minWidth, ...style }}
-            {...other}
-        >
-            {children}
-        </div>
-    );
-});
+        return (
+            <div
+                className={classNames(className, 'badge')}
+                ref={(node) => {
+                    measureRef(node);
+                    if (ref) {
+                        ref(node);
+                    }
+                    if (badgeRef) {
+                        badgeRef(node);
+                    }
+                }}
+                style={{ minWidth, ...style }}
+                {...other}
+            >
+                {children}
+            </div>
+        );
+    }
+);
 
 export default Badge;
 
@@ -35,11 +40,13 @@ Badge.propTypes = {
     className: PropTypes.string,
     // eslint-disable-next-line react/forbid-prop-types
     style: PropTypes.object,
+    badgeRef: PropTypes.func,
 };
 
 Badge.defaultProps = {
     className: '',
     style: undefined,
+    badgeRef: undefined,
 };
 
 Badge.displayName = 'Badge';
