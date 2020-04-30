e1123e2de6d9542950967a4128e616ddcbad7259
Michael Braun <michael.braun@tobit.software>
2020-04-30 11:34:10 +0200

    :recycle: Use regex for parsing TimeString

--- src/react-chayns-openingtimes/utils/parseTimeString.js
+++ src/react-chayns-openingtimes/utils/parseTimeString.js
@@ -1,12 +1,13 @@
+const TIME_STRING_REGEX = /([0-9]{0,2}):([0-9]{0,2})/;
+
 export default function parseTimeString(str) {
-    const regexRes = new RegExp('[0-9]{0,2}:[0-9]{0,2}').exec(str);
+    const regexRes = TIME_STRING_REGEX.exec(str);
     let hours = null;
     let minutes = null;
-    if (regexRes) {
-        const parts = regexRes[0].split(':');
 
-        hours = parseInt(parts[0], 10) || 0;
-        minutes = parseInt(parts[1], 10) || 0;
+    if (regexRes) {
+        hours = parseInt(regexRes[1], 10) || 0;
+        minutes = parseInt(regexRes[2], 10) || 0;
     }
 
     return {
