1bfe20c2c05d763adf18ad905ca47692ed25dbb8
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-08 16:29:16 +0200

    :hammer: Improve rollup config to build different output formats

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve';
+import autoExternal from 'rollup-plugin-auto-external';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
 import external from 'rollup-plugin-peer-deps-external';
-import autoExternal from 'rollup-plugin-auto-external';
 import postcss from 'rollup-plugin-postcss';
 
 const env = process.env.NODE_ENV;
@@ -10,13 +10,10 @@ const pkg = require('./package.json');
 
 export default {
     input: 'src/index.js',
-    output: {
-        file: {
-            es: pkg.module,
-            cjs: pkg.main,
-        }[env],
-        format: env,
-    },
+    output: [
+        { file: pkg.module, format: 'esm', sourcemap: true },
+        { file: pkg.main, format: 'cjs', sourcemap: true },
+    ],
     external: [
         ...Object.keys(pkg.dependencies),
         ...Object.keys(pkg.peerDependencies),
