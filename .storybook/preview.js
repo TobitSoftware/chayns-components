ad75e74f1c328bf0f9983e82daf7a8e112a2527f
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-08 18:10:00 +0200

    :wrench: Fix css styles by adding a tapp wrapper

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import React from 'react';
+
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+export const decorators = [
+    (Story) => (
+        <div className="tapp">
+            <Story />
+        </div>
+    ),
+];
