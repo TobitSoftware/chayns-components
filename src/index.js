827f0c316fe633bd03b9758e177d9457f6e83766
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-08 15:54:53 +0200

    :art: Improve readability of index file

--- src/index.js
+++ src/index.js
@@ -1,190 +1,86 @@
-import Accordion from './react-chayns-accordion/component/Accordion';
-import AccordionIntro from './react-chayns-accordion/component/AccordionIntro';
-import AmountControl from './react-chayns-amountcontrol/component/AmountControl';
-import AnimationWrapper from './react-chayns-animation_wrapper/component/AnimationWrapper';
-
-import Badge from './react-chayns-badge/component/Badge';
-import Button from './react-chayns-button/component/Button';
-import Bubble from './react-chayns-bubble/component/Bubble';
-import ChooseButton from './react-chayns-button/component/ChooseButton';
-
-import Calendar from './react-chayns-calendar/component/Calendar';
-import Checkbox from './react-chayns-checkbox/component/Checkbox';
-import ComboBox from './react-chayns-combobox/component/ComboBox';
-import ContextMenu from './react-chayns-contextmenu/component/ContextMenu';
-import ColorPicker from './react-chayns-color_picker/component/ColorPicker';
-import ColorScheme from './react-chayns-color_scheme/component/ColorScheme';
-import HueSlider from './react-chayns-color_picker/component/hueSlider/HueSlider';
-import TransparencySlider from './react-chayns-color_picker/component/transparencySlider/TransparencySlider';
-import ColorInput from './react-chayns-color_picker/component/colorInput/ColorInput';
-import ColorArea from './react-chayns-color_picker/component/colorArea/ColorArea';
-
-import DateInfo from './react-chayns-dateinfo/component/DateInfo';
-
-import EmojiInput from './react-chayns-emoji_input/component/EmojiInput';
-
-import ExpandableContent from './react-chayns-expandable_content/component/ExpandableContent';
-
-import FilterButton from './react-chayns-filterbutton/component/FilterButton';
-
-import FormattedInput from './react-chayns-formatted_input/component/FormattedInput';
-import {
-    Formatter,
-    IntegerFormatter,
+export { CHAYNS_CSS_VERSION } from './constants';
+export { default as Accordion } from './react-chayns-accordion/component/Accordion';
+export { default as AccordionIntro } from './react-chayns-accordion/component/AccordionIntro';
+export { default as AmountControl } from './react-chayns-amountcontrol/component/AmountControl';
+export { default as AnimationWrapper } from './react-chayns-animation_wrapper/component/AnimationWrapper';
+export { default as Badge } from './react-chayns-badge/component/Badge';
+export { default as Bubble } from './react-chayns-bubble/component/Bubble';
+export { default as Button } from './react-chayns-button/component/Button';
+export { default as ChooseButton } from './react-chayns-button/component/ChooseButton';
+export { default as Calendar } from './react-chayns-calendar/component/Calendar';
+export { default as Checkbox } from './react-chayns-checkbox/component/Checkbox';
+export { default as ColorArea } from './react-chayns-color_picker/component/colorArea/ColorArea';
+export { default as ColorInput } from './react-chayns-color_picker/component/colorInput/ColorInput';
+export { default as ColorPicker } from './react-chayns-color_picker/component/ColorPicker';
+export { default as HueSlider } from './react-chayns-color_picker/component/hueSlider/HueSlider';
+export { default as TransparencySlider } from './react-chayns-color_picker/component/transparencySlider/TransparencySlider';
+export { default as ColorScheme } from './react-chayns-color_scheme/component/ColorScheme';
+export { default as ComboBox } from './react-chayns-combobox/component/ComboBox';
+export { default as ContextMenu } from './react-chayns-contextmenu/component/ContextMenu';
+export { default as DateInfo } from './react-chayns-dateinfo/component/DateInfo';
+export { default as EmojiInput } from './react-chayns-emoji_input/component/EmojiInput';
+export { default as ExpandableContent } from './react-chayns-expandable_content/component/ExpandableContent';
+export { default as FileInput } from './react-chayns-file_input/component/FileInput';
+export { default as FilterButton } from './react-chayns-filterbutton/component/FilterButton';
+export { default as FormattedInput } from './react-chayns-formatted_input/component/FormattedInput';
+export {
     DecimalFormatter,
-    PriceFormatter,
-    FORMAT_INTEGER,
+    Formatter,
     FORMAT_DECIMAL,
+    FORMAT_INTEGER,
     FORMAT_PRICE,
+    IntegerFormatter,
+    PriceFormatter,
 } from './react-chayns-formatted_input/utils/index';
-
-import Gallery from './react-chayns-gallery/component/Gallery';
-import GridCalendar from './react-chayns-gridcalendar/component/GridCalendar';
-
-import Icon from './react-chayns-icon/component/Icon';
-import Input from './react-chayns-input/component/Input';
-import Image from './react-chayns-gallery/component/Image';
-import ImageContainer from './react-chayns-gallery/component/ImageContainer';
-import ImageAccordionGroup from './react-chayns-image_accordion/component/ImageAccordionGroup';
-import ImageAccordion from './react-chayns-image_accordion/component/ImageAccordion';
-
-import List from './react-chayns-list/component/List';
-import ListItem from './react-chayns-list/component/ListItem';
-
-import ModeSwitch from './react-chayns-modeswitch/component/ModeSwitch';
-import Mode from './react-chayns-modeswitch/component/Mode';
-
-import OpeningTimes from './react-chayns-openingtimes/component/OpeningTimes';
-
-import PersonFinder from './react-chayns-personfinder/component/PersonFinder';
-import SimpleWrapperContext from './react-chayns-personfinder/component/data/simpleWrapper/SimpleWrapperContext';
-
-import ProgressBar from './react-chayns-progress_bar/component/ProgressBar';
-
-import RadioButton from './react-chayns-radiobutton/component/RadioButton';
-import ReceiverInput from './react-chayns-receiverinput/component/ReceiverInput';
-import RfidInput from './react-chayns-rfid_input/component/RfidInput';
-
-import ScrollView from './react-chayns-scrollview/component/ScrollView';
-import SearchBox from './react-chayns-searchbox/component/SearchBox';
-import ResultSelection from './react-chayns-searchbox/component/result-selection/ResultSelection';
-import SelectButton from './react-chayns-selectbutton/component/SelectButton';
-import SelectList from './react-chayns-selectlist/component/SelectList';
-import SelectListItem from './react-chayns-selectlist/component/SelectItem';
-import SetupWizard from './react-chayns-setupwizard/component/SetupWizard';
-import SetupWizardItem from './react-chayns-setupwizard/component/SetupItem';
-import withSetupWizardContext from './react-chayns-setupwizard/component/withSetupWizardContext';
-
-import SharingBar from './react-chayns-sharingbar/component/SharingBar';
-import SliderButton from './react-chayns-sliderbutton/component/SliderButton';
-import SmallWaitCursor from './react-chayns-smallwaitcursor/component/SmallWaitCursor';
-
-import TagInput from './react-chayns-tag_input/component/TagInput';
-import TextArea from './react-chayns-textarea/component/TextArea';
-import TextString from './react-chayns-textstring/component/TextString';
-import Tooltip from './react-chayns-tooltip/component/Tooltip';
-
-import FileInput from './react-chayns-file_input/component/FileInput';
-import PositionInput from './react-chayns-position_input/component/PositionInput';
-
-import resolveAbsoluteImport from './utils/babel/resolveAbsoluteImport';
-import OrientationHelper from './utils/OrientationHelper';
-import imageUpload from './utils/imageUpload';
-import {
-    getDataUrlFromFile,
+export { default as Gallery } from './react-chayns-gallery/component/Gallery';
+export { default as Image } from './react-chayns-gallery/component/Image';
+export { default as ImageContainer } from './react-chayns-gallery/component/ImageContainer';
+export {
     getDataUrlFromBase64,
+    getDataUrlFromFile,
 } from './react-chayns-gallery/utils/getDataUrl';
-import {
+export {
     getImageMetaDataFromApi,
     getImageMetaDataFromPreview,
 } from './react-chayns-gallery/utils/getImageMetaData';
-import getOrientation from './react-chayns-gallery/utils/getOrientation';
-import isTobitEmployee from './utils/tobitEmployee';
-import createLinks from './utils/createLinks';
-import removeHtml from './utils/removeHtml';
-import equalizer from './utils/equalizer';
-
-import { CHAYNS_CSS_VERSION } from './constants';
-
-export {
-    Accordion,
-    AccordionIntro,
-    AmountControl,
-    AnimationWrapper,
-    Badge,
-    Bubble,
-    Button,
-    ChooseButton,
-    Calendar,
-    Checkbox,
-    ComboBox,
-    ContextMenu,
-    HueSlider,
-    TransparencySlider,
-    ColorInput,
-    ColorArea,
-    ColorPicker,
-    ColorScheme,
-    DateInfo,
-    EmojiInput,
-    ExpandableContent,
-    FilterButton,
-    FormattedInput,
-    Formatter,
-    IntegerFormatter,
-    DecimalFormatter,
-    PriceFormatter,
-    Gallery,
-    GridCalendar,
-    Icon,
-    Image,
-    ImageContainer,
-    ImageAccordion,
-    ImageAccordionGroup,
-    Input,
-    List,
-    ListItem,
-    ModeSwitch,
-    Mode,
-    OpeningTimes,
-    OrientationHelper,
-    PersonFinder,
-    SimpleWrapperContext,
-    ProgressBar,
-    RadioButton,
-    ReceiverInput,
-    RfidInput,
-    ScrollView,
-    SearchBox,
-    ResultSelection,
-    SelectButton,
-    SelectList,
-    SelectListItem,
-    SetupWizard,
-    SetupWizardItem,
-    withSetupWizardContext,
-    SharingBar,
-    SliderButton,
-    SmallWaitCursor,
-    TagInput,
-    TextArea,
-    TextString,
-    Tooltip,
-    FileInput,
-    PositionInput,
-    resolveAbsoluteImport,
-    imageUpload,
-    isTobitEmployee,
-    createLinks,
-    removeHtml,
-    equalizer,
-    getImageMetaDataFromPreview,
-    getImageMetaDataFromApi,
-    getDataUrlFromBase64,
-    getDataUrlFromFile,
-    getOrientation,
-    CHAYNS_CSS_VERSION,
-    FORMAT_INTEGER,
-    FORMAT_DECIMAL,
-    FORMAT_PRICE,
-};
+export { default as getOrientation } from './react-chayns-gallery/utils/getOrientation';
+export { default as GridCalendar } from './react-chayns-gridcalendar/component/GridCalendar';
+export { default as Icon } from './react-chayns-icon/component/Icon';
+export { default as ImageAccordion } from './react-chayns-image_accordion/component/ImageAccordion';
+export { default as ImageAccordionGroup } from './react-chayns-image_accordion/component/ImageAccordionGroup';
+export { default as Input } from './react-chayns-input/component/Input';
+export { default as List } from './react-chayns-list/component/List';
+export { default as ListItem } from './react-chayns-list/component/ListItem';
+export { default as Mode } from './react-chayns-modeswitch/component/Mode';
+export { default as ModeSwitch } from './react-chayns-modeswitch/component/ModeSwitch';
+export { default as OpeningTimes } from './react-chayns-openingtimes/component/OpeningTimes';
+export { default as SimpleWrapperContext } from './react-chayns-personfinder/component/data/simpleWrapper/SimpleWrapperContext';
+export { default as PersonFinder } from './react-chayns-personfinder/component/PersonFinder';
+export { default as PositionInput } from './react-chayns-position_input/component/PositionInput';
+export { default as ProgressBar } from './react-chayns-progress_bar/component/ProgressBar';
+export { default as RadioButton } from './react-chayns-radiobutton/component/RadioButton';
+export { default as ReceiverInput } from './react-chayns-receiverinput/component/ReceiverInput';
+export { default as RfidInput } from './react-chayns-rfid_input/component/RfidInput';
+export { default as ScrollView } from './react-chayns-scrollview/component/ScrollView';
+export { default as ResultSelection } from './react-chayns-searchbox/component/result-selection/ResultSelection';
+export { default as SearchBox } from './react-chayns-searchbox/component/SearchBox';
+export { default as SelectButton } from './react-chayns-selectbutton/component/SelectButton';
+export { default as SelectListItem } from './react-chayns-selectlist/component/SelectItem';
+export { default as SelectList } from './react-chayns-selectlist/component/SelectList';
+export { default as SetupWizardItem } from './react-chayns-setupwizard/component/SetupItem';
+export { default as SetupWizard } from './react-chayns-setupwizard/component/SetupWizard';
+export { default as withSetupWizardContext } from './react-chayns-setupwizard/component/withSetupWizardContext';
+export { default as SharingBar } from './react-chayns-sharingbar/component/SharingBar';
+export { default as SliderButton } from './react-chayns-sliderbutton/component/SliderButton';
+export { default as SmallWaitCursor } from './react-chayns-smallwaitcursor/component/SmallWaitCursor';
+export { default as TagInput } from './react-chayns-tag_input/component/TagInput';
+export { default as TextArea } from './react-chayns-textarea/component/TextArea';
+export { default as TextString } from './react-chayns-textstring/component/TextString';
+export { default as Tooltip } from './react-chayns-tooltip/component/Tooltip';
+export { default as resolveAbsoluteImport } from './utils/babel/resolveAbsoluteImport';
+export { default as createLinks } from './utils/createLinks';
+export { default as equalizer } from './utils/equalizer';
+export { default as imageUpload } from './utils/imageUpload';
+export { default as OrientationHelper } from './utils/OrientationHelper';
+export { default as removeHtml } from './utils/removeHtml';
+export { default as isTobitEmployee } from './utils/tobitEmployee';
