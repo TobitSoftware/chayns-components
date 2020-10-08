57bb5c10c8d2bb4853bd40fb90147ec831b09774
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-08 16:36:47 +0200

    :arrow_up: Upgrade rollup and rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,9 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import autoExternal from 'rollup-plugin-auto-external';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-
-const env = process.env.NODE_ENV;
-const pkg = require('./package.json');
+import pkg from './package.json';
 
 export default {
     input: 'src/index.js',
@@ -14,25 +11,16 @@ export default {
         { file: pkg.module, format: 'esm', sourcemap: true },
         { file: pkg.main, format: 'cjs', sourcemap: true },
     ],
-    external: [
-        ...Object.keys(pkg.dependencies),
-        ...Object.keys(pkg.peerDependencies),
-    ],
     plugins: [
-        external(),
         autoExternal(),
         babel({
             exclude: 'node_modules/**',
-            // if external helpers true then use global babel object
-            externalHelpers: true,
-            runtimeHelpers: true,
+            babelHelpers: 'runtime',
         }),
         resolve({
             extensions: ['.js', '.jsx'],
         }),
         commonjs(),
-        postcss({
-            plugins: [],
-        }),
+        postcss({}),
     ],
 };
