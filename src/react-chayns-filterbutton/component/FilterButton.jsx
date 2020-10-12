10dac973f726a126e9cf1b70335fcc64dcce455f
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-12 09:30:57 +0200

    :memo: Add documentation to the FilterButton component

--- src/react-chayns-filterbutton/component/FilterButton.jsx
+++ src/react-chayns-filterbutton/component/FilterButton.jsx
@@ -1,3 +1,7 @@
+/**
+ * @component
+ */
+
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { useRef } from 'react';
@@ -9,6 +13,10 @@ const isValue = (val) => typeof val !== 'undefined' && val !== null;
 const PREFIX = 'CC_FB_';
 let currentId = 0;
 
+/**
+ * A chip-like component that is used to filter lists. Usually used in a group
+ * of 2 or more.
+ */
 const FilterButton = ({
     icon,
     label,
@@ -84,17 +92,46 @@ const FilterButton = ({
 };
 
 FilterButton.propTypes = {
+    /**
+     * A string or `ReactNode` that is shown inside of the filter button.
+     */
     label: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.node,
         PropTypes.element,
     ]),
+
+    /**
+     * A number that is shown in bold on the right side of the button.
+     */
     count: PropTypes.number,
+
+    /**
+     * A function that will be called when the state of the button changes.
+     */
     onChange: PropTypes.func,
+
+    /**
+     * Wether the button is checked or not.
+     */
     checked: PropTypes.bool,
+
+    /**
+     * Multiple filter buttons with the same name belong to one group. Only one
+     * of the buttons in a group can be active at one time.
+     */
     name: PropTypes.string,
-    // eslint-disable-next-line react/forbid-prop-types
-    value: PropTypes.any,
+
+    /**
+     * The value that is provided as the first argument to the
+     * `onChanged`-callback when the `name`-property is set.
+     */
+    value: PropTypes.any, // eslint-disable-line react/forbid-prop-types
+
+    /**
+     * An icon that will be shown in the button. Use a FontAwesome icon like
+     * this: `"fa fa-plane"`.
+     */
     icon: PropTypes.oneOfType([
         PropTypes.string.isRequired,
         PropTypes.shape({
@@ -102,13 +139,42 @@ FilterButton.propTypes = {
             prefix: PropTypes.string.isRequired,
         }).isRequired,
     ]),
+
+    /**
+     * A classname string that will be set on the `<label>`-element. To change
+     * the color of the filter button give it a class that sets the CSS
+     * `color`-attribute, not `background-color`.
+     */
     className: PropTypes.string,
+
+    /**
+     * A React style object that will be applied to the `<label>`-element. To
+     * change the color of the filter button, add a `color`-style attribute to
+     * the button, not `background-color`.
+     */
     style: PropTypes.objectOf(
         PropTypes.oneOfType([PropTypes.string, PropTypes.number])
     ),
+
+    /**
+     * An HTML id that will be set on the (invisible) `<input>`-element. This is
+     * given as the second argument to onChange if `name` is unset.
+     */
     id: PropTypes.string,
+
+    /**
+     * Disables any interaction and renders the filter button as disabled.
+     */
     disabled: PropTypes.bool,
+
+    /**
+     * Wether to stop propagation of click events to parent elements.
+     */
     stopPropagation: PropTypes.bool,
+
+    /**
+     * Shrinks the filter button in size.
+     */
     small: PropTypes.bool,
 };
 
