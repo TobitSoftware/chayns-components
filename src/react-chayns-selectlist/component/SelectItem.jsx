ce294007b2d2c2d70c6c255a8b10d773c2446bfc
Leonhard Driesch <leo.driesch@gmail.com>
2020-10-13 09:55:51 +0200

    :memo: Add documentation for the SelectList and SelectItem components

--- src/react-chayns-selectlist/component/SelectItem.jsx
+++ src/react-chayns-selectlist/component/SelectItem.jsx
@@ -1,3 +1,7 @@
+/**
+ * @component
+ */
+
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
@@ -14,6 +18,10 @@ export default function withContext(props) {
     );
 }
 
+/**
+ * A radio button that expands its content when selected. Should be used inside
+ * of a `SelectList`.
+ */
 class SelectItem extends Component {
     handleChange = () => {
         const { changeListItem, id, value } = this.props;
@@ -82,18 +90,62 @@ class SelectItem extends Component {
 }
 
 SelectItem.propTypes = {
+    /**
+     * The select item id. It identifies the `SelectItem` for the
+     * `defaultValue`- and `value`-props of the `SelectList`-component and will
+     * be given as the first argument to the `onChange`-callback of the
+     * `SelectList`-component.
+     */
     id: PropTypes.number,
+
+    /**
+     * A classname string that will be applied to the container element.
+     */
+    className: PropTypes.string,
+
+    /**
+     * Disables any user interaction and renders the `SelectItem` in a disabled
+     * style.
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * The items content that will be revealed when it is selected.
+     */
+    children: PropTypes.node,
+
+    /**
+     * The name of the `SelectItem` that is shown as a label next to its radio
+     * button.
+     */
+    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+
+    /**
+     * Any additional info for the item.
+     */
+    value: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+
+    /**
+     * When specified, a `ToolTip`-component will wrap the radio button and these
+     * props will be forwarded.
+     */
+    tooltipProps: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+
+    /**
+     * **WARNING:** Internal use only. Do not rely on this prop.
+     */
+    selectListSelectedId: PropTypes.number,
+
+    /**
+     * **WARNING:** Internal use only. Do not rely on this prop.
+     */
     changeListItem: PropTypes.func,
+
+    /**
+     * **WARNING:** Internal use only. Do not rely on this prop.
+     */
     selectListId: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
         .isRequired,
-    className: PropTypes.string,
-    disabled: PropTypes.bool,
-    children: PropTypes.node,
-    name: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
-    value: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-    selectListSelectedId: PropTypes.number,
-    // eslint-disable-next-line react/forbid-prop-types
-    tooltipProps: PropTypes.object,
 };
 
 SelectItem.defaultProps = {
