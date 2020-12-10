6531bc82642bc4cbda86e3497956d5cabf8ed8ce
Heiner Pöpping <heiner.poepping@tobit.software>
2020-12-10 10:19:46 +0100

    Add clearIcon in example list

--- examples/utils/components/ExampleList.jsx
+++ examples/utils/components/ExampleList.jsx
@@ -69,6 +69,7 @@ export default class ExampleList extends Component {
                         customProps={{
                             autoFocus: true,
                         }}
+                        clearIcon
                         dynamic
                     />
                 )}
