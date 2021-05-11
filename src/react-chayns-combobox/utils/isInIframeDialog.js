5b45fe5c06747708e67e0a45aa3213d091987f70
Heiner Pöpping <heiner.poepping@tobit.software>
2021-05-11 11:13:04 +0200

    :bug: Fix server side crash when using comboBox

--- src/react-chayns-combobox/utils/isInIframeDialog.js
+++ src/react-chayns-combobox/utils/isInIframeDialog.js
@@ -1,4 +1,8 @@
+import { isServer } from '../../utils/isServer';
+
 export default function isInIframeDialog() {
+    if (isServer()) return false;
+
     const scriptList = document.getElementsByTagName('script');
     for (let i = 0; i < scriptList.length; i++) {
         if (
