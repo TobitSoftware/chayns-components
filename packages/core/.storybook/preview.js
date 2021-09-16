53928af74f79243d1d77512f2141390180a1d1bd
Jannik Weise <jannik.weise@tobit.com>
2021-09-16 18:52:16 +0200

    :wrench: Update storybook configuration

--- packages/core/.storybook/preview.js
+++ packages/core/.storybook/preview.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import { chaynsTheme } from './chaynsTheme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    theme: chaynsTheme,
+};
+
+export const decorators = [
+    (Story) => (
+        <div className="tapp" style={{ margin: 0, padding: 0 }}>
+            <Story />
+        </div>
+    ),
+];
