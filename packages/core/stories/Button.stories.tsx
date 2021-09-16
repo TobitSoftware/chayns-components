186f51812aaea61f53056fee7f9136d770f3ad29
Jannik Weise <jannik.weise@tobit.com>
2021-09-16 19:20:47 +0200

    :truck: Move stories to root directory in packages

--- packages/core/stories/Button.stories.tsx
+++ packages/core/stories/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from '../components/button/Button';
+import Button from '../src/components/button/Button';
 
 export default {
   title: 'Example/Button',
