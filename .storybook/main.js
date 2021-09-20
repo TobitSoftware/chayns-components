6661ec1390625640481e48ae6518de395eb84fa4
Jannik Weise <jannik.weise@tobit.com>
2021-09-20 16:45:12 +0200

    :wrench: Update addon configuration for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,14 @@
 module.exports = {
-  "stories": [
-    "../packages/**/*.stories.mdx",
-    "../packages/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+    stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        {
+            name: '@storybook/addon-essentials',
+            options: {
+                measure: false,
+                outline: false,
+                toolbars: false,
+            },
+        },
+    ],
+};
