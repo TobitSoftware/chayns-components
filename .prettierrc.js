9b8d09d63a58770497eed94b1bf2a5ac607e1699
Jannik Weise <jannik.weise@tobit.com>
2021-09-20 16:44:24 +0200

    :heavy_plus_sign: Add "prettier" and "@chayns/colors" dependency
    :wrench: Add prettier configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/**
+ * Documentation here: https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Options}
+ */
+module.exports = {
+    printWidth: 100,
+    proseWrap: 'always',
+    singleQuote: true,
+    tabWidth: 4,
+};
