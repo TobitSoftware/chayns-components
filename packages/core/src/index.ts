1e1d5454081ce59dbd814e202a52c0d6fe2936d9
Jannik Weise <jannik.weise@tobit.com>
2021-09-27 13:55:17 +0200

    :sparkles: Add export for components in index.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,5 @@
+export { default as Accordion } from './components/accordion/Accordion';
+export { default as Badge } from './components/badge/Badge';
 export { default as Button } from './components/button/Button';
+export { default as ColorSchemeProvider } from './components/color-scheme-provider/ColorSchemeProvider';
+export { default as Icon } from './components/icon/Icon';
