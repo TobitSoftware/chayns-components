2528784b279db320a6d381381390b82570893faf
Jannik Weise <jannik.weise@tobit.com>
2021-10-11 11:02:53 +0200

    :sparkles: Add export for AccordionGroup in index.ts of core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,6 @@
 export { default as Accordion } from './components/accordion/Accordion';
 export { default as AccordionContent } from './components/accordion/accordion-content/AccordionContent';
+export { default as AccordionGroup } from './components/accordion/accordion-group/AccordionGroup';
 export { default as Badge } from './components/badge/Badge';
 export { default as Button } from './components/button/Button';
 export { default as ColorSchemeProvider } from './components/color-scheme-provider/ColorSchemeProvider';
