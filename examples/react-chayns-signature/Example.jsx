8f8d6eb75842d71538821be7b82cce438ed7b90b
Heiner Pöpping <heiner.poepping@tobit.software>
2021-11-26 12:22:38 +0100

    :sparkles: Add forceIntialShow prop and ref to edit/delete in signature

--- examples/react-chayns-signature/Example.jsx
+++ examples/react-chayns-signature/Example.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import Button from '../../src/react-chayns-button/component/Button';
 import Signature from '../../src/react-chayns-signature/component/Signature';
 
-const SignatureExample = () => (
-    <Signature
-        onSubscribe={() =>
-            chayns.dialog.toast({
-                description: 'Unterschrieben',
-                duration: 3000,
-            })
-        }
-        onEdit={(e) => console.log('edit', e)}
-    />
-);
+const SignatureExample = () => {
+    const ref = useRef();
+
+    return (
+        <>
+            <div style={{ display: 'flex', margin: '10px 0' }}>
+                <Button
+                    onClick={() => {
+                        ref.current.edit();
+                    }}
+                    style={{ marginRight: 8 }}
+                >
+                    Bearbeiten
+                </Button>
+                <Button
+                    onClick={() => {
+                        ref.current.delete();
+                    }}
+                >
+                    Löschen
+                </Button>
+            </div>
+            <Signature
+                ref={ref}
+                forceShowSignature
+                onSubscribe={() =>
+                    chayns.dialog.toast({
+                        description: 'Unterschrieben',
+                        duration: 3000,
+                    })
+                }
+                onEdit={(e) => console.log('edit', e)}
+            />
+        </>
+    );
+};
 
 export default SignatureExample;
--- src/react-chayns-signature/component/Signature.jsx
+++ src/react-chayns-signature/component/Signature.jsx
@@ -2,7 +2,13 @@
  * @component
  */
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, {
+    useEffect,
+    useState,
+    useCallback,
+    forwardRef,
+    useImperativeHandle,
+} from 'react';
 import PropTypes from 'prop-types';
 import {
     getUserSignature,
@@ -14,116 +20,137 @@ import Button from '../../react-chayns-button/component/Button';
 /**
  * A component to let the user subscribe
  */
-const Signature = ({
-    buttonText,
-    buttonWrapperClassName,
-    onSubscribe,
-    skipLoadAndSave,
-    disabled,
-    onEdit,
-}) => {
-    const [signatureUrl, setSignatureUrl] = useState(undefined);
-    const [subscribed, setSubscribed] = useState(false);
+const Signature = forwardRef(
+    (
+        {
+            buttonText,
+            buttonWrapperClassName,
+            onSubscribe,
+            skipLoadAndSave,
+            disabled,
+            onEdit,
+            forceInitialShow,
+        },
+        ref
+    ) => {
+        const [signatureUrl, setSignatureUrl] = useState(undefined);
+        const [subscribed, setSubscribed] = useState(() => forceInitialShow);
 
-    useEffect(() => {
-        if (!skipLoadAndSave) getUserSignature().then(setSignatureUrl);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        useEffect(() => {
+            if (!skipLoadAndSave) getUserSignature().then(setSignatureUrl);
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, []);
 
-    const editSignature = useCallback(async () => {
-        const { buttonType, value } = await chayns.dialog.signature({
-            buttons: [
-                {
-                    text: 'Speichern',
-                    buttonType: chayns.dialog.buttonType.POSITIVE,
-                },
-                {
-                    text: chayns.dialog.buttonText.CANCEL,
-                    buttonType: chayns.dialog.buttonType.NEGATIVE,
-                },
-            ],
-        });
+        const editSignature = useCallback(async () => {
+            const { buttonType, value } = await chayns.dialog.signature({
+                buttons: [
+                    {
+                        text: 'Speichern',
+                        buttonType: chayns.dialog.buttonType.POSITIVE,
+                    },
+                    {
+                        text: chayns.dialog.buttonText.CANCEL,
+                        buttonType: chayns.dialog.buttonType.NEGATIVE,
+                    },
+                ],
+            });
 
-        if (buttonType === chayns.dialog.buttonType.POSITIVE) {
-            let success = true;
-            if (!skipLoadAndSave) {
-                success = value
-                    ? await putUserSignature(value)
-                    : await deleteUserSignature();
+            if (buttonType === chayns.dialog.buttonType.POSITIVE) {
+                let success = true;
+                if (!skipLoadAndSave) {
+                    success = value
+                        ? await putUserSignature(value)
+                        : await deleteUserSignature();
+                }
+                if (success) {
+                    setSignatureUrl(value);
+                    onEdit?.(value);
+                }
+                return {
+                    success,
+                    value: value || null,
+                };
             }
-            if (success) {
-                setSignatureUrl(value);
-                onEdit?.(value);
-            }
+
             return {
-                success,
-                value: value || null,
+                success: false,
+                value: null,
             };
-        }
+        }, [skipLoadAndSave, onEdit]);
 
-        return {
-            success: false,
-            value: null,
-        };
-    }, [skipLoadAndSave, onEdit]);
+        const onButtonClick = useCallback(async () => {
+            if (!signatureUrl) {
+                const { success, value } = await editSignature();
 
-    const onButtonClick = useCallback(async () => {
-        if (!signatureUrl) {
-            const { success, value } = await editSignature();
-
-            if (success) {
+                if (success) {
+                    setSubscribed(true);
+                    onSubscribe?.(value);
+                }
+            } else {
                 setSubscribed(true);
-                onSubscribe?.(value);
+                onSubscribe?.(signatureUrl);
             }
-        } else {
-            setSubscribed(true);
-            onSubscribe?.(signatureUrl);
-        }
-    }, [signatureUrl, editSignature, onSubscribe]);
+        }, [signatureUrl, editSignature, onSubscribe]);
 
-    if (!chayns.env.user.isAuthenticated) {
+        useImperativeHandle(ref, () => ({
+            edit: editSignature,
+            delete: async () => {
+                await deleteUserSignature();
+                let success = true;
+                if (!skipLoadAndSave) {
+                    success = await deleteUserSignature();
+                }
+                if (success) {
+                    setSignatureUrl(null);
+                    onEdit?.(null);
+                }
+            },
+        }));
+
+        if (!chayns.env.user.isAuthenticated) {
+            return (
+                <div>
+                    <Button
+                        onClick={async () => {
+                            await new Promise((resolve) => {
+                                const cb = () => {
+                                    resolve();
+                                    chayns.removeAccessTokenChangeListener(cb);
+                                };
+                                chayns.addAccessTokenChangeListener(cb);
+                                chayns.login();
+                            });
+                            getUserSignature().then(setSignatureUrl);
+                        }}
+                    >
+                        Anmelden
+                    </Button>
+                </div>
+            );
+        }
+
+        if (!subscribed || !signatureUrl) {
+            return (
+                <div className={buttonWrapperClassName}>
+                    <Button onClick={onButtonClick} disabled={disabled}>
+                        {buttonText}
+                    </Button>
+                </div>
+            );
+        }
+
         return (
             <div>
-                <Button
-                    onClick={async () => {
-                        await new Promise((resolve) => {
-                            const cb = () => {
-                                resolve();
-                                chayns.removeAccessTokenChangeListener(cb);
-                            };
-                            chayns.addAccessTokenChangeListener(cb);
-                            chayns.login();
-                        });
-                        getUserSignature().then(setSignatureUrl);
-                    }}
-                >
-                    Anmelden
-                </Button>
+                <img
+                    src={signatureUrl}
+                    alt="signature"
+                    style={{ maxHeight: 130 }}
+                    onClick={editSignature}
+                />
             </div>
         );
     }
-
-    if (!subscribed || !signatureUrl) {
-        return (
-            <div className={buttonWrapperClassName}>
-                <Button onClick={onButtonClick} disabled={disabled}>
-                    {buttonText}
-                </Button>
-            </div>
-        );
-    }
-
-    return (
-        <div>
-            <img
-                src={signatureUrl}
-                alt="signature"
-                style={{ maxHeight: 130 }}
-                onClick={editSignature}
-            />
-        </div>
-    );
-};
+);
 
 Signature.propTypes = {
     /**
@@ -150,6 +177,10 @@ Signature.propTypes = {
      * callback which is called when the user edits his subscription
      */
     onEdit: PropTypes.func,
+    /**
+     * Forces to show signature on initial render
+     */
+    forceInitialShow: PropTypes.bool,
 };
 
 Signature.defaultProps = {
@@ -159,6 +190,7 @@ Signature.defaultProps = {
     skipLoadAndSave: false,
     onSubscribe: null,
     onEdit: null,
+    forceInitialShow: false,
 };
 
 Signature.displayName = 'Signature';
