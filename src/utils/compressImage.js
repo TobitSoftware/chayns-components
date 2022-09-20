eb0eec7cc0c5acd2f9b093745b983d0751e15221
Heiner Pöpping <heiner.poepping@tobit.software>
2022-09-20 13:56:13 +0200

    :bug: Fix problems with compress image worker and babel

--- src/utils/compressImage.js
+++ src/utils/compressImage.js
@@ -1,3 +1,135 @@
+/* Creates a worker script like with following method but ensures the code is not further transpiled which could cause problems
+
+let workerScript = [exportCanvasSized]
+        .map((f) => `const ${f.name} = ${f.toString()}`)
+        .join('\n');
+
+    workerScript += `;\n(${workerThread.toString()})()`;
+ */
+const workerScript = `
+const exportCanvasSized = async (
+    origCanvas,
+    fileName,
+    maxFileSize,
+    type,
+    quality,
+    isIOS
+) => {
+    let tempCanvas;
+    let scale = 1;
+    let step = 1;
+    let lastFitBlob = null;
+
+    if (isIOS) {
+        scale = Math.min(1200 / origCanvas.width, 1200 / origCanvas.height, 1);
+    }
+
+    for (let i = 0; i < 10; i += 1) {
+        // eslint-disable-next-line no-await-in-loop
+        await new Promise((resolve) => {
+            setTimeout(resolve, 10);
+        });
+
+        if (typeof document !== 'undefined') {
+            if (!tempCanvas) {
+                tempCanvas = document.createElement('canvas');
+                // eslint-disable-next-line no-loop-func
+                tempCanvas.convertToBlob = (options) =>
+                    canvasToBlob(tempCanvas, options);
+            }
+            tempCanvas.width = origCanvas.width * scale;
+            tempCanvas.height = origCanvas.height * scale;
+        } else if (typeof OffscreenCanvas === 'function') {
+            postMessage({
+                width: Math.round(origCanvas.width * scale),
+                height: Math.round(origCanvas.height * scale),
+                scale,
+            });
+            tempCanvas = new OffscreenCanvas(
+                Math.round(origCanvas.width * scale),
+                Math.round(origCanvas.height * scale)
+            );
+        } else {
+            throw Error('offscreen canvas not supported');
+        }
+
+        if (isIOS) {
+            if (tempCanvas.width > 1200 || tempCanvas.height > 1200) break;
+        }
+
+        tempCanvas
+            .getContext('2d')
+            .drawImage(
+                origCanvas,
+                0,
+                0,
+                origCanvas.width,
+                origCanvas.height,
+                0,
+                0,
+                tempCanvas.width,
+                tempCanvas.height
+            );
+
+        // eslint-disable-next-line no-await-in-loop
+        const blob = await tempCanvas.convertToBlob({ type, quality });
+        const blobSize = blob?.size ?? maxFileSize + 1;
+        if (blobSize > maxFileSize) {
+            step *= 0.5;
+            scale -= step;
+        } else if (blobSize < maxFileSize) {
+            lastFitBlob = blob;
+
+            if (scale === 1) {
+                break;
+            }
+
+            if (blobSize > maxFileSize * 0.8) {
+                break;
+            }
+
+            step *= 0.5;
+            scale += step;
+        }
+    }
+
+    if (!lastFitBlob) {
+        throw new Error(
+            'failed to compress image down to maxFileSize within 10 iterations'
+        );
+    }
+
+    return new File([lastFitBlob], fileName, { type });
+};
+(() => {
+    // eslint-disable-next-line no-restricted-globals
+    self.onmessage = async (ev) => {
+        const { file, maxFileSize, quality, isIOS } = ev.data;
+
+        const bitmap = await createImageBitmap(file);
+        const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(bitmap, 0, 0);
+
+        const type = file.type === 'image/jpg' ? 'image/jpg' : 'image/webp';
+
+        try {
+            const result = await exportCanvasSized(
+                canvas,
+                file.name,
+                maxFileSize,
+                type,
+                quality,
+                isIOS
+            );
+            postMessage({ success: true, file: result });
+        } catch (e) {
+            postMessage({ success: false, error: e });
+        }
+    };
+})()
+`;
+
 const blobToDataUrl = (blob) =>
     new Promise((resolve, reject) => {
         const fr = new FileReader();
@@ -185,12 +317,6 @@ const compressImage = async (file, maxFileSize, quality = 0.8) => {
             chayns.env.isIOS
         );
     }
-
-    let workerScript = [exportCanvasSized]
-        .map((f) => `const ${f.name} = ${f.toString()}`)
-        .join('\n');
-
-    workerScript += `\n(${workerThread.toString()})()`;
 
     const blob = new Blob([workerScript], { type: 'application/javascript' });
     const url = URL.createObjectURL(blob);
