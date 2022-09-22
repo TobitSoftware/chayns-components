f802adb4731e563ecf825c362cf86522d156c3db
Heiner Pöpping <heiner.poepping@tobit.software>
2022-09-22 15:51:12 +0200

    :bug: Fix overlay positioning when parentElement is document.body

--- src/react-chayns-input_box/component/InputBox.jsx
+++ src/react-chayns-input_box/component/InputBox.jsx
@@ -129,13 +129,21 @@ const InputBox = React.forwardRef((props, ref) => {
 
     const parentElement = !isServer() ? parent || document.body : null;
     const parentRect = parentElement?.getBoundingClientRect();
-    const isParentRelative = parentElement ? ['absolute', 'relative'].includes(getComputedStyle(parentElement).position) : false;
+    const isParentRelative = parentElement
+        ? ['absolute', 'relative'].includes(
+              getComputedStyle(parentElement).position
+          )
+        : !isServer() && parentElement === document.body;
 
     const positionStyles = rect
         ? {
               width: `${rect.width}px`,
-              top: `${rect.bottom - (isParentRelative ? (parentRect?.top ?? 0) : 0)}px`,
-              left: `${rect.left - (isParentRelative ? (parentRect?.left ?? 0) : 0)}px`,
+              top: `${
+                  rect.bottom - (isParentRelative ? parentRect?.top ?? 0 : 0)
+              }px`,
+              left: `${
+                  rect.left - (isParentRelative ? parentRect?.left ?? 0 : 0)
+              }px`,
           }
         : null;
 
