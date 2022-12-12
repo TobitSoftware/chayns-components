bcc81ed01b0ac809a72579b2292e04e233665e4a
Jannik Weise <jannik.weise@tobit.com>
2022-12-12 14:40:49 +0100

    :sparkles: Add "maxHeight" prop for AccordionContent component

--- packages/core/src/components/accordion/accordion-content/AccordionContent.styles.ts
+++ packages/core/src/components/accordion/accordion-content/AccordionContent.styles.ts
@@ -1,11 +1,20 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import type { WithTheme } from '../../color-scheme-provider/ColorSchemeProvider';
+import type { AccordionContentProps } from './AccordionContent';
 
 type StyledAccordionContentProps = WithTheme<{
     isWrapped: boolean;
-}>;
+}> &
+    Pick<AccordionContentProps, 'maxHeight'>;
 
 export const StyledAccordionContent = styled.div<StyledAccordionContentProps>`
     color: ${({ theme }: StyledAccordionContentProps) => theme.text};
     padding: ${({ isWrapped }) => (isWrapped ? '0 9px 18px 26px' : '0 9px 9px 10px')};
+
+    ${({ maxHeight }) =>
+        typeof maxHeight === 'number' &&
+        css`
+            max-height: ${maxHeight}px;
+            overflow-y: scroll;
+        `}
 `;
--- packages/core/src/components/accordion/accordion-content/AccordionContent.tsx
+++ packages/core/src/components/accordion/accordion-content/AccordionContent.tsx
@@ -7,12 +7,17 @@ export type AccordionContentProps = {
      * The content of the accordion content element
      */
     children: ReactNode;
+    maxHeight?: number;
 };
 
-const AccordionContent: FC<AccordionContentProps> = ({ children }) => (
+const AccordionContent: FC<AccordionContentProps> = ({ children, maxHeight }) => (
     <AccordionContext.Consumer>
         {({ isWrapped }) => (
-            <StyledAccordionContent className="beta-chayns-accordion-content" isWrapped={isWrapped}>
+            <StyledAccordionContent
+                className="beta-chayns-accordion-content"
+                isWrapped={isWrapped}
+                maxHeight={maxHeight}
+            >
                 {children}
             </StyledAccordionContent>
         )}
