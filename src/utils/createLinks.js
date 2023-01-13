0c7d4ec73c0620655c12edeeb3df7270b83bce01
Heiner Pöpping <heiner.poepping@tobit.software>
2023-01-13 13:56:19 +0100

    :bug: Fix white-space issues in createLinks

--- src/utils/createLinks.js
+++ src/utils/createLinks.js
@@ -1,21 +1,20 @@
-const URL_REGEX = /(\s|^)(?:http(s)?:\/\/|www\.)[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+/gi;
+const URL_REGEX =
+    /(\s|^)(?:http(s)?:\/\/|www\.)[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+/gi;
 
 const makeLinks = (txt) => {
     let text = txt;
-    const urls = text.match(URL_REGEX);
+    let urls = text.match(URL_REGEX);
     if (chayns.utils.isArray(urls)) {
         const indexArray = [];
+        urls = urls.map((url) => url.trim());
         urls.forEach((url) => {
-            const space = url.startsWith('w') || url.startsWith('h') ? 0 : 1;
-            indexArray.push(text.indexOf(url) + space);
-            text = text.replace(url, space ? ' ' : '');
+            indexArray.push(text.indexOf(url));
+            text = text.replace(url, '');
         });
         urls.reverse();
         indexArray.reverse();
         urls.forEach((url, index) => {
-            let link = url.substring(
-                url.startsWith('w') || url.startsWith('h') ? 0 : 1
-            );
+            let link = url;
             if (!link.startsWith('http')) {
                 link = `https://${url}`;
             }
