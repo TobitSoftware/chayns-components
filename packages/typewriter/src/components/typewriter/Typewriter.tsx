59035affd8a9ead123484c27e00e70d043fc8e20
Jannik Weise <jannik.weise@tobit.com>
2023-01-31 16:03:48 +0100

    :sparkles: Add stop animation on click in Typewriter component

--- packages/typewriter/src/components/typewriter/Typewriter.tsx
+++ packages/typewriter/src/components/typewriter/Typewriter.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useMemo, useState } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import {
     StyledTypewriter,
     StyledTypewriterPseudoText,
@@ -15,36 +15,47 @@ export type TypewriterProps = {
 
 const Typewriter: FC<TypewriterProps> = ({ children }) => {
     const [shownCharCount, setShownCharCount] = useState(0);
+    const [shouldStopAnimation, setShouldStopAnimation] = useState(false);
+
+    const isAnimatingText = shownCharCount !== children.length;
+
+    const handleClick = useCallback(() => {
+        setShouldStopAnimation(true);
+    }, []);
+
+    useEffect(() => {
+        let interval: number | undefined;
+
+        if (shouldStopAnimation) {
+            setShownCharCount(children.length);
+        } else {
+            setShownCharCount(0);
+
+            interval = window.setInterval(() => {
+                setShownCharCount((prevState) => {
+                    const nextState = prevState + 1;
+
+                    if (nextState === children.length) {
+                        window.clearInterval(interval);
+                    }
+
+                    return nextState;
+                });
+            }, 35);
+        }
+
+        return () => {
+            window.clearInterval(interval);
+        };
+    }, [children.length, shouldStopAnimation]);
 
     const shownText = useMemo(
         () => getSubTextFromHTML(children, shownCharCount),
         [children, shownCharCount]
     );
 
-    useEffect(() => {
-        setShownCharCount(0);
-
-        const interval = window.setInterval(() => {
-            setShownCharCount((prevState) => {
-                const nextState = prevState + 1;
-
-                if (nextState === children.length) {
-                    window.clearInterval(interval);
-                }
-
-                return nextState;
-            });
-        }, 35);
-
-        return () => {
-            window.clearInterval(interval);
-        };
-    }, [children.length]);
-
-    const isAnimatingText = shownCharCount !== children.length;
-
     return (
-        <StyledTypewriter>
+        <StyledTypewriter onClick={handleClick}>
             <StyledTypewriterText
                 dangerouslySetInnerHTML={{ __html: shownText }}
                 isAnimatingText={isAnimatingText}
