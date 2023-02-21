ff42754b46dbc0343259833efaaf460c214a33ef
Jannik Weise <jannik.weise@tobit.com>
2023-02-21 15:47:28 +0100

    ♻️ Update EmojiInput story

--- packages/emoji-input/stories/EmojiInput.stories.tsx
+++ packages/emoji-input/stories/EmojiInput.stories.tsx
@@ -6,10 +6,31 @@ export default {
     component: EmojiInput,
     args: {
         placeholder: 'Nachricht schreiben',
-        value: 'Ein Test mit 🔥',
+        value: 'Mein 🐧 ist cool',
     },
 } as ComponentMeta<typeof EmojiInput>;
 
-const Template: ComponentStory<typeof EmojiInput> = ({ ...args }) => <EmojiInput {...args} />;
+const Template: ComponentStory<typeof EmojiInput> = ({ ...args }) => (
+    <>
+        <h1>Emoji Input Example</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+            sanctus est. Lorem ipsum dolor sit amet. Consetetur sadipscing elitr, sed diam nonumy
+            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+            vero eos et accusam et justo duo dolores et ea rebum invidunt. At vero eos et accusam et
+            justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.
+            Lorem ipsum dolor sit amet. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+            sanctus est. Lorem ipsum dolor sit amet. Consetetur sadipscing elitr, sed diam nonumy
+            eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem
+            ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        </p>
+        <EmojiInput {...args} />
+    </>
+);
 
 export const General = Template.bind({});
