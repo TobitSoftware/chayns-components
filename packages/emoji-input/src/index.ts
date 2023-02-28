e9803d9f42b511346d5b20fd5b522a20bf2ed435
Jannik Weise <jannik.weise@tobit.com>
2023-02-28 14:22:49 +0100

    ✨ Add export convertAsciiToUnicode function

--- packages/emoji-input/src/index.ts
+++ packages/emoji-input/src/index.ts
@@ -1,2 +1,3 @@
 export { default as EmojiInput } from './components/emoji-input/EmojiInput';
 export { PopupAlignment } from './constants/alignment';
+export { convertAsciiToUnicode } from './utils/emoji';
