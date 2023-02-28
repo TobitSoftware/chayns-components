a277df28fcfff79099fd62f0969b1b9327826f28
Jannik Weise <jannik.weise@tobit.com>
2023-02-28 14:24:07 +0100

    ✨ Add export sub components of emoji-input

--- packages/emoji-input/src/index.ts
+++ packages/emoji-input/src/index.ts
@@ -1,3 +1,5 @@
 export { default as EmojiInput } from './components/emoji-input/EmojiInput';
+export { default as EmojiPickerPopup } from './components/emoji-picker-popup/EmojiPickerPopup';
+export { default as EmojiPicker } from './components/emoji-picker/EmojiPicker';
 export { PopupAlignment } from './constants/alignment';
 export { convertAsciiToUnicode } from './utils/emoji';
