9fcf82107908c34811cdf6a5c8c99f74bebf64a0
Jannik Weise <jannik.weise@tobit.com>
2023-03-06 16:33:44 +0100

    ✨ Update LazyMotionWrapper

--- packages/core/src/components/lazy-motion-wrapper/LazyMotionWrapper.tsx
+++ packages/core/src/components/lazy-motion-wrapper/LazyMotionWrapper.tsx
@@ -1,14 +1,12 @@
-import { LazyMotion } from 'framer-motion';
+import { domAnimation, LazyMotion } from 'framer-motion';
 import React, { FC, ReactNode } from 'react';
 
 export type LazyMotionWrapperProps = {
     children: ReactNode;
 };
 
-const loadFeatures = () => import('../../utils/motion-features').then((res) => res.default);
-
 const LazyMotionWrapper: FC<LazyMotionWrapperProps> = ({ children }) => (
-    <LazyMotion features={loadFeatures}>{children}</LazyMotion>
+    <LazyMotion features={domAnimation}>{children}</LazyMotion>
 );
 
 LazyMotionWrapper.displayName = 'LazyMotionWrapper';
