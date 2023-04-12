e17178b5dc384160cf0ecd67d0cd2e2c295d92de
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-04-12 14:56:38 +0200

    💄Add styles for gallery

--- packages/gallery/src/components/gallery/Gallery.styles.ts
+++ packages/gallery/src/components/gallery/Gallery.styles.ts
@@ -1,3 +1,4 @@
+import type { WithTheme } from '@chayns-components/core';
 import styled from 'styled-components';
 
 export const StyledGallery = styled.div`
@@ -12,9 +13,15 @@ export const StyledGalleryItem = styled.div`
     position: relative;
 `;
 
-export const StyledGalleryItemDeleteButton = styled.button`
-    background-color: white;
-    opacity: 0.7;
+type StyledGalleryItemDeleteButtonProps = WithTheme<unknown>;
+
+export const StyledGalleryItemDeleteButton = styled.button<StyledGalleryItemDeleteButtonProps>`
+    background-color: rgba(
+        ${({ theme }: StyledGalleryItemDeleteButtonProps) => theme['000-rgb']},
+        0.75
+    );
+    box-shadow: 0 0 0 1px
+        rgba(${({ theme }: StyledGalleryItemImageProps) => theme['009-rgb']}, 0.08) inset;
     position: absolute;
     top: 0;
     right: 0;
@@ -23,26 +30,35 @@ export const StyledGalleryItemDeleteButton = styled.button`
     width: 30px;
 `;
 
-export const StyledGalleryItemAdd = styled.button`
+type StyledGalleryItemAddProps = WithTheme<unknown>;
+
+export const StyledGalleryItemAdd = styled.button<StyledGalleryItemAddProps>`
+    background-color: ${({ theme }: StyledGalleryItemAddProps) => theme['101']};
+    box-shadow: 0 0 0 1px
+        rgba(${({ theme }: StyledGalleryItemImageProps) => theme['009-rgb']}, 0.08) inset;
     width: 150px;
     height: 150px;
-    border: darkslateblue solid 2px;
-    border-radius: 2px;
 `;
 
-export const StyledGalleryItemImage = styled.img`
+type StyledGalleryItemImageProps = WithTheme<unknown>;
+
+export const StyledGalleryItemImage = styled.img<StyledGalleryItemImageProps>`
+    background-color: ${({ theme }: StyledGalleryItemImageProps) => theme['101']};
+    box-shadow: 0 0 0 1px
+        rgba(${({ theme }: StyledGalleryItemImageProps) => theme['009-rgb']}, 0.08) inset;
     z-index: 1;
     width: 150px;
     aspect-ratio: 1 / 1;
     object-fit: cover;
-    border: darkslateblue solid 2px;
-    border-radius: 2px;
 `;
 
-export const StyledGalleryItemVideo = styled.video`
+type StyledGalleryItemVideoProps = WithTheme<unknown>;
+
+export const StyledGalleryItemVideo = styled.video<StyledGalleryItemVideoProps>`
+    background-color: ${({ theme }: StyledGalleryItemVideoProps) => theme['101']};
+    box-shadow: 0 0 0 1px
+        rgba(${({ theme }: StyledGalleryItemImageProps) => theme['009-rgb']}, 0.08) inset;
     width: 150px;
     aspect-ratio: 1 / 1;
     object-fit: cover;
-    border: darkslateblue solid 2px;
-    border-radius: 2px;
 `;
