c0074ff05f9a620efd9e407f75f62449eb9b942d
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-04-13 17:28:45 +0200

    ✨ Update gallery storie

--- packages/gallery/stories/Gallery.stories.tsx
+++ packages/gallery/stories/Gallery.stories.tsx
@@ -5,7 +5,22 @@ import Gallery from '../src/components/gallery/Gallery';
 export default {
     title: 'Gallery/Gallery',
     component: Gallery,
-    args: {},
+    args: {
+        files: [
+            {
+                id: 1,
+                url: 'https://tsimg.cloud/77896-21884/8aee1a304297729a4542b97325940a656a3da8f2.png',
+            },
+            {
+                id: 2,
+                url: 'https://tsimg.cloud/77896-21884/54a117f35e5fb57520e64471461af5491c0eff06.png',
+            },
+            {
+                id: 3,
+                url: 'https://tsimg.cloud/77896-21884/25399416f38c1d960f521a3530c8a2bc70a88bb9.png',
+            },
+        ],
+    },
 } as unknown as ComponentMeta<typeof Gallery>;
 
 const Template: ComponentStory<typeof Gallery> = ({ children, ...args }) => (
@@ -13,3 +28,9 @@ const Template: ComponentStory<typeof Gallery> = ({ children, ...args }) => (
 );
 
 export const General = Template.bind({});
+
+export const EditMode = Template.bind({});
+
+EditMode.args = {
+    editMode: true,
+};
