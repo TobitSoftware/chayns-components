8ebaaa61649293f9aa81f80a24549f5d0bec0cfa
Jannik Weise <jannik.weise@tobit.com>
2023-05-04 14:37:55 +0200

    🐛 Replace ZERO WIDTH SPACE character in converHTMLToText function

--- packages/emoji-input/src/utils/text.ts
+++ packages/emoji-input/src/utils/text.ts
@@ -19,6 +19,8 @@ export const convertHTMLToText = (text: string) => {
 
     result = result.replace(HTML_LC_MENTION_REGEX, '[lc_mention id="$1"]$2[/lc_mention]');
 
+    result = result.replace(/\u200B/, '');
+
     result = unescapeHTML(result);
 
     const element = document.createElement('div');
