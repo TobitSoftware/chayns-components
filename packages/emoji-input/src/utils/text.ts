a2778de30653194e359e77a131bda7baf30ae27c
Jannik Weise <jannik.weise@tobit.com>
2023-05-04 16:42:12 +0200

    🐛 Fix convertTextToHTML

--- packages/emoji-input/src/utils/text.ts
+++ packages/emoji-input/src/utils/text.ts
@@ -2,18 +2,18 @@ import { BB_LC_MENTION_REGEX, HTML_LC_MENTION_REGEX } from '../constants/regex';
 import { unescapeHTML } from './emoji';
 
 export const convertTextToHTML = (text: string) => {
-    let result = text;
+    const element = document.createElement('div');
+
+    element.innerText = text;
+
+    let result = element.innerHTML;
 
     result = result.replace(
         BB_LC_MENTION_REGEX,
         '<lc_mention contenteditable="false" id="$1"><span>@</span>$2</lc_mention>'
     );
 
-    const element = document.createElement('div');
-
-    element.innerText = result;
-
-    return element.innerHTML;
+    return result;
 };
 
 export const convertHTMLToText = (text: string) => {
