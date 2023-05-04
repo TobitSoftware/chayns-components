b92d12a10dbbea55dbd95ca130d3974dcc5de02f
Jannik Weise <jannik.weise@tobit.com>
2023-05-04 16:15:09 +0200

    🐛 Fix handle line breaks in convertHTMLToText and convertTextToHTML

--- packages/emoji-input/src/utils/text.ts
+++ packages/emoji-input/src/utils/text.ts
@@ -4,14 +4,16 @@ import { unescapeHTML } from './emoji';
 export const convertTextToHTML = (text: string) => {
     let result = text;
 
-    result = result.replace(/\n/g, '<br>');
-
     result = result.replace(
         BB_LC_MENTION_REGEX,
         '<lc_mention contenteditable="false" id="$1"><span>@</span>$2</lc_mention>'
     );
 
-    return result;
+    const element = document.createElement('div');
+
+    element.innerText = result;
+
+    return element.innerHTML;
 };
 
 export const convertHTMLToText = (text: string) => {
@@ -20,7 +22,6 @@ export const convertHTMLToText = (text: string) => {
     result = result.replace(HTML_LC_MENTION_REGEX, '[lc_mention id="$1"]$2[/lc_mention]');
 
     result = result.replace(/\u200B/g, '');
-    result = result.replace(/<br>/g, '\n');
 
     result = unescapeHTML(result);
 
