3ed8ca872390e3f4f6ac940a794dee77466f0ed1
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-05-17 10:44:47 +0200

    ✨ Add stories for AmountControl component

--- packages/core/stories/AmountControl.stories.tsx
+++ packages/core/stories/AmountControl.stories.tsx
@@ -4,11 +4,21 @@ import AmountControl from '../src/components/amount-control/AmountControl';
 export default {
     title: 'Core/AmountControl',
     component: AmountControl,
-    args: {
-        label: '1,43',
-    },
+    args: {},
 } as ComponentMeta<typeof AmountControl>;
 
 const Template: ComponentStory<typeof AmountControl> = (args) => <AmountControl {...args} />;
 
 export const General = Template.bind({});
+
+export const WithLabel = Template.bind({});
+
+export const WithMaxAmount = Template.bind({});
+
+WithLabel.args = {
+    label: '1,43',
+};
+
+WithMaxAmount.args = {
+    maxAmount: 1,
+};
