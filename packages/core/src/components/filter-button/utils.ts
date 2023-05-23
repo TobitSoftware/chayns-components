e80ee1f2eb71820d101def4a5cef15d53bd4d6c0
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-05-23 13:31:28 +0200

    🔥 Delete unused utils file in filter-button

--- packages/core/src/components/filter-button/utils.ts
+++
@@ -1,9 +0,0 @@
-import type { IFilterButtonItem } from './interface';
-
-interface SelectFilterButtonItemByKeyOptions {
-    key: string;
-    items: IFilterButtonItem[];
-}
-
-export const selectFilterButtonItemByKey = ({ key, items }: SelectFilterButtonItemByKeyOptions) =>
-    items.find(({ id }) => id === key);
