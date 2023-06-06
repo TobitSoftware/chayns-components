3cc1f8895a4e5877dd851a1a0551d02acb324c9e
Heiner Pöpping <heiner.poepping@tobit.com>
2023-06-06 15:21:17 +0200

    Change signature of imageUpload function

--- src/utils/imageUpload.js
+++ src/utils/imageUpload.js
@@ -3,20 +3,37 @@
  *
  * @export
  * @param {string | File} file A URL as a string or a `File` object that should be uploaded.
- * @param {string} referenceId
- * @param {string} personId A person is that should be associated with the uploaded image.
- * @param {string} siteId A site id that should be associated with the uploaded image.
- * @param {string} [url='https://api.tsimg.cloud/image']
+ * @param {object} options either object or referenceId (deprecated)
+ * @param {string} options.referenceId
+ * @param {string} options.personId A person is that should be associated with the uploaded image.
+ * @param {string} options.siteId A site id that should be associated with the uploaded image.
+ * @param {string} [options.url='https://api.tsimg.cloud/image']
+ * @param {string} _personId A person is that should be associated with the uploaded image. (deprecated, use options instead)
+ * @param {string} _siteId A site id that should be associated with the uploaded image. (deprecated, use options instead)
+ * @param {string} [_url='https://api.tsimg.cloud/image'] (deprecated, use options instead)
  * @return {Promise<object>} The response data for the image upload.
  */
 export default async function imageUpload(
     file,
-    referenceId,
-    personId,
-    siteId,
-    url = 'https://api.tsimg.cloud/image'
+    options,
+    _personId,
+    _siteId,
+    _url = 'https://api.tsimg.cloud/image'
 ) {
     const headers = new Headers({ Accept: 'application/json' });
+
+    let referenceId;
+    let personId;
+    let siteId;
+    let url;
+    if (typeof options === 'object') {
+        ({ referenceId, personId, siteId, url = 'https://api.tsimg.cloud/image' } = options);
+    } else {
+        referenceId = options;
+        personId = _personId;
+        siteId = _siteId;
+        url = _url;
+    }
 
     if (referenceId) headers.set('X-Reference-Id', referenceId);
     if (personId) headers.set('X-Person-Id', personId);
