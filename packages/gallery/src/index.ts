fb809613fe9605497f69bc80cc35e3e9cecde594
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-06-06 11:21:10 +0200

    🔥 Remove export from Gallery

--- packages/gallery/src/index.ts
+++ packages/gallery/src/index.ts
@@ -1,4 +1,2 @@
-export { uploadFile } from '@chayns-components/core/src/utils/uploadFile';
 export { default as Gallery } from './components/Gallery';
 export type { FileItem, Image, Meta, Video } from './types/file';
-export { selectFiles } from './utils/file';
