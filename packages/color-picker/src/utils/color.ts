2dd5e2528ab9d2581346037bc32c602d205555bc
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-07-04 09:26:11 +0200

    ✨ Add regex to check for color type and convert to hsl

--- packages/color-picker/src/utils/color.ts
+++ packages/color-picker/src/utils/color.ts
@@ -1,3 +1,4 @@
+import { hexToHsl, rgb255ToHsl } from '@chayns/colors';
 import type { RefObject } from 'react';
 import type { Coordinates } from '../types';
 
@@ -10,20 +11,60 @@ export const getColorFromCoordinates = ({
     coordinates,
     canvas,
 }: GetColorFromCoordinatesOptions) => {
-    const { offsetTop, offsetLeft } = canvas.current;
-    const x = coordinates.x - offsetTop;
-    const y = coordinates.y - offsetLeft;
-    const c = canvas.current?.getContext('2d');
-    const p = c?.getImageData(x, y, 1, 1).data;
-
-    if (!p) {
+    if (!canvas.current) {
         return undefined;
     }
 
-    // If transparency on the image
-    if (p[0] === 0 && p[1] === 0 && p[2] === 0 && p[3] === 0) {
+    const { offsetTop, offsetLeft } = canvas.current;
+
+    const x = coordinates.x - offsetTop;
+    const y = coordinates.y - offsetLeft;
+
+    const ctx = canvas.current?.getContext('2d');
+    const pixels = ctx?.getImageData(x, y, 1, 1).data;
+
+    if (!pixels) {
+        return undefined;
+    }
+
+    // If transparency
+    if (pixels[0] === 0 && pixels[1] === 0 && pixels[2] === 0 && pixels[3] === 0) {
         return 'transparent';
     }
 
-    return `rgba(${p[0] ?? 0}, ${p[1] ?? 0}, ${p[2] ?? 0}, ${p[3] ?? 0})`;
+    return `rgba(${pixels[0] ?? 0}, ${pixels[1] ?? 0}, ${pixels[2] ?? 0}, ${pixels[3] ?? 0})`;
+};
+
+export const convertColorToHsl = (color: string) => {
+    const hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+    const rgbRegex =
+        /^rgb(a)?\(\s*((25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\s*,\s*){2}(25[0-5]|2[0-4]\d|1\d{2}|\d{1,2})\s*(,\s*\d+(\.\d+)?)?\)$/i;
+    const hslRegex =
+        /^hsla?\(\s*((360|3[0-5]\d|[12]\d{2}|[1-9]\d|\d)\s*,\s*){2}(360|3[0-5]\d|[12]\d{2}|[1-9]\d|\d)\s*(,\s*\d+(\.\d+)?)?\)$/i;
+
+    let rgba: string[] | null;
+
+    const newColor = color.replaceAll('%', '');
+
+    switch (true) {
+        case hexRegex.test(newColor):
+            return hexToHsl(color);
+        case rgbRegex.test(newColor):
+            rgba = color.match(/[\d.]+/g);
+
+            if (!rgba) {
+                return undefined;
+            }
+
+            return rgb255ToHsl({
+                r: Number(rgba[0]),
+                g: Number(rgba[1]),
+                b: Number(rgba[2]),
+                a: Number(rgba[3]),
+            });
+        case hslRegex.test(newColor):
+            return color;
+        default:
+            return undefined;
+    }
 };
