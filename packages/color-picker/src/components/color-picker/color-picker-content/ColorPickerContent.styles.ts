bb69866f0f7b496e1d1b4c739cdd8c8e185a5a06
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-07-27 14:53:27 +0200

    🔥 Remove everything exept HueSlider

--- packages/color-picker/src/components/color-picker/color-picker-content/ColorPickerContent.styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledColorPickerContent = styled.div`
-    padding: 8px;
-
-    > div:not(:first-child) {
-        margin-top: 8px;
-    }
-`;
--- packages/color-picker/src/components/color-picker/color-picker-content/color-presets/ColorPresents.styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledColorPresets = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 4px;
-    width: 300px; // ToDo: Find better solution to fix wrapping of dots at correct width
-`;
