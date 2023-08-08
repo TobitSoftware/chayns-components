61d853021290fd0c7bde1e63f7c8b734957872bb
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-08-08 15:54:42 +0200

    Revert "🐛 Fix Accordion content animating height if new content is added while open"
    This reverts commit d9cec039c8453958c24c74954f36c17c12522b19.

--- packages/core/src/components/accordion/accordion-body/AccordionBody.tsx
+++ packages/core/src/components/accordion/accordion-body/AccordionBody.tsx
@@ -1,4 +1,4 @@
-import React, { FC, UIEvent, useEffect, useMemo, useRef, useState } from 'react';
+import React, { FC, UIEvent, useMemo } from 'react';
 import type { AccordionProps } from '../Accordion';
 import { AccordionGroupContext } from '../accordion-group/AccordionGroup';
 import { StyledMotionAccordionBody } from './AccordionBody.styles';
@@ -29,31 +29,9 @@ const AccordionBody: FC<AccordionBodyProps> = ({
         []
     );
 
-    const contentRef = useRef<HTMLDivElement | null>(null);
-    const [height, setHeight] = useState<number | 'auto'>('auto');
-
-    useEffect(() => {
-        if (contentRef.current) {
-            const resizeObserver = new ResizeObserver((entries) => {
-                if (entries && entries[0]) {
-                    const observedHeight = entries[0].contentRect.height;
-                    setHeight(observedHeight);
-                }
-            });
-
-            resizeObserver.observe(contentRef.current);
-
-            return () => {
-                resizeObserver.disconnect();
-            };
-        }
-
-        return () => {};
-    }, []);
-
     return (
         <StyledMotionAccordionBody
-            animate={{ height: shouldHideBody ? '0' : height, opacity: shouldHideBody ? 0 : 1 }}
+            animate={{ height: shouldHideBody ? '0' : 'auto', opacity: shouldHideBody ? 0 : 1 }}
             className="beta-chayns-accordion-body"
             exit={{ height: 0, opacity: 0 }}
             initial={{ height: 0, opacity: 0 }}
@@ -61,7 +39,7 @@ const AccordionBody: FC<AccordionBodyProps> = ({
             onScroll={onScroll}
         >
             <AccordionGroupContext.Provider value={AccordionGroupContextProviderValue}>
-                <div ref={contentRef}>{children}</div>
+                {children}
             </AccordionGroupContext.Provider>
         </StyledMotionAccordionBody>
     );
