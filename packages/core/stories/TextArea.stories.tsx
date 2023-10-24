d0fb3640314e06ee3d4b9759771cebb3b988a245
Simon <simon.vahlbrock@tobit.com>
2023-10-24 17:20:08 +0200

    🔥 Remove TypeWriter import from core package

--- packages/core/stories/TextArea.stories.tsx
+++ packages/core/stories/TextArea.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import TextArea from '../src/components/text-area/TextArea';
-import { Typewriter } from '@chayns-components/typewriter/lib';
 
 export default {
     title: 'Core/TextArea',
@@ -16,22 +15,6 @@ export const General = Template.bind({});
 
 export const MaxHeight = Template.bind({});
 
-export const PlaceholderElement = Template.bind({});
-
 MaxHeight.args = {
     maxHeight: '200px',
-};
-
-PlaceholderElement.args = {
-    maxHeight: '200px',
-    placeholder: (
-        <Typewriter
-            children={[
-                'Hmm, ich würde sagen...',
-                'Ich bin mir nicht ganz sicher...',
-                'Lass mich kurz nachdenken...',
-                'Nochmal von vorne...',
-            ]}
-        />
-    ),
 };
