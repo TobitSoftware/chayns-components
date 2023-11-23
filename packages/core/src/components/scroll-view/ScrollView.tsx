a550fce24c5ed38159474ec219444fb62eddbae4
Michael Gesenhues <michael.gesenhues@tobit.com>
2023-11-23 16:36:39 +0100

    🐛 Fix missing children prop types

--- packages/core/src/components/scroll-view/ScrollView.tsx
+++ packages/core/src/components/scroll-view/ScrollView.tsx
@@ -1,7 +1,11 @@
-import React, { CSSProperties, FC, useMemo } from 'react';
+import React, { CSSProperties, FC, ReactNode, useMemo } from 'react';
 import { StyledScrollView } from './ScrollView.styles';
 
 export type ScrollViewProps = {
+    /**
+     * The elements that should be shown inside the scrollview
+     */
+    children: ReactNode;
     /**
      * The maximum height of the scroll view.
      */
@@ -11,7 +15,7 @@ export type ScrollViewProps = {
 const ScrollView: FC<ScrollViewProps> = ({ maxHeight = '300px', children }) =>
     useMemo(
         () => <StyledScrollView maxHeight={maxHeight}>{children}</StyledScrollView>,
-        [children, maxHeight]
+        [children, maxHeight],
     );
 
 ScrollView.displayName = 'ScrollView';
