ee9d49356789bf003baa0c8de65194adae174780
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-02-01 10:37:32 +0100

    ✨ Allow month switch only in range

--- packages/calendar/src/components/calendar/Calendar.tsx
+++ packages/calendar/src/components/calendar/Calendar.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
-import { isBefore, type Locale } from 'date-fns';
+import { isBefore, isSameMonth, type Locale } from 'date-fns';
 import { de } from 'date-fns/locale';
 import { StyledCalendar } from './Calendar.styles';
 import Month from './month/Month';
@@ -28,9 +28,12 @@ const Calendar: FC<CalendarProps> = ({ locale = de, endDate, startDate, highligh
                 return prevDate;
             }
 
-            const date = new Date();
+            const newDate = new Date(prevDate);
+            newDate.setMonth(prevDate.getMonth() - 1);
 
-            const newDate = new Date(date.setMonth(prevDate.getMonth() - 1));
+            if (prevDate.getMonth() === 0 && newDate.getMonth() === 11) {
+                newDate.setFullYear(prevDate.getFullYear() - 1);
+            }
 
             return isDateInRange({ startDate, endDate, currentDate: newDate });
         });
@@ -42,9 +45,12 @@ const Calendar: FC<CalendarProps> = ({ locale = de, endDate, startDate, highligh
                 return prevDate;
             }
 
-            const date = new Date();
+            const newDate = new Date(prevDate);
+            newDate.setMonth(prevDate.getMonth() + 1);
 
-            const newDate = new Date(date.setMonth(prevDate.getMonth() + 1));
+            if (prevDate.getMonth() === 11 && newDate.getMonth() === 0) {
+                newDate.setFullYear(prevDate.getFullYear() + 1);
+            }
 
             return isDateInRange({ startDate, endDate, currentDate: newDate });
         });
@@ -63,19 +69,20 @@ const Calendar: FC<CalendarProps> = ({ locale = de, endDate, startDate, highligh
                 year={year}
                 onLeftArrowClick={handleLeftArrowClick}
                 onRightArrowClick={handleRightArrowClick}
-                shouldShowLeftArrow
-                shouldShowRightArrow={isBefore(currentDate, endDate)}
+                shouldShowLeftArrow={!isSameMonth(currentDate, startDate)}
+                shouldShowRightArrow={!isSameMonth(currentDate, endDate)}
                 locale={locale}
                 highlightedDates={highlightedDates}
             />
         );
     }, [
-        highlightedDates,
         currentDate,
-        endDate,
         handleLeftArrowClick,
         handleRightArrowClick,
+        startDate,
+        endDate,
         locale,
+        highlightedDates,
     ]);
 
     return <StyledCalendar>{months}</StyledCalendar>;
