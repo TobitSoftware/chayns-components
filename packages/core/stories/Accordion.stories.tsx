f5e32c8a90ffe45742017d132919f3969f31ddda
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-02-14 14:40:44 +0100

    ✨ Force new version

--- packages/core/stories/Accordion.stories.tsx
+++ packages/core/stories/Accordion.stories.tsx
@@ -110,8 +110,7 @@ WrappedAccordions.args = {
             sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
             sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
             aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-            amet.
+            rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor si amet.
         </AccordionContent>,
         <AccordionGroup>
             <Accordion isWrapped title="At vero eos et accusam">
