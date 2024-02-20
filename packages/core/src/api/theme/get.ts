d20de62f80e7b67d1747ca3adf49782b7ee4ef97
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-02-20 15:52:01 +0100

    ✨ Update url to load designsettings

--- packages/core/src/api/theme/get.ts
+++ packages/core/src/api/theme/get.ts
@@ -4,7 +4,7 @@ import type { DesignSettings } from '../../types/colorSchemeProvider';
 export const getDesignSettings = async (): Promise<DesignSettings | undefined> => {
     const { id } = getSite();
 
-    const response = await fetch(`https://api-qa.chayns.net/css/${id}/style/v2`, {
+    const response = await fetch(`https://api.chayns.net/css/${id}/style/v2`, {
         method: 'GET',
     });
 
