985c816d7bc2422e9e335d6f31965f623f73643f
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-03-21 08:58:15 +0100

    🐛 Fix ContentCard styles

--- packages/core/src/components/content-card/ContentCard.styles.ts
+++ packages/core/src/components/content-card/ContentCard.styles.ts
@@ -1,12 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import type { WithTheme } from '../color-scheme-provider/ColorSchemeProvider';
 
 type StyledContentCardProps = WithTheme<unknown>;
 
-export const StyledContentCard = styled.div`
-    background-color: rgba(${({ theme }: StyledContentCardProps) => theme['100-rgb']}, ${({ theme }: StyledContentCardProps) => theme.cardBackgroundOpacity};
-    border-radius: ${({ theme }: StyledContentCardProps) => theme.cardBorderRadius}px;
-    box-shadow: 0 2px 6px 0 rgba(0, 0, 0, ${({ theme }: StyledContentCardProps) => theme.cardShadow});
+export const StyledContentCard = styled.div<StyledContentCardProps>`
+    ${({ theme }: StyledContentCardProps) => css`
+        background-color: rgba(${theme['100-rgb']}, ${theme.cardBackgroundOpacity});
+        border-radius: ${theme.cardBorderRadius}px;
+        box-shadow: 0 2px 6px 0 rgba(0, 0, 0, ${theme.cardShadow});
+    `}
+
     padding: 8px 12px;
 
     &:not(:last-child) {
