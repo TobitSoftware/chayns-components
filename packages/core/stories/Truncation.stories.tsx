f7197fd681062f366bdff4eaffc3ad0f78660ad2
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-03-21 13:06:54 +0100

    ✨ Add Truncation story with size changing children

--- packages/core/stories/Truncation.stories.tsx
+++ packages/core/stories/Truncation.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryFn } from '@storybook/react';
-import { Button } from '../lib';
+import { Accordion, AccordionContent, Button } from '../lib';
 import Truncation from '../src/components/truncation/Truncation';
 
 const BASE_HTML_TEXT = (
@@ -33,10 +33,38 @@ const Template: StoryFn<typeof Truncation> = ({ children, ...args }) => (
 );
 
 export const General = Template.bind({});
+export const AnimatedChildren = Template.bind({});
 
 export const SmallText = Template.bind({});
 
 export const JustText = Template.bind({});
+
+AnimatedChildren.args = {
+    children: (
+        <>
+            <p id="isPasted">
+                Der legendärste Abend: Homecoming at next! Studenten und Ausreißer, Urlauber und
+                Daheimgebliebene, Partymäuse und Partymuffel – sie alle zieht es am Tag vor
+                Heiligabend in die Heimat an einen zuvor ausgemachten Ort, um all die guten Freunde
+                und alte Bekannte wiederzutreffen.
+            </p>
+            <Accordion title="Lorem">
+                <AccordionContent>
+                    Der legendärste Abend: Homecoming at next! Studenten und Ausreißer, Urlauber und
+                    Daheimgebliebene, Partymäuse und Partymuffel – sie alle zieht es am Tag vor
+                    Heiligabend in die Heimat an einen zuvor ausgemachten Ort, um all die guten
+                    Freunde und alte Bekannte wiederzutreffen.
+                </AccordionContent>
+            </Accordion>
+            <p>
+                Was damals vor vielen Jahren auf der StattAlm auf dem Campus in Ahaus begann, führen
+                wir bei uns im next fort!&nbsp;
+            </p>
+            <p>Alle Infos und Tickets zum Event in Kürze.&nbsp;</p>
+            <Button onClick={() => alert('hallo')}>test</Button>
+        </>
+    ),
+};
 
 SmallText.args = {
     children: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>,
