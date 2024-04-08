5f80230eb7f407c2aa23f4935e60f63a9ba5d282
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-04-08 10:20:58 +0200

    💄 Add gap between inputs in OpeningTimes

--- packages/date/src/components/opening-times/opening-inputs/OpeningInputs.styles.ts
+++ packages/date/src/components/opening-times/opening-inputs/OpeningInputs.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const StyledOpeningInputs = styled.div`
     display: flex;
     flex-direction: column;
+    gap: 8px;
 `;
 
 export const StyledOpeningInputPreview = styled.div``;
