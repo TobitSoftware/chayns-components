5b61c695cb8ff17dadbaa57cf7a8ee6eb3602db7
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-04-11 10:49:33 +0200

    ✨ Add TrancparencySlider

---
+++ packages/color-picker/src/components/transparency-slider/TransparencySlider.styles.ts
@@ -0,0 +1,123 @@
+import type { WithTheme } from '@chayns-components/core';
+import type { CSSProperties } from 'react';
+import styled from 'styled-components';
+
+export const StyledTransparencySlider = styled.div`
+    width: 100%;
+    height: 30px;
+    cursor: pointer;
+    position: relative;
+    display: flex;
+    align-items: center;
+`;
+
+export const StyledTransparencySliderBackground = styled.div`
+    height: 10px;
+    background-color: #fff;
+    background-image: linear-gradient(45deg, #a0a0a0 25%, #0000 0),
+        linear-gradient(-45deg, #a0a0a0 25%, #0000 0), linear-gradient(45deg, #0000 75%, #a0a0a0 0),
+        linear-gradient(-45deg, #0000 75%, #a0a0a0 0);
+    background-position:
+        0 0,
+        0 4px,
+        4px -4px,
+        -4px 0;
+    background-repeat: repeat;
+    background-size: 8px 8px;
+    content: '';
+    position: absolute;
+    width: 100%;
+`;
+
+type StyledTransparencySliderInputProps = WithTheme<{ $color?: string }>;
+
+export const StyledTransparencySliderInput = styled.input<StyledTransparencySliderInputProps>`
+    width: 100%;
+    border-radius: 100px;
+    -webkit-appearance: none;
+    height: 10px;
+    background: linear-gradient(90deg, ${({ $color }) => $color}, transparent);
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: 0.2s;
+    transition: opacity 0.2s;
+    z-index: 4;
+
+    // Slider thumb for chrome
+    &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        border-radius: 50%;
+    }
+
+    // slider thumb for firefox
+    &::-moz-range-thumb {
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        border-radius: 50%;
+    }
+`;
+
+type StyledTransparencySliderThumbWrapperProps = WithTheme<{
+    $position: number;
+}>;
+
+export const StyledTransparencySliderThumbWrapper = styled.div.attrs<StyledTransparencySliderThumbWrapperProps>(
+    ({ $position }) => ({
+        style: {
+            left: $position,
+        },
+    }),
+)`
+    position: absolute;
+    min-width: 20px;
+    height: 20px;
+    cursor: pointer;
+    border-radius: 100px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+    pointer-events: none;
+    z-index: 3;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    padding: 0 8px;
+    white-space: nowrap;
+`;
+
+export const StyledTransparencySliderThumbBackground = styled.div`
+    position: absolute;
+    background-color: #fff;
+    background-image: linear-gradient(45deg, #a0a0a0 25%, #0000 0),
+        linear-gradient(-45deg, #a0a0a0 25%, #0000 0), linear-gradient(45deg, #0000 75%, #a0a0a0 0),
+        linear-gradient(-45deg, #0000 75%, #a0a0a0 0);
+    background-position:
+        0 0,
+        0 4px,
+        4px -4px,
+        -4px 0;
+    background-repeat: repeat;
+    background-size: 8px 8px;
+    height: 100%;
+    width: 100%;
+`;
+
+type StyledTransparencySliderThumbProps = WithTheme<{
+    $color: CSSProperties['color'];
+}>;
+
+export const StyledTransparencySliderThumb = styled.div.attrs<StyledTransparencySliderThumbProps>(
+    ({ $color }) => ({
+        style: {
+            backgroundColor: $color,
+        },
+    }),
+)`
+    height: 100%;
+    width: 100%;
+    position: absolute;
+`;
---
+++ packages/color-picker/src/components/transparency-slider/TransparencySlider.tsx
@@ -0,0 +1,103 @@
+import React, { ChangeEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { splitRgb } from '../../utils/color';
+import {
+    StyledTransparencySlider,
+    StyledTransparencySliderBackground,
+    StyledTransparencySliderInput,
+    StyledTransparencySliderThumb,
+    StyledTransparencySliderThumbBackground,
+    StyledTransparencySliderThumbWrapper,
+} from './TransparencySlider.styles';
+
+interface TransparencySliderProps {
+    color: string;
+    onChange?: (color: string) => void;
+}
+
+const TransparencySlider = ({ onChange, color }: TransparencySliderProps) => {
+    const [editedValue, setEditedValue] = useState(0);
+    const [pureColor, setPureColor] = useState<string>();
+    const [previewColor, setPreviewColor] = useState<string>();
+
+    const sliderThumbRef = useRef<HTMLDivElement>(null);
+    const sliderRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (color) {
+            const rgb = splitRgb(color);
+
+            if (!rgb) {
+                return;
+            }
+
+            const { r, g, b, a } = rgb;
+
+            const newColor = `rgba(${r}, ${g}, ${b}, ${(100 - a) / 100})`;
+
+            setPreviewColor(newColor);
+            setPureColor(`rgb(${r},${g},${b},1)`);
+            setEditedValue(100 - a * 100);
+        }
+    }, [color]);
+
+    const handleInputChange = useCallback(
+        (event: ChangeEvent<HTMLInputElement>) => {
+            const a = Number(event.target.value);
+
+            setEditedValue(a);
+
+            const rgb = splitRgb(pureColor);
+
+            if (!rgb) {
+                return;
+            }
+
+            const { r, g, b } = rgb;
+
+            const newColor = `rgba(${r}, ${g}, ${b}, ${(100 - a) / 100})`;
+
+            setPreviewColor(newColor);
+
+            if (typeof onChange === 'function') {
+                onChange(newColor);
+            }
+        },
+        [onChange, pureColor],
+    );
+
+    const sliderThumbPosition = useMemo(() => {
+        if (sliderRef.current && sliderThumbRef.current) {
+            return (
+                (editedValue / 100) *
+                (sliderRef.current.offsetWidth - sliderThumbRef.current.offsetWidth / 2)
+            );
+        }
+        return 0;
+    }, [editedValue]);
+
+    return (
+        <StyledTransparencySlider>
+            <StyledTransparencySliderInput
+                ref={sliderRef}
+                $color={pureColor}
+                type="range"
+                min={0}
+                max={100}
+                value={editedValue}
+                onChange={handleInputChange}
+            />
+            <StyledTransparencySliderBackground />
+            <StyledTransparencySliderThumbWrapper
+                ref={sliderThumbRef}
+                $position={sliderThumbPosition}
+            >
+                <StyledTransparencySliderThumbBackground />
+                <StyledTransparencySliderThumb $color={previewColor} />
+            </StyledTransparencySliderThumbWrapper>
+        </StyledTransparencySlider>
+    );
+};
+
+TransparencySlider.displayName = 'TransparencySlider';
+
+export default TransparencySlider;
