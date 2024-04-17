2799c680242900f3f3d5a5e4ac92887b02b5005e
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-04-17 16:01:41 +0200

    ✨ Add presetcolor type export

--- packages/color-picker/src/components/index.ts
+++ packages/color-picker/src/components/index.ts
@@ -1,3 +1,4 @@
+export type { IPresetColor as PresetColor } from '../types';
 export { default as ColorPicker } from './color-picker/ColorPicker';
 export { default as HueSlider } from './hue-slider/HueSlider';
 export { default as TransparencySlider } from './transparency-slider/TransparencySlider';
