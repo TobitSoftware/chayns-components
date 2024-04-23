e7ec79e298abb34168b46d013c7e33fa5fe0a6ec
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-04-23 17:28:28 +0200

    ✨ Update image service

--- packages/core/src/api/image/post.ts
+++ packages/core/src/api/image/post.ts
@@ -1,6 +1,5 @@
 import { getAccessToken, getSite, getUser } from 'chayns-api';
 import type { Meta } from '../../types/file';
-import { getFileAsArrayBuffer } from '../../utils/fileDialog';
 
 export interface PostImageResult {
     key: string;
@@ -8,10 +7,38 @@ export interface PostImageResult {
     meta?: Meta;
 }
 
+interface Result {
+    requestId: string;
+    image: Image;
+    baseDomain: string;
+    signature: Signature;
+}
+
+export interface Image {
+    path: string;
+    width: number;
+    height: number;
+    size: number;
+    hash: string;
+    preview: string;
+    isSecured: boolean;
+    format: string;
+    fileExtension: string;
+}
+
+export interface Signature {
+    verify: string;
+    signature: string;
+    expires: Date;
+}
+
 interface PostImageOptions {
     file: File;
     shouldUploadImageToSite?: boolean;
 }
+
+const IMAGE_SERVICE_API_V3_URL = 'https://cube.tobit.cloud/image-service/v3/Images';
+const IMAGE_RESIZER_API_URL = 'https://cube.tobit.cloud/image-resizer-backend/api/v1.0/image';
 
 /**
  * Uploads an image to the tsimg cloud service
@@ -28,27 +55,39 @@ export const postImage = async ({
         return undefined;
     }
 
-    let head: { [key: string]: string } = { 'X-Person-Id': user.personId };
+    // const body = await getFileAsArrayBuffer(file);
 
-    if (shouldUploadImageToSite && site.id) {
-        head = { 'X-Site-Id': site.id };
-    }
+    const body = new FormData();
 
-    const body = await getFileAsArrayBuffer(file);
+    body.append('File', file);
 
-    const response = await fetch('https://api.tsimg.cloud/image', {
+    const url =
+        file.size > 10 * 1024 * 1024
+            ? `${IMAGE_RESIZER_API_URL}/${shouldUploadImageToSite ? site.id : user.personId}`
+            : `${IMAGE_SERVICE_API_V3_URL}/${shouldUploadImageToSite ? site.id : user.personId}`;
+
+    const response = await fetch(url, {
         body,
         headers: {
             Accept: 'application/json',
             Authorization: `bearer ${accessToken}`,
-            'Content-Type': 'image/*',
-            ...head,
         },
         method: 'POST',
     });
 
     if (response.ok) {
-        return (await response.json()) as PostImageResult;
+        const { image, baseDomain } = (await response.json()) as Result;
+        // Maps image-service-api-v3 result to v2 result.
+        return {
+            key: image.path,
+            // Removes trailing slash from baseDomain, since image-service-api-v2 doesn't have a trailing slash on base.
+            base: baseDomain.endsWith('/') ? baseDomain.slice(0, -1) : baseDomain,
+            meta: {
+                preview: image.preview,
+                width: String(image.width),
+                height: String(image.height),
+            },
+        };
     }
 
     throw Error(`Failed to POST image (status code: ${response.status}).`);
