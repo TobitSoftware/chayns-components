2a066b7f73fba88c109488d51f3779753e2ad661
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-05-29 16:54:01 +0200

    Revert "🔥 Remove color, speed and size prop from SmallWaitCursor"
    This reverts commit 7e779d3b0445ebcef34b73865f3a2248ccd4b22a.

--- packages/core/src/components/small-wait-cursor/SmallWaitCursor.tsx
+++ packages/core/src/components/small-wait-cursor/SmallWaitCursor.tsx
@@ -1,24 +1,52 @@
-import React, { FC } from 'react';
+// noinspection JSUnusedGlobalSymbols
+
+import React, { CSSProperties, FC } from 'react';
 import {
     StyledSmallWaitCursor,
     StyledSmallWaitCursorBackground,
     StyledSmallWaitCursorWaitCursor,
 } from './SmallWaitCursor.styles';
 
+export enum SmallWaitCursorSize {
+    Small = 16,
+    Medium = 30,
+}
+
+export enum SmallWaitCursorSpeed {
+    Slow = 1.5,
+    Medium = 1,
+    Fast = 0.5,
+}
+
 export type SmallWaitCursorProps = {
+    color?: CSSProperties['color'];
+    /**
+     * Specifies whether the wait cursor should be displayed with a background.
+     */
     shouldHideBackground?: boolean;
     /**
      * Specifies whether the wait cursor should be displayed.
      */
     shouldHideWaitCursor?: boolean;
+    /**
+     * The size of the wait cursor in pixels. Use the SmallWaitCursorSize enum for this prop.
+     */
+    size?: SmallWaitCursorSize | number;
+    /**
+     * The speed of the animation in seconds. Use the SmallWaitCursorSpeed enum for this prop.
+     */
+    speed?: SmallWaitCursorSpeed;
 };
 
 const SmallWaitCursor: FC<SmallWaitCursorProps> = ({
+    color,
     shouldHideBackground = false,
     shouldHideWaitCursor = false,
+    size = SmallWaitCursorSize.Medium,
+    speed = SmallWaitCursorSpeed.Medium,
 }) => (
-    <StyledSmallWaitCursor $shouldShowWaitCursor={!shouldHideWaitCursor}>
-        <StyledSmallWaitCursorWaitCursor />
+    <StyledSmallWaitCursor $shouldShowWaitCursor={!shouldHideWaitCursor} $size={size}>
+        <StyledSmallWaitCursorWaitCursor $color={color} $size={size} $speed={speed} />
         {!shouldHideBackground && <StyledSmallWaitCursorBackground />}
     </StyledSmallWaitCursor>
 );
