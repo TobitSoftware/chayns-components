9d3b0cf6014dbf2c7b42735d20030810262d175f
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-06-03 09:45:06 +0200

    ✨ Add color mapper function

--- packages/core/src/utils/font.ts
+++ packages/core/src/utils/font.ts
@@ -1,4 +1,4 @@
-import { IconStyle } from '../types/colorSchemeProvider';
+import { IconStyle, type ParagraphFormat } from '../types/colorSchemeProvider';
 
 export interface Font {
     family: string;
@@ -98,6 +98,82 @@ export const getFontSize = () => {
     document.body.removeChild(element);
 
     return fontSize.replace('px', '');
+};
+
+const hexToRgb = (hex: string) => {
+    let r: number;
+    let g: number;
+    let b: number;
+    let a: number;
+
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})?$/i.exec(hex);
+
+    if (result) {
+        r = parseInt(result[1] ?? '0', 16);
+        g = parseInt(result[2] ?? '0', 16);
+        b = parseInt(result[3] ?? '0', 16);
+        a = result[4] ? parseInt(result[4], 16) / 100 : 1;
+
+        return { r, g, b, a };
+    }
+
+    return undefined;
+};
+
+export const getHeadlineColorSelector = (data: ParagraphFormat[]) => {
+    const themeResult: { [key: string]: string } = {};
+    const colorResult: { [key: string]: string } = {};
+    const headlineMap: { [key: string]: string } = {
+        h1: 'headline-1',
+        h2: 'headline-2',
+        h3: 'headline-3',
+        h4: 'headline-4',
+        h5: 'headline-5',
+        h6: 'headline-6',
+        p: 'text',
+        '.button': 'buttonColor',
+    };
+
+    data.forEach(({ selector, color, backgroundColor }) => {
+        const selectors = selector.split(',');
+
+        const tag = selectors[selectors.length - 1]?.trim();
+
+        const path = headlineMap[tag ?? ''];
+
+        if (!path) {
+            return;
+        }
+
+        if (path === 'buttonColor' && backgroundColor) {
+            themeResult[path] = backgroundColor;
+
+            return;
+        }
+
+        if (!color) {
+            return;
+        }
+
+        const hexPath = `--chayns-color--${path}`;
+        const rgbPath = `--chayns-color-rgb--${path}`;
+
+        colorResult[hexPath] = color;
+        themeResult[path] = color;
+
+        const rgb = hexToRgb(color);
+
+        if (!rgb) {
+            return;
+        }
+
+        const { r, g, b } = rgb;
+
+        colorResult[rgbPath] = `${r}, ${g}, ${b}`;
+        themeResult[`${path}-rgb`] = `${r}, ${g}, ${b}`;
+    });
+
+    return { colorResult, themeResult };
 };
 
 export const generateFontFaces = () =>
