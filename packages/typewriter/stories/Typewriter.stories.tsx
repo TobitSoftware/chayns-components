fc550fb7e51a659141ff88ce7757413701fc9101
Jannik Weise <jannik.weise@tobit.com>
2024-06-05 10:24:19 +0200

    ✨ Add example with code highlighter in Typewriter stories

--- packages/typewriter/stories/Typewriter.stories.tsx
+++ packages/typewriter/stories/Typewriter.stories.tsx
@@ -1,6 +1,6 @@
+import { CodeHighlighter } from '@chayns-components/code-highlighter';
 import { TextArea } from '@chayns-components/core';
 import { Meta, StoryFn } from '@storybook/react';
-import { ChangeEvent, useState } from 'react';
 import Typewriter from '../src/components/typewriter/Typewriter';
 
 export default {
@@ -31,6 +31,8 @@ export const HTMLText = Template.bind({});
 export const MultipleTexts = Template.bind({});
 
 export const WithOwnStyles = Template.bind({});
+
+export const WithCodeHighlighter = Template.bind({});
 
 export const InsideTextArea = TextAreaTemplate.bind({});
 
@@ -85,4 +87,35 @@ WithOwnStyles.args = {
     textStyle: {
         color: 'red',
     },
+};
+
+const code = `import { CodeHighlighter } from '@chayns-components/code-highlighter';
+
+const AppWrapper = () => {
+    const { color, colorMode } = getSite();
+
+    return (
+        <CodeHighlighter
+            code={code}
+            language="jsx"
+            theme={CodeHighlighterTheme.Dark}
+        />
+    )
+}
+
+export default AppWrapper;`;
+
+WithCodeHighlighter.args = {
+    children: (
+        <>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            <CodeHighlighter shouldShowLineNumbers code={code} language={'tsx'} />
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            <button onClick={() => alert('Button clicked')}>Button</button>
+            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no
+            sea takimata sanctus est.
+        </>
+    ),
 };
