bcaee93721b2adb50429807d4b3414f578515d95
Leopold Pinkernell <leopold.pinkernell@tobit.com>
2024-06-11 14:52:39 +0200

    :bug: Prevent drop from inserting html

--- packages/emoji-input/src/components/emoji-input/EmojiInput.tsx
+++ packages/emoji-input/src/components/emoji-input/EmojiInput.tsx
@@ -385,6 +385,32 @@ const EmojiInput = forwardRef<EmojiInputRef, EmojiInputProps>(
             [isDisabled],
         );
 
+        const handleDrop = useCallback((event: DragEvent) => {
+            if (editorRef.current) {
+                event.preventDefault();
+
+                if (isDisabled) {
+                    event.stopPropagation();
+
+                    return;
+                }
+
+                let text = event.dataTransfer?.getData('text');
+
+                if (!text) {
+                    return;
+                }
+
+                text = convertEmojisToUnicode(text);
+
+                insertTextAtCursorPosition({ editorElement: editorRef.current, text });
+
+                const newEvent = new Event('input', { bubbles: true });
+
+                editorRef.current.dispatchEvent(newEvent);
+            }
+        }, []);
+
         /**
          * This function uses the 'insertTextAtCursorPosition' function to insert the emoji at the
          * correct position in the editor element.
@@ -692,6 +718,7 @@ const EmojiInput = forwardRef<EmojiInputRef, EmojiInputProps>(
                         onInput={handleInput}
                         onKeyDown={handleKeyDown}
                         onPaste={handlePaste}
+                        onDrop={handleDrop}
                         ref={editorRef}
                         $shouldShowContent={isPrefixAnimationFinished}
                         transition={{ type: 'tween', duration: 0.2 }}
