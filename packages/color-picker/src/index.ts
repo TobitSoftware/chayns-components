e0a98f5983e2ece0b1e0c2c31b2a32fab91f8ea5
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-06-13 13:08:15 +0200

    ✨ Add export for ColorPicker converter

--- packages/color-picker/src/index.ts
+++ packages/color-picker/src/index.ts
@@ -1,4 +1,5 @@
 export { default as ColorPicker } from './components/color-picker/ColorPicker';
 export { default as HueSlider } from './components/hue-slider/HueSlider';
 export { default as TransparencySlider } from './components/transparency-slider/TransparencySlider';
-export type { IPresetColor as PresetColor } from './types/colorPicker';
+export type { IPresetColor as PresetColor, RGB, RGBA } from './types/colorPicker';
+export { hexToRgb, rgbToHex } from './utils/color';
