0300aeebec72dd7809aa66579bc21950ad43495d
Leopold Pinkernell <leopold.pinkernell@tobit.com>
2024-07-08 16:45:14 +0200

    :fire: Remove unused code

--- packages/format/src/utils/escape.ts
+++ packages/format/src/utils/escape.ts
@@ -1,7 +1,2 @@
 export const escapeHtmlInText = (text: string): string =>
     text.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-
-export const MESSAGE_CONVERSION_LINE_BREAK = '<br is-replaced-linebreak>';
-export const MESSAGE_CONVERSION_LINE_BREAK_ESCAPED = escapeHtmlInText(
-    MESSAGE_CONVERSION_LINE_BREAK,
-);
