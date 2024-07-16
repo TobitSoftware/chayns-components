bc26abd620b453903551d1540542f0fc776a5eef
Jannik Weise <jannik.weise@tobit.com>
2024-07-16 16:50:21 +0200

    💄 Update font size of weekday in Calender component
    In the old components, the day of the week was only displayed in 90 percent font size.

--- packages/date/src/components/calendar/month-wrapper/month/weekday-wrapper/weekday/Weekday.styles.ts
+++ packages/date/src/components/calendar/month-wrapper/month/weekday-wrapper/weekday/Weekday.styles.ts
@@ -1,10 +1,11 @@
-import styled from 'styled-components';
 import type { WithTheme } from '@chayns-components/core';
+import styled from 'styled-components';
 
 type StyledWeekdayProps = WithTheme<unknown>;
 
 export const StyledWeekday = styled.div<StyledWeekdayProps>`
-    text-align: center;
     color: ${({ theme }: StyledWeekdayProps) => theme.headline};
     cursor: default;
+    font-size: 90%;
+    text-align: center;
 `;
