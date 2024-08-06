c4415d242c22cc648f42857418739af0befd4ace
Bernd Verwohlt <bernd.verwohlt@tobit.com>
2024-08-06 11:38:28 +0200

    :sparkles: add docs for DevalueSlider.tsx

--- packages/devalue-slider/src/components/DevalueSlider.tsx
+++ packages/devalue-slider/src/components/DevalueSlider.tsx
@@ -9,11 +9,36 @@ export type OnChangeHandler = (relativeValue: number) => void;
 export type OnCompleteHandler = () => void;
 
 export type DevalueSliderProps = {
+    /**
+     * The basic color of the slider.
+     * This color is the background of the track before the slider is devalued.
+     */
     color?: string;
+    /**
+     * The devalue color of the slider.
+     * This color fills the track from the left on user movement.
+     * This color is the background of the timer after the slider is devalued.
+     */
     devalueColor?: string;
+    /**
+     * If this slider was devalued, provide the time when it was devalued.
+     * This will show a timer.
+     */
     devalueTime?: Date;
+    /**
+     * This function is called when the slider is devalued.
+     */
     onDevalue?: OnDevalueHandler;
+    /**
+     * This function is called when the slider value changes.
+     * With this function you can keep track of the movement of the slider.
+     */
     onChange?: OnChangeHandler;
+    /**
+     * This function is called when the slider is completed.
+     * The slider is completed when the user devalues the slider
+     * and the animation is completed.
+     */
     onComplete?: OnCompleteHandler;
 };
 
