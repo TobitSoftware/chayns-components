cfb4a8503c19269d7ca4949a1b41235764c2ea0a
Heiner Pöpping <heiner.poepping@tobit.com>
2024-08-08 18:58:08 +0200

    :bug: Fix default for position in contextMenu

--- src/react-chayns-contextmenu/component/ContextMenu.jsx
+++ src/react-chayns-contextmenu/component/ContextMenu.jsx
@@ -24,7 +24,7 @@ const ContextMenu = React.forwardRef(
     (
         {
             parent,
-            position: userPosition,
+            position: userPosition = null,
             minWidth,
             maxWidth,
             className,
