da21f07eeb3f6c1be39694b45d2cd89a037e2ca2
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-09 15:28:38 +0200

    ✨ Add useDateInfo export

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -1,5 +1,6 @@
 export { default as Calendar } from './components/calendar/Calendar';
 export { default as DateInfo } from './components/date-info/DateInfo';
 export { default as OpeningTimes } from './components/opening-times/OpeningTimes';
+export { useDateInfo } from './hooks/useDateInfo';
 export type { Categories, HighlightedDates, HighlightedDateStyles } from './types/calendar';
 export type { HintTextPosition, OpeningTime, Time, Weekday } from './types/openingTimes';
