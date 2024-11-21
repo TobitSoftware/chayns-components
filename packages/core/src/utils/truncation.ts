d72f36431fd9bce1e4d2670912fdb76d5ced782f
BerndVerwohlt <bernd.verwohlt@tobit.com>
2024-11-21 09:56:47 +0100

    :bug: fix truncation for a tags in text

--- packages/core/src/utils/truncation.ts
+++ packages/core/src/utils/truncation.ts
@@ -7,31 +7,42 @@ const doesElementHasOnlyText = (element: HTMLElement): boolean => {
         // If element has text (not empty), it is only text.
         return element.textContent !== '';
     }
+
     // Element has child elements or no text, so it's not only text.
     return false;
 };
 
 const removeLastLeafElement = (element: HTMLElement) => {
     // remove last element of html element where the last element is a leaf element and its content is a string
-    const lastChild: Element | null = element.lastElementChild;
+    const { lastElementChild, lastChild } = element;
+
     if (
-        lastChild &&
-        !doesElementHasOnlyText(lastChild as HTMLElement) &&
-        lastChild.hasChildNodes()
+        lastElementChild &&
+        !doesElementHasOnlyText(lastElementChild as HTMLElement) &&
+        lastElementChild.hasChildNodes()
     ) {
-        removeLastLeafElement(lastChild as HTMLElement);
+        removeLastLeafElement(lastElementChild as HTMLElement);
     } else if (
         lastChild &&
-        doesElementHasOnlyText(lastChild as HTMLElement) &&
+        lastChild.nodeType === Node.TEXT_NODE &&
         lastChild.textContent &&
         lastChild.textContent.length > 25
     ) {
-        lastChild.textContent = `${lastChild.textContent.substring(
+        lastChild.textContent = `${lastChild.textContent.substring(0, lastChild.textContent.length - 25)} ...`;
+    } else if (
+        lastElementChild &&
+        doesElementHasOnlyText(lastElementChild as HTMLElement) &&
+        lastElementChild.textContent &&
+        lastElementChild.textContent.length > 25
+    ) {
+        lastElementChild.textContent = `${lastElementChild.textContent.substring(
             0,
-            lastChild.textContent.length - 25,
+            lastElementChild.textContent.length - 25,
         )} ...`;
     } else if (lastChild) {
         element.removeChild(lastChild);
+    } else if (lastElementChild) {
+        element.removeChild(lastElementChild);
     }
 };
 export const truncateElement = (element: HTMLElement, referenceHeight: number) => {
