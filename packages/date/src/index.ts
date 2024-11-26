c5733d830a8649eb548a22d2f36db3bb68053f1d
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-11-26 09:26:10 +0100

    ✨ Update CalendarType export

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -3,10 +3,6 @@ export { default as DateInfo } from './components/date-info/DateInfo';
 export { default as OpeningInputs } from './components/opening-times/opening-inputs/OpeningInputs';
 export { default as OpeningTimes } from './components/opening-times/OpeningTimes';
 export { useDateInfo } from './hooks/useDateInfo';
-export type {
-    CalendarType,
-    Categories,
-    HighlightedDates,
-    HighlightedDateStyles,
-} from './types/calendar';
+export type { Categories, HighlightedDates, HighlightedDateStyles } from './types/calendar';
+export { CalendarType } from './types/calendar';
 export type { HintTextPosition, OpeningTime, Time, Weekday } from './types/openingTimes';
