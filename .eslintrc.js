60115850d4e289f76f2632b3aceafec480a73c01
Heiner Pöpping <heiner.poepping@tobit.com>
2025-02-11 18:23:21 +0100

    :wrench: Disable react in scope eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
         'no-void': 'off',
         // We organize imports on commit so this is not needed.
         'import/order': 'off',
+        'react/react-in-jsx-scope': 'off',
     },
     extends: ['@chayns-toolkit', 'plugin:storybook/recommended'],
 };
