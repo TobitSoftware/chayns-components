bd6917c32702004a8982a8fcc89f1950a2992186
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-04-15 15:41:50 +0200

    ✨Exports HTML escaping utility
    Exports a utility function for escaping HTML within text strings.
    
    This makes the function available for use in other modules.

--- packages/format/src/index.ts
+++ packages/format/src/index.ts
@@ -1,3 +1,4 @@
 export { BB_CODE_HTML_TAG_PREFIX } from './constants/format';
 export type { TableObject } from './types/format';
 export { formatStringToHtml } from './utils/formatString/formatString';
+export { escapeHtmlInText } from './utils/escape';
