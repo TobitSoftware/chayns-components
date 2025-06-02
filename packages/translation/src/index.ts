592697d3336ad98c7b1d06daa45223256814962f
Heiner Pöpping <heiner.poepping@tobit.com>
2025-06-02 16:56:12 +0200

    Add export for translateText-function

--- packages/translation/src/index.ts
+++ packages/translation/src/index.ts
@@ -1,2 +1,6 @@
+import translationHandler from './utils/translationHandler';
+
 export { useAdaptiveTranslation } from './hooks/useAdaptiveTranslation';
 export { default as AdaptiveTranslation } from './components/AdaptiveTranslation';
+export const translateText = (text: string, from: string, to: string) =>
+    translationHandler.translateText(text, from, to);
