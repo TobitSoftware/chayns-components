c30927b8586c90eae71f6f0a64d26b0ec89d2dc2
Heiner Pöpping <heiner.poepping@tobit.com>
2025-06-04 15:07:41 +0200

    Update story to use chayns-api language as default

--- packages/translation/stories/AdaptiveTranslation.stories.tsx
+++ packages/translation/stories/AdaptiveTranslation.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryFn } from '@storybook/react';
+import { useLanguage } from 'chayns-api';
 import React from 'react';
-import { AdaptiveTranslation } from '../src';
+import { AdaptiveTranslation, TranslationOptionsProvider } from '../src';
 
 export default {
     title: 'Translation/AdaptiveTranslation',
@@ -8,9 +9,15 @@ export default {
     args: {},
 } as Meta<typeof AdaptiveTranslation>;
 
-const Template: StoryFn<typeof AdaptiveTranslation> = ({ ...args }) => (
-    <AdaptiveTranslation {...args} />
-);
+const Template: StoryFn<typeof AdaptiveTranslation> = ({ ...args }) => {
+    const language = useLanguage();
+
+    return (
+        <TranslationOptionsProvider from={language.site} to={language.active}>
+            <AdaptiveTranslation {...args} />
+        </TranslationOptionsProvider>
+    );
+};
 
 export const General = Template.bind({});
 
