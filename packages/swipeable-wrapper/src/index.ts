d3bc7e56f7bdce81be502cfbb54607d529351d4b
JegorSch <jegor.schweizer@tobit.com>
2025-07-16 14:24:33 +0200

    🏷️Exports the SwipeableActionItem type.

--- packages/swipeable-wrapper/src/index.ts
+++ packages/swipeable-wrapper/src/index.ts
@@ -1,1 +1,3 @@
 export { default as SwipeableWrapper } from './components/swipeable-wrapper/SwipeableWrapper';
+
+export type { SwipeableActionItem } from './components/swipeable-wrapper/SwipeableWrapper';
