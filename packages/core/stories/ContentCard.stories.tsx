b8850f1c8b5dcbc92ac2671e99079024d9130591
Jannik Weise <jannik.weise@tobit.com>
2025-07-30 15:20:41 +0200

    ✨ Add ContentCard story with input
    Adds a new story to showcase the ContentCard component with an input field.
    
    This demonstrates how the ContentCard can be used to display content alongside interactive elements like input fields, providing a more versatile use case.

--- packages/core/stories/ContentCard.stories.tsx
+++ packages/core/stories/ContentCard.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryFn } from '@storybook/react';
 import ContentCard from '../src/components/content-card/ContentCard';
 import React from 'react';
+import { Input } from '../src';
 
 export default {
     title: 'Core/ContentCard',
@@ -16,3 +17,23 @@ const Template: StoryFn<typeof ContentCard> = ({ children, ...args }) => (
 );
 
 export const General = Template.bind({});
+
+export const ContentCardWithInput = Template.bind({});
+
+ContentCardWithInput.args = {
+    children: (
+        <>
+            <span>
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+            </span>
+            <div style={{ height: '10px' }} />
+            <Input placeholder="Your advertisement could be here..." />
+            <div style={{ height: '10px' }} />
+            <span>
+                At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                no sea takimata sanctus est.
+            </span>
+        </>
+    ),
+};
