c95d55947e022d9720df1b08afb5221251496553
TLensing <tom.lensing@tobit.com>
2025-07-31 10:36:27 +0200

    Add load and sync emoji convert

--- packages/emoji-input/src/utils/convertEmojisToUnicodeAsync.ts
+++ packages/emoji-input/src/utils/convertEmojisToUnicodeAsync.ts
@@ -1,16 +1,20 @@
 ﻿import { convertEmojisToUnicode } from './emoji';
-import { loadEmojiShortList } from './asyncEmojiData';
+import { loadEmojiShortList as loadList } from './asyncEmojiData';
 
 let regShortnames: RegExp | undefined;
 let shortNameList: Record<string, string> | undefined;
 
-const getEmojiShortList = async () => {
-    if ( regShortnames &&  shortNameList ) return { regShortnames, shortNameList };
-    ({ regShortnames, shortNameList } = await loadEmojiShortList());
-    return { regShortnames, shortNameList };
+export const loadEmojiShortList = async () => {
+    if (regShortnames && shortNameList) return;
+    ({ regShortnames, shortNameList } = await loadList());
 }
 
 export const convertEmojisToUnicodeAsync = async (text: string) => {
-    const {regShortnames: reg, shortNameList: shortNames} = await getEmojiShortList();
-    return convertEmojisToUnicode(text, reg, shortNames);
+    await loadEmojiShortList();
+    return convertEmojiToUnicodeSync(text);
+}
+
+export const convertEmojiToUnicodeSync = (text: string) => {
+    if (!regShortnames || !shortNameList) throw new Error("convertEmojisToUnicodeSync requires to call loadEmojiShortList before being called");
+    return convertEmojisToUnicode(text, regShortnames, shortNameList);
 }
