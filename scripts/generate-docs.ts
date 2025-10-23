5305170f9d9bf245b9cc2e854b0413a2eb510f47
Michael Gesenhues <michael.gesenhues@tobit.com>
2025-10-23 15:17:48 +0200

    Revert "🐛 Fix docs script file path"
    This reverts commit 17477f094bd8055d615516c249cbdbbe97e169d2.

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1044,10 +1044,7 @@ const main = async () => {
     for (const pkg of config.packages) {
         const packagePath = path.resolve(config.rootDir, pkg);
 
-        const indexPath = path.resolve(packagePath, 'src/index.ts');
-
-        console.log(`🔍 Scanning ${pkg} at: ${indexPath}`);
-
+        const indexPath = path.resolve(config.rootDir, pkg, 'src/index.ts');
         if (!fs.existsSync(indexPath)) continue;
 
         const sf = project.addSourceFileAtPathIfExists(indexPath);
