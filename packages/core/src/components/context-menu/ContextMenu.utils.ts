112e335f5ab0fa30a1c303af450a02796390c5fa
Michael Gesenhues <michael.gesenhues@tobit.com>
2026-01-12 13:11:43 +0100

    🐛 Fix ContextMenu first entry highlighted

--- packages/core/src/components/context-menu/ContextMenu.utils.ts
+++ packages/core/src/components/context-menu/ContextMenu.utils.ts
@@ -1,7 +1,4 @@
 import type { ContextMenuItem } from './ContextMenu.types';
 
-export const getDefaultFocusedIndex = (items: ContextMenuItem[]): number => {
-    const firstSelectedIndex = items.findIndex((item) => item.isSelected);
-
-    return firstSelectedIndex !== -1 ? firstSelectedIndex : 0;
-};
+export const getDefaultFocusedIndex = (items: ContextMenuItem[]): number =>
+    items.findIndex((item) => item.isSelected);
