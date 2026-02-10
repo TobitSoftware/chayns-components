ea78de3e79def16fc72ba54d0f3349ce3ece848e
Tom Wissing <77831931+tomwissing@users.noreply.github.com>
2026-02-10 12:39:33 +0100

    🐛 Fix verification badge styling for v5 CSS compatibility
    The verification badge component was not displaying correctly in v5
    due to missing font-family definitions and CSS classes.
    
    Changes:
    - Replace external CSS classes with inline styles for reliability
    - Rename badge class name to prevent conflicts with legacy CSS

--- packages/core/src/components/verification-badge/VerificationBadge.styles.ts
+++ packages/core/src/components/verification-badge/VerificationBadge.styles.ts
@@ -1,3 +1,39 @@
 import styled from 'styled-components';
 
-export const StyledVerificationBadge = styled.span``;
+export const StyledVerificationBadge = styled.span`
+    @font-face {
+        font-family: 'Verified-chaynsID';
+        src:
+            url(https://api.chayns-static.space/font-vcid/Verified-chaynsID.woff2) format('woff2'),
+            url(https://api.chayns-static.space/font-vcid/Verified-chaynsID.woff2) format('woff'),
+            url(https://api.chayns-static.space/font-vcid/Verified-chaynsID.woff2) format('woff'),
+            url(https://api.chayns-static.space/font-vcid/Verified-chaynsID.ttf) format('truetype'),
+            url(https://api.chayns-static.space/font-vcid/Verified-chaynsID.svg) format('svg');
+        font-weight: normal;
+        font-style: normal;
+        font-display: block;
+    }
+
+    /*noinspection CssNoGenericFontName*/
+    font-family: 'Verified-chaynsID' !important;
+    font-style: normal;
+    font-weight: normal;
+    font-variant: normal;
+    text-transform: none;
+    line-height: 1;
+    font-size: 0.8em;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    margin-left: 0.2em;
+
+    & > :first-child {
+        color: #5890ff;
+    }
+
+    & > :nth-child(2),
+    & > :nth-child(3) {
+        margin-left: -1em;
+        color: #fff;
+    }
+`;
--- packages/core/src/components/verification-badge/VerificationBadge.tsx
+++ packages/core/src/components/verification-badge/VerificationBadge.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { StyledVerificationBadge } from './VerificationBadge.styles';
 
 const VerificationBadge = () => (
-    <StyledVerificationBadge className="vcid-check--blue">
-        <span />
-        <span />
-        <span />
+    <StyledVerificationBadge className="beta-chayns-verification-badge">
+        <span>{'\ue903'}</span>
+        <span>{'\ue904'}</span>
+        <span>{'\ue905'}</span>
     </StyledVerificationBadge>
 );
 
