dce21b705941e2a4d0de1024deb6eadcd0792c9c
Tom Wissing <77831931+tomwissing@users.noreply.github.com>
2026-04-08 15:28:44 +0200

    🌐 Implement localization for FilterContent component
    Wrap the FilterContent component in a TextstringProvider and replace hardcoded strings for the search input and sorting section with localized text strings. This includes adding a new constants file to manage text string keys and their fallbacks.

--- packages/core/src/components/filter/filter-content/FilterContent.tsx
+++ packages/core/src/components/filter/filter-content/FilterContent.tsx
@@ -21,6 +21,9 @@ import {
 import ComboBox from '../../combobox/ComboBox';
 import Checkbox from '../../checkbox/Checkbox';
 import { IComboBoxItem } from '../../combobox/ComboBox.types';
+import { Textstring, TextstringProvider, ttsToITextString } from '@chayns-components/textstring';
+import textStrings from '../../../constants/textStrings';
+import { useLanguage } from 'chayns-api';
 
 export type FilterContentProps = {
     searchConfig?: SearchConfig;
@@ -43,6 +46,9 @@ const FilterContent: FC<FilterContentProps> = ({
     const searchRef = useRef<InputRef>(null);
 
     const [sortTextWidth, setSortTextWidth] = useState(0);
+
+    const { active: language } = useLanguage();
+    const ts = textStrings.components.filter.filterContent;
 
     const handleSelectSortItem = useCallback(
         (item: IComboBoxItem | undefined) => {
@@ -73,67 +79,76 @@ const FilterContent: FC<FilterContentProps> = ({
 
     return useMemo(
         () => (
-            <StyledFilterContent>
-                {searchConfig && (
-                    <Input
-                        ref={searchRef}
-                        onChange={(ev) => searchConfig.onSearchChange(ev.target.value)}
-                        placeholder="Suche"
-                        value={searchConfig.searchValue}
-                        shouldShowClearIcon={searchConfig.searchValue.length > 0}
-                        leftElement={<Icon icons={['fa fa-search']} />}
-                    />
-                )}
-                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-                {filterButtonConfig && <FilterButtons {...filterButtonConfig} />}
-                {sortConfig && (
-                    <StyledFilterSort>
-                        <StyledFilterSortText ref={sortTextRef}>Sortierung</StyledFilterSortText>
-                        <StyledFilterComboboxWrapper $textWidth={sortTextWidth}>
-                            <ComboBox
-                                lists={[
-                                    {
-                                        list: sortConfig.items.map(({ text, id }) => ({
-                                            text,
-                                            value: id,
-                                        })),
-                                    },
-                                ]}
-                                placeholder=""
-                                selectedItem={{
-                                    text: sortConfig.selectedItem.text,
-                                    value: sortConfig.selectedItem.id,
-                                }}
-                                onSelect={handleSelectSortItem}
-                            />
-                        </StyledFilterComboboxWrapper>
-                    </StyledFilterSort>
-                )}
-                {comboboxConfig && (
-                    <StyledFilterComboboxInline>
-                        <StyledFilterComboboxInlineLabel>
-                            {comboboxConfig.label}
-                        </StyledFilterComboboxInlineLabel>
-                        <StyledFilterComboboxInlineComboboxWrapper>
-                            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-                            <ComboBox shouldUseCurrentItemWidth {...comboboxConfig} />
-                        </StyledFilterComboboxInlineComboboxWrapper>
-                    </StyledFilterComboboxInline>
-                )}
-                {checkboxConfig && (
-                    // eslint-disable-next-line react/jsx-props-no-spreading
-                    <Checkbox {...checkboxConfig} />
-                )}
-            </StyledFilterContent>
+            <TextstringProvider language={language} libraryName="@chayns-components-core">
+                <StyledFilterContent>
+                    {searchConfig && (
+                        <Input
+                            ref={searchRef}
+                            onChange={(ev) => searchConfig.onSearchChange(ev.target.value)}
+                            placeholder={
+                                <Textstring textstring={ttsToITextString(ts.input.placeholder)} />
+                            }
+                            value={searchConfig.searchValue}
+                            shouldShowClearIcon={searchConfig.searchValue.length > 0}
+                            leftElement={<Icon icons={['fa fa-search']} />}
+                        />
+                    )}
+                    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                    {filterButtonConfig && <FilterButtons {...filterButtonConfig} />}
+                    {sortConfig && (
+                        <StyledFilterSort>
+                            <StyledFilterSortText ref={sortTextRef}>
+                                <Textstring textstring={ttsToITextString(ts.sort)} />
+                            </StyledFilterSortText>
+                            <StyledFilterComboboxWrapper $textWidth={sortTextWidth}>
+                                <ComboBox
+                                    lists={[
+                                        {
+                                            list: sortConfig.items.map(({ text, id }) => ({
+                                                text,
+                                                value: id,
+                                            })),
+                                        },
+                                    ]}
+                                    placeholder=""
+                                    selectedItem={{
+                                        text: sortConfig.selectedItem.text,
+                                        value: sortConfig.selectedItem.id,
+                                    }}
+                                    onSelect={handleSelectSortItem}
+                                />
+                            </StyledFilterComboboxWrapper>
+                        </StyledFilterSort>
+                    )}
+                    {comboboxConfig && (
+                        <StyledFilterComboboxInline>
+                            <StyledFilterComboboxInlineLabel>
+                                {comboboxConfig.label}
+                            </StyledFilterComboboxInlineLabel>
+                            <StyledFilterComboboxInlineComboboxWrapper>
+                                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                                <ComboBox shouldUseCurrentItemWidth {...comboboxConfig} />
+                            </StyledFilterComboboxInlineComboboxWrapper>
+                        </StyledFilterComboboxInline>
+                    )}
+                    {checkboxConfig && (
+                        // eslint-disable-next-line react/jsx-props-no-spreading
+                        <Checkbox {...checkboxConfig} />
+                    )}
+                </StyledFilterContent>
+            </TextstringProvider>
         ),
         [
             checkboxConfig,
             comboboxConfig,
             filterButtonConfig,
             handleSelectSortItem,
+            language,
             searchConfig,
             sortConfig,
             sortTextWidth,
+            ts.input.placeholder,
+            ts.sort,
         ],
     );
 };
---
+++ packages/core/src/constants/textStrings.ts
@@ -0,0 +1,19 @@
+export default {
+    components: {
+        filter: {
+            filterContent: {
+                input: {
+                    placeholder: {
+                        stringName:
+                            'txt_chayns_components_core_components_filter_filterContent_input_placeholder',
+                        fallback: 'Suche',
+                    },
+                },
+                sort: {
+                    stringName: 'txt_chayns_components_core_components_filter_filterContent_sort',
+                    fallback: 'Sortierung',
+                },
+            },
+        },
+    },
+} as const;
