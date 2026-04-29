dfa243bb539fba8fee2708df569db8862b990db0
Michael Gesenhues <michael.gesenhues@tobit.com>
2026-04-29 13:11:13 +0200

    ♻️ Rename textstring config to fix command bug

--- packages/communication/src/constants/textStrings.ts
+++ packages/communication/src/constants/textStrings.ts
@@ -1,47 +1,54 @@
 export default {
     communicationHeader: {
-        headerMembers: {
-            to: {
-                stringName: '',
-                fallback: 'An',
-            },
-            cc: {
-                stringName: '',
-                fallback: 'Cc',
-            },
-        },
         headerActions: {
+            more: {
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationHeader_headerActions_more',
+                fallback: 'Mehr',
+            },
             read: {
-                stringName: '',
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationHeader_headerActions_read',
                 fallback: 'Als gelesen markieren',
             },
-            unread: {
-                stringName: '',
-                fallback: 'Als ungelesen markieren',
-            },
             teamTalk: {
-                stringName: '',
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationHeader_headerActions_teamTalk',
                 fallback: 'TeamTalk',
             },
-            more: {
-                stringName: '',
-                fallback: 'Mehr',
+            unread: {
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationHeader_headerActions_unread',
+                fallback: 'Als ungelesen markieren',
+            },
+        },
+        headerMembers: {
+            cc: {
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationHeader_headerMembers_cc',
+                fallback: 'Cc',
+            },
+            to: {
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationHeader_headerMembers_to',
+                fallback: 'An',
             },
         },
     },
     communicationList: {
         sort: {
+            older: {
+                stringName: 'txt_chayns_components_v5_communication_communicationList_sort_older',
+                fallback: 'Letzte 7 Tage',
+            },
             today: {
-                stringName: '',
+                stringName: 'txt_chayns_components_v5_communication_communicationList_sort_today',
                 fallback: 'Heute',
             },
             yesterday: {
-                stringName: '',
+                stringName:
+                    'txt_chayns_components_v5_communication_communicationList_sort_yesterday',
                 fallback: 'Gestern',
-            },
-            older: {
-                stringName: '',
-                fallback: 'Letzte 7 Tage',
             },
         },
     },
