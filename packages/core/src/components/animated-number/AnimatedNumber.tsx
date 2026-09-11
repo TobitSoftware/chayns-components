93cf82560acc125104fc4def916ee8099a9b0ae5
Michael Gesenhues <michael.gesenhues@tobit.com>
2026-09-11 07:13:50 +0200

    Revert "📦 Upgrade Motion to v13 with v12 compatibility"
    This reverts commit f53f514cdb1a1fcf07256a33bac3b887f9d16936.

--- packages/core/src/components/animated-number/AnimatedNumber.tsx
+++ packages/core/src/components/animated-number/AnimatedNumber.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect, useMemo, useState } from 'react';
 import { animate, useMotionValue } from 'motion/react';
-import { Easing } from 'motion';
+import { Easing, MotionValue, ObjectTarget } from 'motion';
 
 type Ease = [number, number, number, number] | ((t: number) => number) | string;
 
@@ -75,7 +75,7 @@ export const AnimatedNumber: FC<AnimatedNumberProps> = ({
     useEffect(() => {
         motionValue.set(startFrom);
 
-        const controls = animate(motionValue, value, {
+        const controls = animate(motionValue, value as ObjectTarget<MotionValue<number>>, {
             duration,
             delay,
             ease: ease as Easing,
